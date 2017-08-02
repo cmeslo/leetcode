d967c1bf88f15b02b565f35d94d2de539b0fbefa
Cmes <rm-xp@hotmail.com>
2017-08-02 22:28:22 +0800

    20170802

---
+++ solution/389. Find the Difference/389_xor02.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char ans = 0;
+		for (int i = 0; i < s.length(); ++i) {
+			ans ^= s[i];
+			ans ^= t[i];
+		}
+		ans ^= t[t.length() - 1];
+		return ans;
+    }
+};
---
+++ solution/389. Find the Difference/README.md
@@ -0,0 +1,10 @@
+Got a wrong answer... what?
+
+Submission Result: Wrong Answer
+
+Input:
+"abcd"
+"abcde"
+
+Output:"e"
+Expected:'e'
