bdfabbba569a3584d57b061b50300921ad6eb327
Cmes <rm-xp@hotmail.com>
2017-08-14 23:31:49 +0800

    20170814

---
+++ solution/18. 4Sum/18_02.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        if (nums.size() < 4) return ans;
+
+        std::sort(nums.begin(), nums.end());
+        
+        for (int i = 0; i < nums.size() - 3; ++i) {
+            
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            for (int j = i + 1; j < nums.size() - 2; ++j) {
+                
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int left = j + 1, right = nums.size() - 1;
+                
+                while (left < right) {
+                    
+                    //std::cout << nums[i] << ", " << nums[j] << ", " << nums[left] << ", " << nums[right] << std::endl;
+                    
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        ans.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (nums[right] == nums[right - 1]) right--;
+                        left++, right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        //std::cout << "==========" << std::endl;
+        return ans;
+    }
+};
+
+int main() {
+    Solution* sol = new Solution;
+    //vector<int> test = {1, 0, -1, 0, -2, 2}; // target = 0
+    //vector<int> test = {0, 0, 0, 0}; // target = 0
+    vector<int> test = {-1, 0, 1, 2, -1, -4}; // target = -1;
+    //vector<int> test = {-3, -1, 0, 2, 4, 5}; // target = 2
+    vector<vector<int>> ans = sol->fourSum(test, -1);
+    
+    for (auto i : ans) {
+        for (auto j : i) {
+            std::cout << j << ", ";
+        }
+        std::cout << std::endl;
+    }
+}
