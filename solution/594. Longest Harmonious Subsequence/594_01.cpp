4022aca9118ec6a3e3696c0d44d4c679125e8532
Ray <rm-xp@hotmail.com>
2017-09-10 03:04:25 +0800

    Create 594_01.cpp

---
+++ solution/594. Longest Harmonious Subsequence/594_01.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        
+        int maxLen = 0;
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            int len = 1;
+            for (int j = i + 1; j < nums.size(); ++j) {
+                if (abs(nums[j] - nums[i]) <= 1)
+                    ++len;
+                else 
+                    break;
+                
+                if (len > maxLen && nums[i] != nums[j]) maxLen = len;
+            }
+        }
+        return maxLen;
+    }
+};
