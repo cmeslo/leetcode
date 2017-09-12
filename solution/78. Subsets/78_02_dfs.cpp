4e238046961f84e09804e1a695e6df90b28c2bfb
Ray <rm-xp@hotmail.com>
2017-09-12 13:54:36 +0800

    Create 78_02_dfs.cpp

---
+++ solution/78. Subsets/78_02_dfs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> root;
+        dfs(nums, 0, root, res);
+        return res;
+    }
+    
+    void dfs(vector<int>& nums, int index, vector<int>& root, vector<vector<int>>& res) {
+        res.push_back(root);
+        for (int i = index; i < nums.size(); ++i) {
+            root.push_back(nums[i]);
+            dfs(nums, i + 1, root, res);
+            root.pop_back();
+        }
+    }
+};
