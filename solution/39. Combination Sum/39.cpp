753830d93aecb3235f8c54cf682dcb7c35e76545
Ray <rm-xp@hotmail.com>
2017-09-17 22:11:52 +0800

    Create 39.cpp

---
+++ solution/39. Combination Sum/39.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> res;
+        vector<int> out;
+        
+        dfs(candidates, target, 0, out, res);
+        return res;
+    }
+    
+    void dfs(vector<int>& candidates, int target, int start, vector<int>& out, vector<vector<int>>& res) {
+        if (target < 0) return;
+        if (target == 0) {
+            res.push_back(out);
+            return;
+        }
+        for (int i = start; i < candidates.size(); ++i) {
+            out.push_back(candidates[i]);
+            dfs(candidates, target - candidates[i], i, out, res);
+            out.pop_back();
+        }
+    }
+};
