a28435c25c078ce19e23e88610f674cc96f49b7c
Ray <rm-xp@hotmail.com>
2017-09-18 14:19:15 +0800

    Create 135_01.cpp

---
+++ solution/135. Candy/135_01.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> candys(n, 1);
+        int cnt = 0;
+
+        vector<vector<int>> sortedRatings(n);
+
+        for (int i = 0; i < n; ++i) {
+            sortedRatings[i].push_back(ratings[i]);
+            sortedRatings[i].push_back(i);
+        }
+
+        sort(sortedRatings.begin(), sortedRatings.end(), [&](vector<int> a, vector<int> b) {
+            return a[0] < b[0];
+        });
+
+        for (int i = 1; i < n; ++i) {
+            int index = sortedRatings[i][1];
+            if (index - 1 >= 0) {
+                if (ratings[index] > ratings[index - 1]) {
+                    candys[index] = max(candys[index], candys[index - 1] + 1);
+                }
+            }
+            if (index + 1 < n) {
+                if (ratings[index] > ratings[index + 1]) {
+                    candys[index] = max(candys[index], candys[index + 1] + 1);
+                }
+            }
+        }
+
+        for (int c : candys) cnt += c;
+        return cnt;
+    }
+};
