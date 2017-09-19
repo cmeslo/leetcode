f4b9d8b004a70285241020a3314b13fd7670dbde
Ray <rm-xp@hotmail.com>
2017-09-19 10:56:01 +0800

    Update 135_02.cpp

--- solution/135. Candy/135_02.cpp
+++ solution/135. Candy/135_02.cpp
@@ -3,16 +3,16 @@ public:
     int candy(vector<int>& ratings) {
         int sum = 0;
         int size = ratings.size();
-        vector<int> candys(size, 1);
+        vector<int> candies(size, 1);
         
         for (int i = 0; i < size - 1; ++i) {
-            if (ratings[i] < ratings[i + 1]) candys[i + 1] = candys[i] + 1;
+            if (ratings[i] < ratings[i + 1]) candies[i + 1] = candies[i] + 1;
         }
         for (int i = size - 1; i > 0; --i) {
-            if (ratings[i - 1] > ratings[i]) candys[i - 1] = max(candys[i - 1], candys[i] + 1);
+            if (ratings[i - 1] > ratings[i]) candies[i - 1] = max(candies[i - 1], candies[i] + 1);
         }
         
-        for (int c : candys) sum += c;
+        for (int c : candies) sum += c;
         return sum;
     }
 };
