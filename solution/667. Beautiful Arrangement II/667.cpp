aca460849576826328211c1d41e28dde22db97a3
Ray <rm-xp@hotmail.com>
2017-09-30 09:27:13 +0800

    Create 667.cpp

---
+++ solution/667. Beautiful Arrangement II/667.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> constructArray(int n, int k) {
+        vector<int> res;
+        
+        for (int i = 1, j = n; i <= j; ) {
+            if (k > 1)
+                res.push_back(k-- % 2 == 1 ? i++ : j--);
+            else
+                res.push_back(i++);
+        }
+        return res;
+    }
+};
