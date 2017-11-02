38927e958aff1e263613e2a4d26e180adcb0506f
Ray <rm-xp@hotmail.com>
2017-11-02 23:44:36 +0800

    Create 46_02.cpp

---
+++ solution/46. Permutations/46_02.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        dfs(nums, 0, res);
+        return res;
+    }
+    
+    void dfs(vector<int>& nums, int start, vector<vector<int>>& res) {
+        if (start >= nums.size()) {
+            res.push_back(nums);
+            return;
+        }
+        
+        for (int i = start; i < nums.size(); ++i) {
+            swap(nums[start], nums[i]);
+            dfs(nums, start + 1, res);
+            swap(nums[start], nums[i]);
+        }
+    }
+};
