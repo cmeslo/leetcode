ef003f1e7f9e58d9a0b34e2677ea8931a23a2ae2
Ray <rm-xp@hotmail.com>
2018-07-28 08:31:33 +0800

    Create 268.cpp

---
+++ solution/268. Missing Number/268.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] != 0 && nums[i] != i + 1) std::swap(nums[i], nums[nums[i] - 1]);
+        }
+        
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == 0) return i + 1;
+        }
+        
+        return 0;
+    }
+};
