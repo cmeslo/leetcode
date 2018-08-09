f5979dfd601b53d80fd01d69f754ecef1d452827
Ray <rm-xp@hotmail.com>
2018-08-10 00:04:24 +0800

    Create 174.cpp

---
+++ solution/174. Dungeon Game/174.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        const vector<vector<int>> &d = dungeon;
+        const int m = d.size();
+        const int n = d[0].size();
+        
+        vector<vector<int>> hp(m + 1, vector<int>(n + 1, INT_MAX));
+        hp[m][n - 1] = hp[m - 1][n] = 1;
+        
+        for (int y = m - 1; y >= 0; --y) {
+            for (int x = n - 1; x >= 0; --x) {
+                hp[y][x] = max(1, min(hp[y + 1][x], hp[y][x + 1]) - d[y][x]);
+            }
+        }
+        
+        return hp[0][0];
+    }
+};
