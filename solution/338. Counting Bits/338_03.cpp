d33dbfa2e613b414293d9f5fb14baf51fa789592
Ray <rm-xp@hotmail.com>
2020-05-29 00:31:39 +0800

    Create 338_03.cpp

---
+++ solution/338. Counting Bits/338_03.cpp
@@ -0,0 +1,18 @@
+// Runtime: 4 ms
+
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        if (num == 0) return vector<int>{0};
+        vector<int> res {0};
+        
+        for (int i = 1; i <= num; i++) {
+            if (i % 2 != 0) {
+                res.push_back(res[i-1]+1);
+            } else {
+                res.push_back(res[i/2]);
+            }
+        }
+        return res;
+    }
+};
