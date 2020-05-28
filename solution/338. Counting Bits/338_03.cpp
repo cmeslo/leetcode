d9ab514190b499348bbd095813a0bc4b1a569f62
Ray <rm-xp@hotmail.com>
2020-05-29 00:53:14 +0800

    增加一種寫法

--- solution/338. Counting Bits/338_03.cpp
+++ solution/338. Counting Bits/338_03.cpp
@@ -17,3 +17,19 @@ public:
         return res;
     }
 };
+/*
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> res(num+1, 0);        
+        for (int i = 1; i <= num; i++) {
+            if (i % 2 != 0) {
+                res[i] = res[i-1]+1;
+            } else {
+                res[i] = res[i/2];
+            }
+        }
+        return res;
+    }
+};
+*/
