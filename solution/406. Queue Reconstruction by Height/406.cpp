687624b295fcc3e6cdbcd8cdd985e5f9688c1af0
Ray <rm-xp@hotmail.com>
2020-06-07 00:23:13 +0800

    Create 406.cpp

---
+++ solution/406. Queue Reconstruction by Height/406.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(), people.end(), [](const vector<int>& a, const vector<int>& b) {
+            return (a[0] > b[0]) || (a[0] == b[0] && a[1] < b[1]);
+        });
+        
+        vector<vector<int>> res;
+        for (const vector<int>& p : people) {
+            res.insert(res.begin() + p[1], p);
+        }
+        return res;
+    }
+};
