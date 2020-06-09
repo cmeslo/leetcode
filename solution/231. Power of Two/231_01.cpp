e486d4fda8fb0788027ec24fc37b840caa2fdb08
Ray <rm-xp@hotmail.com>
2020-06-09 08:53:00 +0800

    Update 231_01.cpp

--- solution/231. Power of Two/231_01.cpp
+++ solution/231. Power of Two/231_01.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-		int ans = 0;
-		while (n) {
-			ans += (n & 1);
-			n >>= 1;
-			if (ans > 1) return false;
-		}
-		return ans == 1;
-	}
+        int cnt = 0;
+        while (n) {
+            cnt += (n & 1);
+            if (cnt > 1) return false;
+            n >>= 1;
+        }
+        return cnt == 1;
+    }
 };
