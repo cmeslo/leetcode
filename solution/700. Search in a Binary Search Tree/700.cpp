e94df7d8857c18f27bff1bdd2b1cdc6ef197de1f
Ray <rm-xp@hotmail.com>
2020-06-16 14:02:50 +0800

    Update 700.cpp

--- solution/700. Search in a Binary Search Tree/700.cpp
+++ solution/700. Search in a Binary Search Tree/700.cpp
@@ -11,16 +11,38 @@
  */
 class Solution {
 public:
+    // 寫法一
+    // TreeNode* searchBST(TreeNode* root, int val) {
+    //     TreeNode* cur = root;
+        
+    //     while (cur != nullptr) {
+    //         if (cur->val == val) {
+    //             return cur;
+    //         } else {
+    //             cur = (val < cur->val) ? cur->left : cur->right;
+    //         }
+    //     }
+    //     return nullptr;
+    // }
+
+    // 寫法二
     TreeNode* searchBST(TreeNode* root, int val) {
         TreeNode* cur = root;
-        
-        while (cur != nullptr) {
-            if (cur->val == val) {
-                return cur;
-            } else {
-                cur = (val < cur->val) ? cur->left : cur->right;
-            }
+
+        while (cur && cur->val != val) {
+            cur = (val < cur->val) ? cur->left : cur->right;
         }
-        return nullptr;
+        return cur;
     }
+
+    // 寫法三
+    // TreeNode* searchBST(TreeNode* root, int val) {
+    //     if (!root) return nullptr;
+    //     if (root->val == val) return root;
+    //     if (root->val > val) {
+    //         return searchBST(root->left, val);
+    //     } else {
+    //         return searchBST(root->right, val);
+    //     }        
+    // }
 };
