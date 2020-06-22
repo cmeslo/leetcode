91ea3f680f3b61f3af89a6902605a50d4da24920
Ray <rm-xp@hotmail.com>
2020-06-22 22:13:18 +0800

    Update 137_01.cpp

--- solution/137. Single Number II/137_01.cpp
+++ solution/137. Single Number II/137_01.cpp
@@ -1,28 +1,14 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-	int singleNumber(vector<int>& nums) {
-		int ans = 0;
-		
-		for (int i = 0; i < 32; ++i) {
-			int col = 0;
-			for (int j = 0; j < nums.size(); ++j) {
-				col += (nums[j] >> i) & 1;
-			}
-			ans |= (col % 3) << i;
-		}
-		return ans;
-	}
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        
+        for (int i = 0; i < 32; i++) {
+            int sum = 0;
+            for (int n : nums)
+                sum += (n >> i) & 1;
+            ans |= (sum % 3) << i;
+        }
+        return ans;
+    }
 };
-
-void main()
-{
-	auto ans = new Solution;
-
-	vector<int> test = { 1, 1, 1, 2, 3, 3, 3 };
-
-	cout << ans->singleNumber(test) << endl;
-}
