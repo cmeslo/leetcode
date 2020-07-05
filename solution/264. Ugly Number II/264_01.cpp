1cb6b9a47cbb49308f8490fe9542da701d55ab40
Ray <rm-xp@hotmail.com>
2020-07-05 22:59:13 +0800

    Create 264_01.cpp

---
+++ solution/264. Ugly Number II/264_01.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int> ugly_nums;
+        for (long a = 1; a < INT_MAX; a*=2)
+            for (long b = a; b < INT_MAX; b*=3)
+                for (long c = b; c < INT_MAX; c*=5)
+                    ugly_nums.push_back(c);
+        sort(ugly_nums.begin(), ugly_nums.end());
+        return ugly_nums[n-1];
+    }
+};
