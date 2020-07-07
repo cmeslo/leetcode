871ef05679fd6cb505c2cf054b0a175deb5583f1
Ray <rm-xp@hotmail.com>
2020-07-08 01:00:35 +0800

    Update 461.cpp

--- solution/461. Hamming Distance/461.cpp
+++ solution/461. Hamming Distance/461.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int hammingDistance(int x, int y) {
         int n = x ^ y;
-		int dist = 0;
-		while (n) {
-			if ((n >> 1) << 1 != n) {
-				dist++;
-			}
-			n >>= 1;
-		}
-		return dist;
+        int dist = 0;
+        while (n) {
+            if ((n >> 1) << 1 != n) {
+                dist++;
+            }
+            n >>= 1;
+        }
+        return dist;
     }
 };
