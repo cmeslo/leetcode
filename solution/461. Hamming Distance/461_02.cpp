a20a5eca9b8fc25167acd3d2aca229d5e07b0bca
Ray <rm-xp@hotmail.com>
2020-07-08 01:01:25 +0800

    Create 461_02.cpp

---
+++ solution/461. Hamming Distance/461_02.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int z = x^y, cnt = 0;
+        for (int i = 0; i < 32; i++) cnt += (z>>i)&1;
+        return cnt;
+    }
+};
