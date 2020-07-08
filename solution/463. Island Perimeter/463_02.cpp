e0dc09db51d9d1c8dff77c3c1345bfbde5acdca7
Ray <rm-xp@hotmail.com>
2020-07-09 07:30:09 +0800

    Update 463_02.cpp

--- solution/463. Island Perimeter/463_02.cpp
+++ solution/463. Island Perimeter/463_02.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-	int islandPerimeter(vector<vector<int>>& grid) {
-		int res = 0;
-		int rowLen = grid.size();
-		int colLen = grid[0].size();
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int res = 0;
+        int rowLen = grid.size();
+        int colLen = grid[0].size();
 
-		for (int i = 0; i < rowLen; ++i) {
-			for (int j = 0; j < colLen; ++j) {
-				if (grid[i][j] == 0) continue;
-				res += 4;
-				if (j > 0 && grid[i][j - 1] == 1) res-=2;
-				if (i > 0 && grid[i - 1][j] == 1) res-=2;
-			}
-		}
-		return res;
-	}
+        for (int i = 0; i < rowLen; ++i) {
+            for (int j = 0; j < colLen; ++j) {
+                if (grid[i][j] == 0) continue;
+                res += 4;
+                if (j > 0 && grid[i][j - 1] == 1) res-=2;
+                if (i > 0 && grid[i - 1][j] == 1) res-=2;
+            }
+        }
+        return res;
+    }
 };
