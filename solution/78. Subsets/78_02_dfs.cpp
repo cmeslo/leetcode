7c3707757efc3571e1f8662aa78911954ec3d2f1
Ray <rm-xp@hotmail.com>
2020-07-14 00:01:13 +0800

    Update 78_02_dfs.cpp

--- solution/78. Subsets/78_02_dfs.cpp
+++ solution/78. Subsets/78_02_dfs.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> res;
-        vector<int> root;
-        dfs(nums, 0, root, res);
+        vector<int> cur;
+        dfs(nums, 0, cur, res);
         return res;
     }
     
-    void dfs(vector<int>& nums, int index, vector<int>& root, vector<vector<int>>& res) {
-        res.push_back(root);
+    void dfs(vector<int>& nums, int index, vector<int>& cur, vector<vector<int>>& res) {
+        res.push_back(cur);
         for (int i = index; i < nums.size(); ++i) {
-            root.push_back(nums[i]);
-            dfs(nums, i + 1, root, res);
-            root.pop_back();
+            cur.push_back(nums[i]);
+            dfs(nums, i + 1, cur, res);
+            cur.pop_back();
         }
     }
 };
