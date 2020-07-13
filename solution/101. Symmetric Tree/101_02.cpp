bbdcbedfa9a1790f15f0ab1757e9904753c8483c
Ray <rm-xp@hotmail.com>
2020-07-14 00:54:54 +0800

    Create 101_02.cpp

---
+++ solution/101. Symmetric Tree/101_02.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(root);
+        
+        while (!q.empty()) {
+            auto* a = q.front(); q.pop();
+            auto* b = q.front(); q.pop();
+            if (!a && !b) continue;
+            if (!a || !b) return false;
+            if (a->val != b->val) return false;
+            q.push(a->left);
+            q.push(b->right);
+            q.push(a->right);
+            q.push(b->left);
+        }
+        return true;
+    }
+};
