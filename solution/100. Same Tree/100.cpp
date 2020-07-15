8ec483f6642bbec914d4f152ded787c6601afcea
Ray <rm-xp@hotmail.com>
2020-07-15 08:51:43 +0800

    Update 100.cpp

--- solution/100. Same Tree/100.cpp
+++ solution/100. Same Tree/100.cpp
@@ -1,59 +1,24 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-	bool isSameTree(TreeNode* p, TreeNode* q)
-	{
-		if (nullptr == p && nullptr == q) return true;
-
-		if (nullptr != p && nullptr != q && p->val == q->val)
-		{
-			if (isSameTree(p->left, q->left) && isSameTree(p->right, q->right)) {
-				return true;
-			}
-		}
-		return false;
-	}
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        // both null
+        if (!p && !q) return true;
+        // one of them is null
+        if (!p || !q) return false;
+        // different value
+        if (p->val != q->val) return false;
+        return isSameTree(p->left, q->left)
+            && isSameTree(p->right, q->right);
+    }
 };
-
-void main()
-{
-//	TreeNode* t = nullptr;
-
-	TreeNode* t = new TreeNode(1);
-
-	t->left = new TreeNode(2);
-	t->left->left = new TreeNode(4);
-	t->left->left->left = new TreeNode(6);
-	t->right = new TreeNode(3);
-	t->right->right = new TreeNode(5);
-	t->right->right->right = new TreeNode(7);
-
-//	TreeNode* t2 = nullptr;
-	TreeNode* t2 = new TreeNode(1);
-	t2->left = new TreeNode(2);
-	t2->left->left = new TreeNode(4);
-	t2->left->left->left = new TreeNode(6);
-	t2->right = new TreeNode(3);
-	t2->right->right = new TreeNode(5);
-	t2->right->right->right = new TreeNode(7);
-
-// 	t->left = new TreeNode(9);
-// 	t->right = new TreeNode(20);
-// 	t->right->left = new TreeNode(15);
-// 	t->right->right = new TreeNode(7);
-
-	auto ans = new Solution;
-
-	auto v = ans->isSameTree(t, t2);
-	v ? cout << "is same" << endl : cout << "is not same" << endl;
-}
