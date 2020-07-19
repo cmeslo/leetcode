deca3884a533c2746e9df99b328505ab523c131f
Ray <rm-xp@hotmail.com>
2020-07-19 23:31:12 +0800

    Create 347_01.cpp

---
+++ solution/347. Top K Frequent Elements/347_01.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> count;
+        for (const int num : nums) {
+            count[num]++;
+        }
+        
+        unordered_map<int, vector<int>> bucket;
+        for (auto& kv : count) {
+            bucket[kv.second].push_back(kv.first);
+        }
+
+        vector<int> ans;
+        for (int i = nums.size(); i > 0, k > 0; i--) {
+            auto it = bucket.find(i);
+            if (it == bucket.end()) continue;
+            for (int j : bucket[i]) {
+                ans.push_back(j);
+                if (--k == 0) break;
+            }
+        }
+        
+        return ans;
+    }
+};
