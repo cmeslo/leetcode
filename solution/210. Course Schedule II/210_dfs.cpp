36dca6ab64959582cb9b27b9a5a39587bb9c0f16
Ray <rm-xp@hotmail.com>
2020-07-21 23:44:07 +0800

    Update 210_dfs.cpp

--- solution/210. Course Schedule II/210_dfs.cpp
+++ solution/210. Course Schedule II/210_dfs.cpp
@@ -1,63 +1,33 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-	vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites)
-	{
-		vector<vector<int>> graph = makeGraph(numCourses, prerequisites);
-		vector<bool> visited(numCourses, false), dfsPath(numCourses, false);
-		vector<int> topoSort;
-
-		for (int i = 0; i < numCourses; ++i) {
-			if (!visited[i] && doDFS(i, graph, visited, dfsPath, topoSort)) {
-				return {};
-			}
-		}
-		reverse(topoSort.begin(), topoSort.end());
-		return topoSort;
-	}
-
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> g = vector<vector<int>>(numCourses);
+        vector<int> v = vector<int>(numCourses, 0); // 0 = unknow, 1 = visiting, 2 = visited
+        
+        for (const auto& pre : prerequisites)
+            g[pre[1]].push_back(pre[0]);
+        
+        vector<int> ans;
+        for (int i = 0; i < numCourses; i++) {
+            if (dfs(i, ans, g, v)) return {};
+        }
+        
+        std::reverse(ans.begin(), ans.end());
+        return ans;
+    }
+    
 private:
-	vector<vector<int>> makeGraph(int numCourses, vector<pair<int, int>>& prerequisites)
-	{
-		vector<vector<int>> graph(numCourses, vector<int>(0));
-
-		for (auto pre : prerequisites) {
-			graph[pre.second].push_back(pre.first);
-		}
-		return graph;
-	}
-
-	bool doDFS(int node, vector<vector<int>>& graph, vector<bool>& visited, vector<bool>&dfsPath, vector<int>& topoSort) {
-		if (visited[node]) return false;
-		visited[node] = dfsPath[node] = true;
-
-		for (int neigh : graph[node]) {
-			if (dfsPath[neigh] || doDFS(neigh, graph, visited, dfsPath, topoSort)) return true;
-		}
-		dfsPath[node] = false;
-		topoSort.push_back(node);
-		return false;
-	}
+    bool dfs(int node, vector<int>& ans, vector<vector<int>>& g, vector<int>& v) {
+        if (v[node] == 1) return true; // cycle
+        if (v[node] == 2) return false;
+        
+        v[node] = 1;
+        for (int neighbour : g[node]) {
+            if (dfs(neighbour, ans, g, v)) return true;
+        }
+        v[node] = 2;
+        ans.push_back(node);
+        
+        return false;
+    }
 };
-
-void main()
-{
-	auto ans = new Solution;
-
-// 	pair<int, int> a = { 1, 0 };
-// 	pair<int, int> b = { 2, 1 };
-	vector<pair<int, int>> test;
-
-	test.push_back({ 1, 0 });
-	test.push_back({ 2, 0 });
-	test.push_back({ 3, 1 });
-	test.push_back({ 3, 2 });
-
-	vector<int> path = ans->findOrder(4, test);
-	for (auto i : path) {
-		cout << i << ", ";
-	}
-}
