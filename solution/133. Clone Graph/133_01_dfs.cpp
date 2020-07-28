dec6de7535c68cda09ec1eae64d5b0809f862525
Ray <rm-xp@hotmail.com>
2020-07-28 15:05:07 +0800

    Update 133_01_dfs.cpp

--- solution/133. Clone Graph/133_01_dfs.cpp
+++ solution/133. Clone Graph/133_01_dfs.cpp
@@ -1,27 +1,42 @@
-/**
- * Definition for undirected graph.
- * struct UndirectedGraphNode {
- *     int label;
- *     vector<UndirectedGraphNode *> neighbors;
- *     UndirectedGraphNode(int x) : label(x) {};
- * };
- */
-class Solution {
+/*
+// Definition for a Node.
+class Node {
 public:
-    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {   
-        unordered_map<int, UndirectedGraphNode*> m;
-        return cloneGraph(node, m);
+    int val;
+    vector<Node*> neighbors;
+    
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
     }
     
-    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node, unordered_map<int, UndirectedGraphNode*>& m) {
-        if (node == NULL) return NULL;
-        if (m.count(node->label)) return m[node->label];
-        
-        UndirectedGraphNode *dup = new UndirectedGraphNode(node->label);
-        m[node->label] = dup;
-        for (int i = 0; i < node->neighbors.size(); ++i) {
-            dup->neighbors.push_back(cloneGraph(node->neighbors[i], m));
-        }
-        return dup;
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
     }
+    
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (!node) return nullptr;
+        if (copies.find(node->val) != copies.end()) return copies[node->val];
+        
+        Node* new_node = new Node(node->val);
+        copies[node->val] = new_node;
+        
+        for (Node* n : node->neighbors)
+            new_node->neighbors.push_back(cloneGraph(n));
+        
+        return new_node;
+    }
+    
+private:
+    unordered_map<int, Node*> copies;
 };
