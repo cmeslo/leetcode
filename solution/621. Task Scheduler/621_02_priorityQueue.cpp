74f3f12bddbf5515340b4d738ab9d0837ee12b93
Ray <rm-xp@hotmail.com>
2020-07-29 14:14:56 +0800

    Update 621_02_priorityQueue.cpp

--- solution/621. Task Scheduler/621_02_priorityQueue.cpp
+++ solution/621. Task Scheduler/621_02_priorityQueue.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        
         int res = 0, partLen = n + 1;
         unordered_map<char, int> m;
         priority_queue<int> q;
@@ -10,17 +9,15 @@ public:
         for (auto a : m) q.push(a.second);
         
         while(!q.empty()) {
-            int cnt = 0;
             vector<int> part;
             for (int i = 0; i < partLen; ++i) {
                 if (!q.empty()) {
                     part.push_back(q.top()); q.pop();
-                    cnt++;
                 }
             }
             for (int p : part) if (--p > 0) q.push(p);
             
-            res += q.empty() ? cnt : partLen;
+            res += q.empty() ? part.size() : partLen;
         }
         return res;
     }
