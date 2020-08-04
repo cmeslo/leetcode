8537fd2eadcc5ab016adb237268be38cec8e18b0
Ray <rm-xp@hotmail.com>
2020-08-04 08:48:15 +0800

    Update 1535.cpp

--- solution/1535. Find the Winner of an Array Game/1535.cpp
+++ solution/1535. Find the Winner of an Array Game/1535.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     int getWinner(vector<int>& arr, int k) {
-        deque<int> dq(arr.begin(), arr.end());
-        int win_count = 0;
+        int winner = arr[0], win_count = 0;
         
-        int winner = dq.front(); dq.pop_front();
-        while (win_count < k && win_count < arr.size()) {
-            if (winner > dq.front()) {
-                win_count++;
-                int loser = dq.front(); dq.pop_front();
-                dq.push_back(loser);
-            } else {
-                win_count = 1;
-                dq.push_back(winner);
-                winner = dq.front(); dq.pop_front();
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr[i] > winner) {
+                winner = arr[i];
+                win_count = 0;
             }
+            if (++win_count >= k) break;
         }
         
         return winner;
