8549be73185181378fad2ccc48fedc56dd67c24c
Ray <rm-xp@hotmail.com>
2020-08-21 08:51:16 +0800

    Update 409_01.cpp

--- solution/409. Longest Palindrome/409_01.cpp
+++ solution/409. Longest Palindrome/409_01.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        int res = 0;
-		unordered_map<char, int> m;
-
-		for (int i = 0; i < s.size(); ++i) m[s[i]]++;
-
-		bool middle = false;
-		for (auto i : m) {
-			while (i.second > 1) {
-				res += 2;
-				i.second -= 2;
-			}
-			if (i.second == 1 && !middle) {
-				res += 1;
-				middle = true;
-			}
-		}
-		return res;
+        unordered_map<char, int> freq;
+        
+        for (const char& c : s) freq[c]++;
+        
+        int ans = 0, odd = 0;
+        for (auto& it : freq) {
+            if (it.second % 2 == 0) {
+                ans += it.second;
+            } else {
+                odd = 1;
+                ans += it.second - 1;
+            }
+        }
+        
+        return ans + odd;
     }
 };
