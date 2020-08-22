003bf462251d72d42c619605194ec5c6ee7f3778
Ray <rm-xp@hotmail.com>
2020-08-22 22:11:08 +0800

    Update 435_01.cpp

--- solution/435. Non-overlapping Intervals/435_01.cpp
+++ solution/435. Non-overlapping Intervals/435_01.cpp
@@ -1,49 +1,21 @@
-/**
- * Definition for an interval.
- * struct Interval {
- *     int start;
- *     int end;
- *     Interval() : start(0), end(0) {}
- *     Interval(int s, int e) : start(s), end(e) {}
- * };
- */
 class Solution {
 public:
-    int eraseOverlapIntervals(vector<Interval>& intervals) {
-        if (intervals.size() <= 1) return 0;
-        std::sort(intervals.begin(), intervals.end(), [](Interval& a, Interval& b) {
-            return a.end < b.end;
-        });
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        if (n <= 1) return 0;
         
-        int maxLen = 1, n = intervals.size();
-        int end = intervals[0].end;
-        for (int i = 1; i < n; ++i) {
-            if (end <= intervals[i].start) {
-                maxLen++;
-                end = intervals[i].end;
-            }
+        sort(intervals.begin(), intervals.end(), [](auto& a, auto& b) {
+            return a[1] < b[1];
+        });
+        int ans = 0;
+        
+        auto& prev = intervals[0];
+        for (int i = 1; i < n; i++) {
+            auto& next = intervals[i];
+            if (prev[1] <= next[0]) prev = next;
+            else ans++;
         }
-        return n - maxLen;
+        
+        return ans;
     }
 };
-
-int main() {
-    vector<Interval> intervals;
-    // intervals.push_back(Interval(1, 100)); // Expected: 2
-    // intervals.push_back(Interval(11, 22));
-    // intervals.push_back(Interval(1, 11));
-    // intervals.push_back(Interval(2, 12));
-    // intervals.push_back(Interval(1, 2)); // Expected: 1
-    // intervals.push_back(Interval(2, 3));
-    // intervals.push_back(Interval(3, 4));
-    // intervals.push_back(Interval(1, 3));
-    // intervals.push_back(Interval(0, 1)); // Expected: 0
-    // intervals.push_back(Interval(3, 4));
-    // intervals.push_back(Interval(1, 2));
-    intervals.push_back(Interval(0, 2)); // Expected: 2
-    intervals.push_back(Interval(1, 3));
-    intervals.push_back(Interval(2, 4));
-    intervals.push_back(Interval(3, 5));
-    intervals.push_back(Interval(4, 6));
-    cout << Solution().eraseOverlapIntervals(intervals) << endl;
-}
