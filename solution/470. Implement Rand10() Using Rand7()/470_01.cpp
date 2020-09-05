58de6582ad864b385cb20c07d9ea7dd113b14533
Ray <rm-xp@hotmail.com>
2020-09-05 22:08:31 +0800

    Create 470_01.cpp

---
+++ solution/470. Implement Rand10() Using Rand7()/470_01.cpp
@@ -0,0 +1,13 @@
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+
+class Solution {
+public:
+    int rand10() {
+        int index = INT_MAX;
+        while (index >= 40)
+            index = 7 * (rand7() - 1) + (rand7() - 1);
+        return index % 10 + 1;
+    }
+};
