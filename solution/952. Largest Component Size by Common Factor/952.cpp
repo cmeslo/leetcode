e157e7a44686cbfb4fce781c4151040730f8ceac
Ray <rm-xp@hotmail.com>
2020-09-07 13:59:31 +0800

    Create 952.cpp

---
+++ solution/952. Largest Component Size by Common Factor/952.cpp
@@ -0,0 +1,45 @@
+class DSU {
+public:
+    DSU(int n): _parents(n) {
+        for (int i = 0; i < n; i++)
+            _parents[i] = i;
+    }
+    
+    int Find(int x) {
+        if (x != _parents[x])
+            _parents[x] = Find(_parents[x]);
+        return _parents[x];
+    }
+    
+    void Union(int a, int b) {
+        _parents[Find(a)] = _parents[Find(b)];
+    }
+    
+private:
+    vector<int> _parents;
+};
+
+class Solution {
+public:
+    int largestComponentSize(vector<int>& A) {
+        int n = *max_element(A.begin(), A.end());
+        DSU dsu(n + 1);
+        
+        for (int a : A) {
+            int t= sqrt(a);
+            for (int k = 2; k <= t; ++k) {
+                if (a % k == 0) {
+                    dsu.Union(a, k);
+                    dsu.Union(a, a / k);
+                }
+            }
+        }
+        
+        unordered_map<int, int> c;
+        int ans = 1;
+        for (int a : A)
+            ans = max(ans, ++c[dsu.Find(a)]);
+        
+        return ans;
+    }
+};
