ef5ebddf17f91c6e92611302e0119363246b0ad7
Ray <rm-xp@hotmail.com>
2020-09-16 13:56:32 +0800

    Create 290.cpp

---
+++ solution/290. Word Pattern/290.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string str) {
+        unordered_map<char, int> m1;
+        unordered_map<string, int> m2;
+        istringstream iss(str);
+        
+        int i = 0, n = pattern.length();
+        for (string word; iss >> word; ++i) {
+            if (i == n || m1[pattern[i]] != m2[word])
+                return false;
+            m1[pattern[i]] = m2[word] = i + 1; // i + 1 to avoid default 0
+        }
+        
+        return i == n;
+    }
+};
