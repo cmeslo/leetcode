e6311816509527b3ef4d60b0b5476279569fd10d
Ray <rm-xp@hotmail.com>
2020-09-25 13:55:40 +0800

    Update 198_01.cpp

--- solution/198. House Robber/198_01.cpp
+++ solution/198. House Robber/198_01.cpp
@@ -1,37 +1,14 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
-	int rob(vector<int>& nums)
-	{
-		//d[i] = max(d[i-1], d[i-2] + a[i])
-
-		if (nums.size() == 0) return 0;
-		if (nums.size() == 1) return nums[0];
-
-		int prepre = nums[0];
-		int pre = max(nums[0], nums[1]);
-		int ans = pre;
-
-		for (int i = 2; i < nums.size(); ++i) {
-			int cur = max(pre, prepre + nums[i]);
-			ans = max(ans, cur);
-			prepre = pre;
-			pre = cur;
-		}
-		return ans;
-	}
+    int rob(vector<int>& nums) {
+        int ans = 0;
+        int pre2 = 0, pre1 = 0;
+        for (const int n : nums) {
+            ans = max(pre1, pre2 + n);
+            pre2 = pre1;
+            pre1 = ans;
+        }
+        
+        return ans;
+    }
 };
-
-void main()
-{
-	auto ans = new Solution;
-
-//	vector<int> test = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
-	vector<int> test = { 2, 3, 2, 4 };
-	cout << ans->rob(test) << endl;
-	
-}
