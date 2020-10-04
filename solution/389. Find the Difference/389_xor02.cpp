4724ac88050dfffd4a6f35a9b66caae57c458695
Ray <rm-xp@hotmail.com>
2020-10-04 16:03:49 +0800

    Update 389_xor02.cpp

--- solution/389. Find the Difference/389_xor02.cpp
+++ solution/389. Find the Difference/389_xor02.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        char ans = 0;
+        int ans = 0;
 		for (int i = 0; i < s.length(); ++i) {
-			ans ^= s[i];
-			ans ^= t[i];
+			ans ^= (s[i] - 'a');
+			ans ^= (t[i] - 'a');
 		}
-		ans ^= t[t.length() - 1];
-		return ans;
+		ans ^= t[t.length() - 1] - 'a';
+		return ans + 'a';
     }
 };
