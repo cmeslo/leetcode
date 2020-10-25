87f9e2daf04428d70f2447162436a639df051f40
Ray <rm-xp@hotmail.com>
2020-10-25 16:08:35 +0800

    Create 704.cpp

---
+++ solution/704. Binary Search/704.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if (nums.empty()) return -1;
+        
+        int l = 0, r = nums.size();
+        
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            
+            if (nums[mid] == target)
+                return mid;
+            
+            if (nums[mid] < target)
+                l = mid + 1;
+            else
+                r = mid;
+        }
+        
+        return -1;
+    }
+};
