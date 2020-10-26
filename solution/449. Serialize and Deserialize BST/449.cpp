ec49e1f6df056faa03aebb0b571ce2ef4b80127c
Ray <rm-xp@hotmail.com>
2020-10-26 19:55:41 +0800

    Create 449.cpp

---
+++ solution/449. Serialize and Deserialize BST/449.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        if (!root) return "x";
+        return to_string(root->val) + ' ' + serialize(root->left) + ' ' + serialize(root->right);
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        istringstream iss(data);
+        return deserializeHelper(iss);
+    }
+    
+private:
+    TreeNode* deserializeHelper(istringstream& iss) {
+        string val;
+        iss >> val;
+        if (val == "x") return nullptr;
+        
+        auto *root = new TreeNode(stoi(val));
+        root->left = deserializeHelper(iss);
+        root->right = deserializeHelper(iss);
+        return root;
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec* ser = new Codec();
+// Codec* deser = new Codec();
+// string tree = ser->serialize(root);
+// TreeNode* ans = deser->deserialize(tree);
+// return ans;
