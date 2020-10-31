e2f72c92fe28a07e0a1c1e41339acdc4802e7832
Ray <rm-xp@hotmail.com>
2020-11-01 00:48:03 +0800

    Update 213.cpp

--- solution/213. House Robber II/213.cpp
+++ solution/213. House Robber II/213.cpp
@@ -1,48 +1,25 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
-	int rob(vector<int>& nums)
-	{
-		if (nums.size() == 0) return 0;
-		if (nums.size() == 1) return nums[0];
-		if (nums.size() == 2) return max(nums[0], nums[1]);
-
-		int n = nums.size();
-
-		int a = rob(nums, 1, n - 1); //assuming that the first element will not be robbed
-		int b = rob(nums, 0, n - 2); //assuming that the last element will not be robbed
-		return max(a, b);
-	}
-
-	int rob(vector<int>& nums, int start, int end)
-	{
-		//d[i] = max(d[i-1], d[i-2] + a[i])
-
-		if (nums.size() == 0) return 0;
-		if (nums.size() == 1) return nums[start];
-
-		int prepre = nums[start];
-		int pre = max(nums[start], nums[start + 1]);
-		int ans = pre;
-
-		for (int i = start + 2; i <= end; ++i) {
-			int cur = max(pre, prepre + nums[i]);
-			ans = max(ans, cur);
-			prepre = pre;
-			pre = cur;
-		}
-		return ans;
-	}
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n <= 3)
+            return *max_element(begin(nums), end(nums));
+        else
+            return max(rob(nums, 0, n - 1), rob(nums, 1, n));
+    }
+    
+private:
+    int rob(vector<int>& nums, int start, int end) {
+        int pre = nums[start];
+        int last = max(pre, nums[start + 1]);
+        
+        int ans = 0;
+        for (int i = start + 2; i < end; ++i) {
+            ans = max(pre + nums[i], last);
+            pre = last;
+            last = ans;
+        }
+        
+        return ans;
+    }
 };
-
-void main()
-{
-	auto ans = new Solution;
-
-	vector<int> test = {1, 2, 3, 4, 5};
-	cout << ans->rob(test) << endl;
-}
