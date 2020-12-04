aebb1658192e14c337d67ff3061866b6057de7c2
Ray <rm-xp@hotmail.com>
2020-12-04 08:40:47 +0800

    Create 563.cpp

---
+++ solution/563. Binary Tree Tilt/563.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findTilt(TreeNode* root) {
+		int ans = 0;
+		postOrderTraversal(root, ans);
+		return ans;
+    }
+
+private:
+    int postOrderTraversal(TreeNode* root, int& ans) {
+		if (!root) return 0;
+		int leftSum = postOrderTraversal(root->left, ans);
+		int rightSum = postOrderTraversal(root->right, ans);
+		ans += abs(leftSum - rightSum);
+		return root->val + leftSum + rightSum;
+	}
+};
