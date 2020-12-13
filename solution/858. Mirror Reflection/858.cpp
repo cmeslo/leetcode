85579cc0b78f6f7f1bc8a602c716c5927a43a692
Ray <rm-xp@hotmail.com>
2020-12-14 07:36:56 +0800

    Create 858.cpp

---
+++ solution/858. Mirror Reflection/858.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int mirrorReflection(int p, int q) {
+        while (p % 2 == 0 && q % 2 == 0) {
+            p >>= 1, q >>= 1;
+        }
+        if (p % 2 == 0) return 2;
+        if (q % 2 == 0) return 0;
+        return 1;
+    }
+};
