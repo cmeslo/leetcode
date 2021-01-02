b2e3eff75a9c074f4f140a62828fe4515dc2c372
Ray <rm-xp@hotmail.com>
2021-01-02 10:09:22 +0800

    Create 1306.cpp

---
+++ solution/1306. Jump Game III/1306.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        int n = arr.size();
+        queue<int> q;
+        q.push(start);
+        
+        while (!q.empty()) {
+            int index = q.front(); q.pop();
+            int left = index - arr[index];
+            int right = index + arr[index];
+            
+            if (arr[index] == 0) return true;
+            
+            if (left >= 0 && arr[left] != -1) q.push(left);
+            if (right < n && arr[right] != -1) q.push(right);
+            
+            arr[index] = -1;
+        }
+        
+        return false;
+    }
+};
