dd1a04b5f34cf44593b2b1946ccae07d3209605a
Ray <rm-xp@hotmail.com>
2021-01-11 08:49:36 +0800

    Update 605.cpp

--- solution/605. Can Place Flowers/605.cpp
+++ solution/605. Can Place Flowers/605.cpp
@@ -1,44 +1,19 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-	bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-		int a = -1;
-		int length = flowerbed.size();
-		for (int i = 0; i < length; ++i) {
-			if (flowerbed[i] == 1) {
-				if (a == -1) 
-					n -= i / 2;
-				else
-					if (i - a > 3) n -= (i - a) / 3;
-				a = i;
-			}
-			else if (i == length - 1) {
-				if (a == -1) {
-					if (length % 2 == 0)
-						n -= length / 2;
-					else
-						n -= length / 2 + 1;
-				}
-				else {
-					n -= (i - a) / 2;
-				}
-			}
-		}
-		return  n <= 0;
-	}
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        if (n == 0) return true;
+        int size = flowerbed.size();
+        
+        for (int i = 0; i < size; ++i) {
+            int left = (i == 0) ? 0 : flowerbed[i - 1];
+            int right = (i == size - 1) ? 0 : flowerbed[i + 1];
+            
+            if (left || flowerbed[i] || right) continue;
+            
+            flowerbed[i++] = 1;
+            if (--n == 0) return true;
+        }
+        
+        return false;
+    }
 };
-
-void main()
-{
-	Solution *sol = new Solution;
-	//vector<int> test = { 1, 0, 0, 0, 1 }; //n = 1, expect: true
-	//vector<int> test = { 1, 0, 0, 0, 1 }; //n = 2, expect: false
-	//vector<int> test = { 1, 0, 1, 0, 0, 0, 1 }; //n = 2, expect: false
-	//vector<int> test = { 0, 0, 1, 0, 0, 0, 0 }; //n = 3, expect: true
-	vector<int> test = { 0, 0, 0, 0, 0, 0, 0, 0}; // n = 4, expect: true
-
-	cout << sol->canPlaceFlowers(test, 1) << endl;
-}
