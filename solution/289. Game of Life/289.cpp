ac52d73d451b2b1373a6eed209ce979a1de8443b
Ray <rm-xp@hotmail.com>
2021-02-21 19:05:13 +0800

    Update 289.cpp

--- solution/289. Game of Life/289.cpp
+++ solution/289. Game of Life/289.cpp
@@ -1,38 +1,23 @@
 class Solution {
 public:
     void gameOfLife(vector<vector<int>>& board) {
-        vector<vector<int>> old_board(board);
+        int h = board.size();
+        int w = h ? board[0].size() : 0;
         
-        int h = board.size(), w = board[0].size();
         for (int y = 0; y < h; ++y) {
             for (int x = 0; x < w; ++x) {
-                int lives = countLive(old_board, y, x);
-                if (old_board[y][x] == 1)
-                    board[y][x] = lives < 2 || lives > 3 ? 0 : 1;
-                else
-                    board[y][x] = lives == 3;
+                int lives = 0;
+                for (int i = max(y - 1, 0); i < min(y + 2, h); ++i)
+                    for (int j = max(x - 1, 0); j < min(x + 2, w); ++j)
+                        lives += board[i][j] & 1;
+                
+                if (lives == 3 || (lives == 4 && board[y][x] == 1))
+                    board[y][x] |= (1 << 1);
             }
         }
-    }
-
-private:
-    int countLive(vector<vector<int>>& board, int y, int x) {
-        int h = board.size(), w = board[0].size();
-        int res = 0;
         
-        if (y - 1 >= 0) {
-            if (x - 1 >= 0) res += board[y - 1][x - 1];
-            res += board[y - 1][x];
-            if (x + 1 < w) res += board[y - 1][x + 1];
-        }
-        if (x - 1 >= 0) res += board[y][x - 1];
-        if (x + 1 < w) res += board[y][x + 1];
-        if (y + 1 < h) {
-            if (x - 1 >= 0) res += board[y + 1][x - 1];
-            res += board[y + 1][x];
-            if (x + 1 < w) res += board[y + 1][x + 1];
-        }
-        
-        return res;
+        for (int y = 0; y < h; ++y)
+            for (int x = 0; x < w; ++x)
+                board[y][x] >>= 1;
     }
 };
