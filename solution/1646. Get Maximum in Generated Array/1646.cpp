5c0db8eccc0ef0b0ac9e16863edb1a05ed10ac2f
Ray <rm-xp@hotmail.com>
2021-03-04 07:06:19 +0800

    Create 1646.cpp

---
+++ solution/1646. Get Maximum in Generated Array/1646.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int getMaximumGenerated(int n) {
+        if (n == 0) return 0;
+        
+        vector<int> A(n + 1);
+        int ans = A[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            A[i] = A[i / 2] + (i % 2 ? A[i / 2 + 1] : 0);
+            ans = max(ans, A[i]);
+        }
+        return ans;
+    }
+};
