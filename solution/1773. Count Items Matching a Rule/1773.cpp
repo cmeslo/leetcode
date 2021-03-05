63319d9f7c3f8cf2a1abe91c0e9e802202ee6c36
Ray <rm-xp@hotmail.com>
2021-03-06 00:35:11 +0800

    Create 1773.cpp

---
+++ solution/1773. Count Items Matching a Rule/1773.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        int ans = 0;
+        for (auto& item : items) {
+            if ((ruleKey == "type" && item[0] == ruleValue)
+                || (ruleKey == "color" && item[1] == ruleValue)
+                || (ruleKey == "name" && item[2] == ruleValue))
+                ++ans;
+        }
+        return ans;
+    }
+};
