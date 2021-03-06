54bac5e45544d68174f2e407be7a7c451d7a1bb6
Ray <rm-xp@hotmail.com>
2021-03-07 00:17:31 +0800

    Create 127_01.cpp

---
+++ solution/127. Word Ladder/127_01.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> words(begin(wordList), end(wordList));
+        if (!words.count(endWord)) return 0;
+        
+        int ans = 0;
+        int len = beginWord.length();
+        
+        queue<string> q;
+        words.erase(beginWord);
+        q.push(beginWord);
+        
+        while (!q.empty()) {
+            ++ans;
+            int size = q.size();
+            while (size--) {
+                auto word = q.front(); q.pop();
+                for (int i = 0; i < len; ++i) {
+                    char tmp = word[i];
+                    for (char c = 'a'; c <= 'z'; ++c) {
+                        word[i] = c;
+                        if (word == endWord) return ans + 1;
+                        if (words.count(word)) {
+                            words.erase(word);
+                            q.push(word);
+                        }
+                    }
+                    word[i] = tmp;
+                }
+            }
+        }
+        
+        return 0;
+    }
+};
