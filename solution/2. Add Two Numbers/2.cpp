36ac6d0a42a58ecc3d47d95cf1786355c736d8ad
Ray <rm-xp@hotmail.com>
2021-03-11 07:13:59 +0800

    Update 2.cpp

--- solution/2. Add Two Numbers/2.cpp
+++ solution/2. Add Two Numbers/2.cpp
@@ -3,32 +3,25 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* sum = new ListNode(0);
-        ListNode* index = sum;
+        ListNode dummy(0);
+        auto *p = &dummy;
         int carry = 0;
-        
-        while (l1 != NULL || l2 != NULL) {
-            if (l1 != NULL) {
-                index->val += l1->val;
-                l1 = l1->next;
-            }
-            if (l2 != NULL) {
-                index->val += l2->val;
-                l2 = l2->next;
-            }
-            carry = index->val / 10;
-            index->val = index->val % 10;
-            if (l1 != NULL || l2 != NULL || carry != 0) {
-                index->next = new ListNode(carry);
-                index = index->next;
-            }
+        while (l1 || l2 || carry) {
+            int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+            p->next = new ListNode(sum % 10);
+            carry = sum / 10;
+            p = p->next;
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
         }
-        return sum;
+        return dummy.next;
     }
 };
