e78fb64582a3a80042a4ab0e3228edb5db74a7ec
Ray <rm-xp@hotmail.com>
2021-04-06 14:28:17 +0800

    Update 191.cpp

--- solution/191. Number of 1 Bits/191.cpp
+++ solution/191. Number of 1 Bits/191.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int bit = 0;
-		while (n) {
-			if ((n >> 1) << 1 != n) {
-				bit++;
-			}
-			n >>= 1;
-		}
-		return bit;
+        int ans = 0;
+        while (n) {
+            ++ans;
+            n &= n - 1;
+        }
+        return ans;
     }
 };
