6294aa560e6af2a2848b0c65007e5b98df4fd590
Ray <rm-xp@hotmail.com>
2021-04-13 19:43:54 +0800

    Update 284_01.cpp

--- solution/284. Peeking Iterator/284_01.cpp
+++ solution/284. Peeking Iterator/284_01.cpp
@@ -3,46 +3,46 @@
  * **DO NOT** modify the interface for Iterator.
  *
  *  class Iterator {
- *		struct Data;
- * 		Data* data;
- *		Iterator(const vector<int>& nums);
- * 		Iterator(const Iterator& iter);
+ *        struct Data;
+ *         Data* data;
+ *        Iterator(const vector<int>& nums);
+ *         Iterator(const Iterator& iter);
  *
- * 		// Returns the next element in the iteration.
- *		int next();
+ *         // Returns the next element in the iteration.
+ *        int next();
  *
- *		// Returns true if the iteration has more elements.
- *		bool hasNext() const;
- *	};
+ *        // Returns true if the iteration has more elements.
+ *        bool hasNext() const;
+ *    };
  */
 
 class PeekingIterator : public Iterator {
 public:
-	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
-	    // Initialize any member here.
-	    // **DO NOT** save a copy of nums and manipulate it directly.
-	    // You should only use the Iterator interface methods.
-	    hasNext_ = Iterator::hasNext();
+    PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+        // Initialize any member here.
+        // **DO NOT** save a copy of nums and manipulate it directly.
+        // You should only use the Iterator interface methods.
+        hasNext_ = Iterator::hasNext();
         if (hasNext_) val_ = Iterator::next();
-	}
-	
+    }
+    
     // Returns the next element in the iteration without advancing the iterator.
-	int peek() {
+    int peek() {
         return val_;
-	}
-	
-	// hasNext() and next() should behave the same as in the Iterator interface.
-	// Override them if needed.
-	int next() {
+    }
+    
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    int next() {
         int tmp = val_;
         hasNext_ = Iterator::hasNext();
         if (hasNext_) val_ = Iterator::next();
-	    return tmp;
-	}
-	
-	bool hasNext() const {
-	    return hasNext_;
-	}
+        return tmp;
+    }
+    
+    bool hasNext() const {
+        return hasNext_;
+    }
     
 private:
     int val_;
