06466f964ec51977363380108dfbab88a97495ae
Ray <rm-xp@hotmail.com>
2021-04-23 01:02:53 +0800

    Create 11.cpp

---
+++ solution/11. Container With Most Water/11.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0, r = height.size() - 1;
+        
+        int ans = 0;
+        while (l < r) {
+            int w = r - l;
+            int h = min(height[l], height[r]);
+            ans = max(ans, w * h);
+            if (height[l] < height[r])
+                ++l;
+            else
+                --r;
+        }
+        return ans;
+    }
+};
