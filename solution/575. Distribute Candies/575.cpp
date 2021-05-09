377d1593177bfbeb8b3d5f9aac3784621f004cfe
Ray <rm-xp@hotmail.com>
2021-05-09 20:33:37 +0800

    Create 575.cpp

---
+++ solution/575. Distribute Candies/575.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        unordered_set<int> types;
+        for (int type : candyType)
+            types.insert(type);
+        return min(types.size(), candyType.size() / 2);
+    }
+};
