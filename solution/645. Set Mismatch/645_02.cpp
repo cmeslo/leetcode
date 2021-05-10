5280473561a2e8d0d05a195d6e001268cca69025
Ray <rm-xp@hotmail.com>
2021-05-10 08:46:56 +0800

    Update 645_02.cpp

--- solution/645. Set Mismatch/645_02.cpp
+++ solution/645. Set Mismatch/645_02.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        vector<int> res(2, 0);
-        vector<int> cnt(nums.size(), 0);
+        int n = nums.size();
+        vector<int> m(n + 1);
+        for (int x : nums)
+            ++m[x];
         
-        for (int i = 0; i < nums.size(); ++i) {
-            cnt[nums[i] - 1]++;
+        vector<int> ans(2);
+        for (int x = 1; x <= n; ++x) {
+            if (m[x] == 2)
+                ans[0] = x;
+            if (m[x] == 0)
+                ans[1] = x;
+            if (ans[0] && ans[1])
+                return ans;
         }
-        
-        for (int i = 0; i < nums.size(); ++i) {
-            if (cnt[i] == 2) res[0] = i + 1;
-            if (cnt[i] == 0) res[1] = i + 1;
-            if (res[0] != 0 && res[1] != 0) return res;
-        }
-        return res;
+        return ans;
     }
 };
