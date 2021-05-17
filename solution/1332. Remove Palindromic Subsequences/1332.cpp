085c7081fb28f4c0c4531e3f1ab227b6166fc39c
Ray <rm-xp@hotmail.com>
2021-05-18 01:45:37 +0800

    Create 1332.cpp

---
+++ solution/1332. Remove Palindromic Subsequences/1332.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        if (s.empty()) return 0;
+        return isPalindrome(s) ? 1 : 2;
+    }
+    
+private:
+    bool isPalindrome(string& s) {
+        int l = 0, r = s.size() - 1;
+        while (l < r) {
+            if (s[l++] != s[r--])
+                return false;
+        }
+        return true;
+    }
+};
