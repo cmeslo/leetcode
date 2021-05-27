f2532afe7835c4df0c3fc2687462bf9a1fa4b6f8
Ray <rm-xp@hotmail.com>
2021-05-28 00:08:55 +0800

    Create 478.cpp

---
+++ solution/478. Generate Random Point in a Circle/478.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    Solution(double radius, double x_center, double y_center) {
+        r_ = radius;
+        x_ = x_center;
+        y_ = y_center;
+    }
+    
+    vector<double> randPoint() {
+        double x, y;
+        do {
+            double f1 = (double) rand() / RAND_MAX;
+            x = f1 * (2 * r_) - r_;
+            double f2 = (double) rand() / RAND_MAX;
+            y = f2 * (2 * r_) - r_;
+        } while (x * x + y * y > r_ * r_);
+        
+        return {x_ + x, y_ + y};
+    }
+    
+private:
+    double x_, y_;
+    double r_;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(radius, x_center, y_center);
+ * vector<double> param_1 = obj->randPoint();
+ */
