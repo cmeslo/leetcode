4f39e75e30f1eab346fd6724cda16581a6c7cde1
Ray <rm-xp@hotmail.com>
2021-05-31 08:43:07 +0800

    Create 376_02.cpp

---
+++ solution/376. Wiggle Subsequence/376_02.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int n = nums.size();
+        
+        int up = 1, down = 1;
+        for (int i = 1; i < n; ++i) {
+            if (nums[i - 1] > nums[i])
+                up = down + 1;
+            else if (nums[i - 1] < nums[i])
+                down = up + 1;
+        }
+        
+        return max(up, down);
+    }
+};
