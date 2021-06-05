697e2be524348cbf7af9a559456d9527ed3e3bfc
Ray <rm-xp@hotmail.com>
2021-06-05 11:10:26 +0800

    Update 869_02.cpp

--- solution/869. Reordered Power of 2/869_02.cpp
+++ solution/869. Reordered Power of 2/869_02.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool reorderedPowerOf2(int N) {
-        int res = counter(N);
+        long res = counter(N);
         for (int i = 0; i < 32; ++i) {
             if (counter(1 << i) == res)
                 return true;
@@ -10,8 +10,8 @@ public:
     }
     
 private:
-    int counter(int n) {
-        int res = 0;
+    long counter(int n) {
+        long res = 0;
         for (; n; n /= 10)
             res += pow(10, n % 10);
         return res;
