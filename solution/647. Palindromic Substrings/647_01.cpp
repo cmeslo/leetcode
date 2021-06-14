1abba7c2d95fd22d459ff06fe499474948fd9407
Ray <rm-xp@hotmail.com>
2021-06-14 15:37:13 +0800

    Create 647_01.cpp

---
+++ solution/647. Palindromic Substrings/647_01.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.size();
+        vector<vector<bool>> dp(n, vector<bool>(n));
+        
+        int ans = 0;
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i; j < n; ++j) {
+                if (j - i + 1 < 3)
+                    dp[i][j] = s[i] == s[j];
+                else
+                    dp[i][j] = s[i] == s[j] ? dp[i + 1][j - 1] : false;
+                ans += dp[i][j];
+            }
+        }
+        
+        return ans;
+    }
+};
