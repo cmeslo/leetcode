a358e55d94848989a5dc9dc81dcf62336a3885f7
Ray <rm-xp@hotmail.com>
2021-06-19 02:03:45 +0800

    Create 354_01.cpp

---
+++ solution/354. Russian Doll Envelopes/354_01.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        int n = envelopes.size();
+        sort(begin(envelopes), end(envelopes));
+        
+        int ans = 0;
+        vector<int> dp(n, 1);
+        for (int i = 0; i < n; ++i) {
+            for (int j = i - 1; j >= 0; --j) {
+                if (envelopes[j][0] < envelopes[i][0] && envelopes[j][1] < envelopes[i][1])
+                    dp[i] = max(dp[i], dp[j] + 1);
+            }
+            ans = max(ans, dp[i]);
+        }
+        
+        return ans;
+    }
+};
