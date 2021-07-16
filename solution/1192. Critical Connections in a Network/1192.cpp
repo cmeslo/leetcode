b44fab479b4f0102694ad88944ced36bfd348245
Ray <rm-xp@hotmail.com>
2021-07-16 14:18:28 +0800

    Create 1192.cpp

---
+++ solution/1192. Critical Connections in a Network/1192.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+        g.resize(n);
+        for (auto& conn : connections) {
+            g[conn[0]].push_back(conn[1]);
+            g[conn[1]].push_back(conn[0]);
+        }
+        
+        tags.resize(n, -1);
+        dfs(0, -1, 0);
+        return ans;
+    }
+    
+private:
+    vector<vector<int>> g;
+    vector<int> tags;
+    vector<vector<int>> ans;
+
+    int dfs(int cur, int parent, int tag) {
+        tags[cur] = tag;
+        
+        for (int next : g[cur]) {
+            if (next == parent)
+                continue;
+            else if (tags[next] == -1)
+                tags[cur] = min(tags[cur], dfs(next, cur, tag + 1));
+            else
+                tags[cur] = min(tags[cur], tags[next]);
+        }
+        
+        if (tags[cur] == tag && cur != 0)
+            ans.push_back({parent, cur});
+        return tags[cur];
+    }
+};
