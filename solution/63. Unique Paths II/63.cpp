234e0e68e78a74c3d84048d6c5207d0c0f99837f
Ray <rm-xp@hotmail.com>
2021-07-19 01:48:10 +0800

    Update 63.cpp

--- solution/63. Unique Paths II/63.cpp
+++ solution/63. Unique Paths II/63.cpp
@@ -1,34 +1,19 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        if (obstacleGrid.size() == 0 || obstacleGrid[0].size() == 0 || obstacleGrid[0][0] == 1) return 0;
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        if (obstacleGrid[0][0] == 1) return 0;
         
-        vector<vector<int>> dp(obstacleGrid.size(), vector<int>(obstacleGrid[0].size(), 0));
-        
-        for (int i = 0; i < obstacleGrid.size(); ++i)
-        {
-            for (int j = 0; j < obstacleGrid[i].size(); ++j)
-            {
-                if (obstacleGrid[i][j] == 1) continue;
-                if (i == 0 && j == 0) {
-                    dp[i][j] = 1;
-                } else if (i == 0 && j > 0) {
-                    dp[i][j] = dp[i][j - 1];
-                } else if (j == 0 && i > 0) {
-                    dp[i][j] = dp[i - 1][j];
-                } else {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-                }
+        vector<int> dp(n + 1, 0);
+        dp[1] = 1;
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (obstacleGrid[i - 1][j - 1])
+                    dp[j] = 0;
+                else
+                    dp[j] += dp[j - 1];
             }
         }
-        // for (int i = 0; i < obstacleGrid.size(); ++i) {
-        //     for (int j = 0; j < obstacleGrid[i].size(); ++j) {
-        //         cout << dp[i][j] << ", ";
-        //     }
-        //     cout << endl;
-        // }
-        
-        // return dp[obstacleGrid.size() - 1][obstacleGrid[0].size() - 1];
-        return dp.back().back();
+        return dp[n];
     }
 };
