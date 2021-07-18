3c257164b217d9a0cbc22cb2ff0c73a727f9a6ed
Ray <rm-xp@hotmail.com>
2021-07-18 10:22:52 +0800

    Update 326_01.cpp

--- solution/326. Power of Three/326_01.cpp
+++ solution/326. Power of Three/326_01.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         if (n == 0) return 0;
-		while (n % 3 == 0) {
-			n /= 3;
-		}
-		return n == 1;
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
     }
 };
