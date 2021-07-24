6f7afd87a940433464d65024a090d87a021fdb16
Ray <rm-xp@hotmail.com>
2021-07-24 10:50:50 +0800

    Create 630.cpp

---
+++ solution/630. Course Schedule III/630.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int scheduleCourse(vector<vector<int>>& courses) {
+        sort(courses.begin(), courses.end(), [&](auto& a, auto& b) {
+            return a[1] < b[1];
+        });
+        
+        int time = 0;
+        priority_queue<int> pq;
+        for (auto& c : courses) {
+            pq.push(c[0]);
+            time += c[0];
+            if (time > c[1]) {
+                time -= pq.top();
+                pq.pop();
+            }
+        }
+        return pq.size();
+    }
+};
