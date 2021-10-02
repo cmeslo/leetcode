b33d6724f6ce97819e55ebd4c22a57c8259cd549
Ray <rm-xp@hotmail.com>
2021-10-03 02:59:05 +0800

    Update 135_01.cpp

--- solution/135. Candy/135_01.cpp
+++ solution/135. Candy/135_01.cpp
@@ -2,35 +2,22 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> candies(n, 1);
-        int cnt = 0;
-
-        vector<vector<int>> sortedRatings(n);
-
+        
+        vector<pair<int, int>> ratings_(n);
+        for (int i = 0; i < n; ++i)
+            ratings_[i] = {ratings[i], i};
+        
+        vector<int> ans(n);
+        sort(begin(ratings_), end(ratings_));
         for (int i = 0; i < n; ++i) {
-            sortedRatings[i].push_back(ratings[i]);
-            sortedRatings[i].push_back(i);
+            auto [rating, index] = ratings_[i];
+            ans[index] = max(ans[index], 1);
+            if (index - 1 >= 0 && ratings[index - 1] > rating)
+                ans[index - 1] = max(ans[index - 1], ans[index] + 1);
+            if (index + 1 < n && ratings[index + 1] > rating)
+                ans[index + 1] = max(ans[index + 1], ans[index] + 1);
         }
-
-        sort(sortedRatings.begin(), sortedRatings.end(), [&](vector<int> a, vector<int> b) {
-            return a[0] < b[0];
-        });
-
-        for (int i = 1; i < n; ++i) {
-            int index = sortedRatings[i][1];
-            if (index - 1 >= 0) {
-                if (ratings[index] > ratings[index - 1]) {
-                    candies[index] = max(candies[index], candies[index - 1] + 1);
-                }
-            }
-            if (index + 1 < n) {
-                if (ratings[index] > ratings[index + 1]) {
-                    candies[index] = max(candies[index], candies[index + 1] + 1);
-                }
-            }
-        }
-
-        for (int c : candies) cnt += c;
-        return cnt;
+        
+        return accumulate(begin(ans), end(ans), 0);
     }
 };
