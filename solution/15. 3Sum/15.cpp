bbd846bb1e4adfbb753aff1b76fa5599c496ee2d
Ray <rm-xp@hotmail.com>
2021-10-04 01:43:44 +0800

    Update 15.cpp

--- solution/15. 3Sum/15.cpp
+++ solution/15. 3Sum/15.cpp
@@ -1,52 +1,25 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
-        if (nums.size() < 3) return ans;
-        
-        std::sort(nums.begin(), nums.end());
-        
-        for (int k = 0; k < nums.size() - 2; ++k) {
-            if (nums[k] > 0) break;
-            if (k > 0 && nums[k] == nums[k-1]) continue; // remove duplicate
-            int target = nums[k];
-            int i = k + 1, j = nums.size() - 1;
-            
-            while (i < j) {
-                int triple = nums[i] + nums[j] + target;
-                //std::cout << nums[i] << ", " << nums[j] << ", " << nums[k] << std::endl;
-                if (triple == 0) {
-                    ans.push_back({target, nums[i], nums[j]});
-                    while(nums[i] == nums[i + 1]) i++;
-                	while(nums[j] == nums[j - 1]) j--;
-                    i++; j--;
-                } else if (triple < 0) {
-                    i++;
-                } else {
-                    j--;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            if (i && nums[i - 1] == nums[i]) continue;
+            int j = i + 1, k = n - 1;
+            while (j < k) {
+                if (nums[j] + nums[k] < - nums[i])
+                    ++j;
+                else if (nums[j] + nums[k] > - nums[i])
+                    --k;
+                else {
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    while (j < k && nums[j] == nums[j + 1]) ++j;
+                    while (j < k && nums[k - 1] == nums[k]) --k;
+                    ++j, --k;
                 }
             }
         }
-        //std::cout << "================" << std::endl;
         return ans;
     }
 };
-
-int main() {
-    Solution* sol = new Solution;
-    
-    //vector<int> test = {-1, 0, 1, 2,-1 ,-4}; //-4, -1, -1, 0, 1, 2
-    //vector<int> test = {0, 0, 0};
-    vector<int> test = {-2, 0, 1, 1, 2};
-    vector<vector<int>> ans = sol->threeSum(test);
-    for (auto i : ans) {
-        for (auto j : i) {
-            std::cout << j << ", ";
-        }
-        std::cout<<endl;
-    }
-}
