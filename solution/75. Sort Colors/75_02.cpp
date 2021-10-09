920f9b868b4a902227a3591a3e780041fa35549b
Ray <rm-xp@hotmail.com>
2021-10-09 22:44:46 +0800

    Update 75_02.cpp

--- solution/75. Sort Colors/75_02.cpp
+++ solution/75. Sort Colors/75_02.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int red = 0, blue = nums.size()-1;
-        
-        for (int i = 0; i <= blue; i++) {
-            if (nums[i] == 0) {
-                swap(nums[i], nums[red++]);
-            } else if (nums[i] == 2) {
-                swap(nums[i--], nums[blue--]);
-            }
+        int l = 0, m = 0, r = nums.size() - 1;
+        while (m <= r) {
+            if (nums[m] == 0)
+                swap(nums[l++], nums[m++]);
+            else if (nums[m] == 2)
+                swap(nums[r--], nums[m]);
+            else
+                ++m;
         }
     }
 };
