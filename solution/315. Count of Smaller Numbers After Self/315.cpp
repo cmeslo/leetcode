7a2d669cdef0e700dd9b0e619bec502c1df45df0
Ray <rm-xp@hotmail.com>
2021-10-17 01:33:02 +0800

    Create 315.cpp

---
+++ solution/315. Count of Smaller Numbers After Self/315.cpp
@@ -0,0 +1,46 @@
+class FenwickTree {
+public:
+    FenwickTree(int n): sums_(n + 1) {};
+    
+    void update(int i, int delta) {
+        while (i < sums_.size()) {
+            sums_[i] += delta;
+            i += lowbit(i);
+        }
+    }
+    
+    int query(int i) {
+        int sum = 0;
+        while (i > 0) {
+            sum += sums_[i];
+            i -= lowbit(i);
+        }
+        return sum;
+    }
+    
+private:
+    static inline int lowbit(int x) {
+        return x & (-x);
+    }
+    
+    vector<int> sums_;
+};
+
+class Solution {
+public:
+    vector<int> countSmaller(vector<int>& nums) {
+        int n = nums.size();
+        auto minmax = minmax_element(nums.begin(), nums.end());
+        int mn = *minmax.first;
+        int mx = *minmax.second;
+        
+        vector<int> ans(n);
+        FenwickTree tree(mx + abs(mn));
+        int offset = abs(mn) + 1; // make it positive
+        for (int i = n - 1; i >= 0; --i) {
+            ans[i] = tree.query(nums[i] + offset - 1);
+            tree.update(nums[i] + offset, 1);
+        }
+        return ans;
+    }
+};
