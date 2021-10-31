e9df4c56422e82d746ece1cb7c9c822b873477c7
Ray <rm-xp@hotmail.com>
2021-10-31 19:56:49 +0800

    Create 2042.cpp

---
+++ solution/2042. Check if Numbers Are Ascending in a Sentence/2042.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool areNumbersAscending(string s) {
+        int pre = 0;
+        istringstream ss(s);
+        string str;
+        while (ss >> str) {
+            int i = 0;
+            for (; i < str.size() && isdigit(str[i]); ++i);
+            if (i == str.size()) {
+                int cur = stoi(str);
+                if (pre >= cur) return false;
+                pre = cur;
+            }
+        }
+        return true;
+    }
+};
