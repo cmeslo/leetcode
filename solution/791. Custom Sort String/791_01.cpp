47674fd2de5bae757f1275c6fff0c2af32861605
Ray <3932488+cmeslo@users.noreply.github.com>
2021-11-12 01:08:51 +0800

    Create 791_01.cpp

---
+++ solution/791. Custom Sort String/791_01.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string customSortString(string order, string str) {
+        vector<int> dict(128);
+        for (int i = 0; i < order.size(); ++i)
+            dict[order[i]] = i + 1;
+        
+        sort(str.begin(), str.end(), [&](char& a, char &b) {
+            return dict[a] < dict[b];
+        });
+        return str;
+    }
+};
