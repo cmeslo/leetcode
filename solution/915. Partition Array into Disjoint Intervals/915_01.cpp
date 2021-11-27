7dc7eeb14068389283ca6d85c47b133af8656e05
Ray <3932488+cmeslo@users.noreply.github.com>
2021-11-27 12:12:24 +0800

    Create 915_01.cpp

---
+++ solution/915. Partition Array into Disjoint Intervals/915_01.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int partitionDisjoint(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> left(n), right(n);
+        int mx = INT_MIN, mn = INT_MAX;
+        for (int i = 0; i < n; ++i) {
+            left[i] = mx = max(mx, nums[i]);
+            right[n - i - 1] = mn = min(mn, nums[n - i - 1]);
+        }
+        
+        for (int i = 0; i + 1 < n; ++i) {
+            if (left[i] <= right[i + 1])
+                return i + 1;
+        }
+        return -1;
+    }
+};
