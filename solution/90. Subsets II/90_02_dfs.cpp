821cd4d5c2a00ec1f5d6b85df39602d05d816f67
Ray <3932488+cmeslo@users.noreply.github.com>
2021-12-14 14:50:21 +0800

    Update 90_02_dfs.cpp

--- solution/90. Subsets II/90_02_dfs.cpp
+++ solution/90. Subsets II/90_02_dfs.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        int n = nums.size();
-        visited.resize(n);
-        
         sort(nums.begin(), nums.end());
-        
-        vector<int> cur;
-        dfs(nums, 0, cur);
-        return res;
+        vector<vector<int>> ans;
+        vector<int> path;
+        dfs(nums, 0, path, ans);
+        return ans;
     }
-
-private:
-    vector<vector<int>> res;
-    vector<bool> visited;
     
-    void dfs(vector<int>& nums, int start, vector<int>& cur) {
-        res.push_back(cur);
-        
-        for (int i = start; i < nums.size(); ++i) {
-            if (i > start && nums[i - 1] == nums[i]) continue;
-            visited[i] = true;
-            cur.push_back(nums[i]);
-            dfs(nums, i + 1, cur);
-            cur.pop_back();
-            visited[i] = false;
+private:
+    void dfs(vector<int>& nums, int s, vector<int>& path, vector<vector<int>>& ans) {
+        ans.push_back(path);
+        for (int i = s; i < nums.size(); ++i) {
+            if (i > s && nums[i - 1] == nums[i]) continue;
+            path.push_back(nums[i]);
+            dfs(nums, i + 1, path, ans);
+            path.pop_back();
         }
     }
 };
