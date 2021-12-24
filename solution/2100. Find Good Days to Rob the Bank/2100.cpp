b790654249b6a48c7da1a3e5a317697d2df06f19
Ray <3932488+cmeslo@users.noreply.github.com>
2021-12-24 17:13:10 +0800

    Update 2100.cpp

--- solution/2100. Find Good Days to Rob the Bank/2100.cpp
+++ solution/2100. Find Good Days to Rob the Bank/2100.cpp
@@ -6,14 +6,10 @@ public:
         for (int i = 1; i < n; ++i) {
             if (S[i - 1] >= S[i])
                 left[i] = left[i - 1] + 1;
-            else
-                left[i] = 0;
         }
         for (int i = n - 2; i >= 0; --i) {
             if (S[i] <= S[i + 1])
                 right[i] = right[i + 1] + 1;
-            else
-                right[i] = 0;
         }
         vector<int> res;
         for (int i = 0; i < n; ++i) {
