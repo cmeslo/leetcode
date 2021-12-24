733ed86d10a82091c22e3bdd6f8d9de9ac921f44
Ray <3932488+cmeslo@users.noreply.github.com>
2021-12-25 02:24:44 +0800

    Update 415.java

--- solution/415. Add Strings/415.java
+++ solution/415. Add Strings/415.java
@@ -1,17 +1,16 @@
 class Solution {
     public String addStrings(String num1, String num2) {
-        int s1 = num1.length() - 1, s2 = num2.length() - 1;
+        int i = num1.length() - 1, j = num2.length() - 1;
         int carry = 0;
         StringBuilder sb = new StringBuilder();
-        
-        while (s1 >= 0 || s2 >= 0) {
-            int n1 = s1 >= 0 ? num1.charAt(s1--) - '0' : 0;
-            int n2 = s2 >= 0 ? num2.charAt(s2--) - '0' : 0;
-            int sum = n1 + n2 + carry;
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int a = i >= 0 ? num1.charAt(i--) - '0' : 0;
+            int b = j >= 0 ? num2.charAt(j--) - '0' : 0;
+            int sum = a + b + carry;
             sb.append(sum % 10);
             carry = sum / 10;
         }
         String res = sb.reverse().toString();
-        return carry != 0 ? "1" + res : res;
+        return res;
     }
 }
