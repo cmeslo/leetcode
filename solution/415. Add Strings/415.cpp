4e45d0cb966617f2f8fcd8eb5753f84ccea94886
Ray <3932488+cmeslo@users.noreply.github.com>
2021-12-25 02:18:24 +0800

    Update 415.cpp

--- solution/415. Add Strings/415.cpp
+++ solution/415. Add Strings/415.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        int s1 = num1.size() - 1, s2 = num2.size() - 1;
         string res = "";
+        int i = num1.size() - 1, j = num2.size() - 1;
         int carry = 0;
-        
-        while (s1 >= 0 || s2 >= 0) {
-            int n1 = s1 >= 0 ? num1[s1--] - '0' : 0;
-            int n2 = s2 >= 0 ? num2[s2--] - '0' : 0;
-            int sum = n1 + n2 + carry;
-            res.insert(res.begin(), sum % 10 + '0');
+        while (i >= 0 || j >= 0 || carry) {
+            int a = i >= 0 ? num1[i--] - '0' : 0;
+            int b = j >= 0 ? num2[j--] - '0' : 0;
+            int sum = a + b + carry;
+            res += sum % 10 + '0';
             carry = sum / 10;
         }
-        return carry ? "1" + res : res;
+        reverse(res.begin(), res.end());
+        return res;
     }
 };
