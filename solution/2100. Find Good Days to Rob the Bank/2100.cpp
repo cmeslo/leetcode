ef6fe4849127ddd119fe57f2105b45e22813ca04
Ray <3932488+cmeslo@users.noreply.github.com>
2021-12-24 17:09:23 +0800

    Create 2100.cpp

---
+++ solution/2100. Find Good Days to Rob the Bank/2100.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> goodDaysToRobBank(vector<int>& S, int time) {
+        int n = S.size();
+        vector<int> left(n), right(n);
+        for (int i = 1; i < n; ++i) {
+            if (S[i - 1] >= S[i])
+                left[i] = left[i - 1] + 1;
+            else
+                left[i] = 0;
+        }
+        for (int i = n - 2; i >= 0; --i) {
+            if (S[i] <= S[i + 1])
+                right[i] = right[i + 1] + 1;
+            else
+                right[i] = 0;
+        }
+        vector<int> res;
+        for (int i = 0; i < n; ++i) {
+            if (left[i] >= time && right[i] >= time)
+                res.push_back(i);
+        }
+        return res;
+    }
+};
