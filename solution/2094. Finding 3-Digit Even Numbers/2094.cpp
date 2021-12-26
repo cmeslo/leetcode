b2bd3dc6b996ad85326672edf09db7eea0fe6c6c
Ray <3932488+cmeslo@users.noreply.github.com>
2021-12-26 18:39:37 +0800

    Create 2094.cpp

---
+++ solution/2094. Finding 3-Digit Even Numbers/2094.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        set<int> s;
+        int n = digits.size();
+        for (int i = 0; i < n; ++i) {
+            if (digits[i] == 0) continue;
+            for (int j = 0; j < n; ++j) {
+                for (int k = 0; k < n; ++k) {
+                    if (i == j || j == k || i == k) continue;
+                    if (digits[k] % 2) continue;
+                    s.insert(digits[i] * 100 + digits[j] * 10 + digits[k]);
+                }
+            }
+        }
+        return vector<int>(s.begin(), s.end());
+    }
+};
