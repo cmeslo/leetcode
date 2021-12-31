617e8b0578c3c00a628b3dafb56a60c138228ea4
Ray <3932488+cmeslo@users.noreply.github.com>
2022-01-01 03:05:35 +0800

    Update 954.cpp

--- solution/954. Array of Doubled Pairs/954.cpp
+++ solution/954. Array of Doubled Pairs/954.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
     bool canReorderDoubled(vector<int>& arr) {
-        int n = arr.size();
-        sort(arr.begin(), arr.end(), [&](int a, int b) {
-            if (a < 0 && b < 0)
-                return a > b;
-            return a < b;
-        });
-        
         unordered_map<int, int> m;
         for (int x : arr) ++m[x];
-        if (m[0] % 2) return false;
-        m.erase(0);
         
-        for (int x : arr) {
-            if (m[x] == 0) continue;
-            if (m.count(x * 2)) {
-                int mn = min(m[x], m[x * 2]);
-                m[x] -= mn;
-                m[x * 2] -= mn;
-            }
-        }
-        for (int x : arr)
-            if (m[x] != 0)
+        vector<int> keys;
+        for (auto it : m)
+            keys.push_back(it.first);
+        
+        sort(keys.begin(), keys.end(), [&](int a, int b) {
+            return abs(a) < abs(b);
+        });
+        
+        for (int x : keys) {
+            if (m[x] > m[x * 2])
                 return false;
+            m[x * 2] -= m[x];
+        }
         return true;
     }
 };
