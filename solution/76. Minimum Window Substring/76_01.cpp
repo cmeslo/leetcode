af5b7aba467941e966d13f05a1ba463f58b9f739
Ray <3932488+cmeslo@users.noreply.github.com>
2022-01-04 01:50:58 +0800

    Create 76_01.cpp

---
+++ solution/76. Minimum Window Substring/76_01.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char, int> dict;
+        for (char& c : t) dict[c]++;
+        
+        int n = s.size();
+        int start = 0, len = n + 1;
+        unordered_map<char, int> window;
+        for (int l = 0, r = 0; r < n; ++r) {
+            window[s[r]]++;
+            while (isOK(window, dict)) {
+                if (r - l + 1 < len) {
+                    start = l;
+                    len = r - l + 1;
+                }
+                window[s[l++]]--;
+            }
+        }
+        return len == n + 1 ? "" : s.substr(start, len);
+    }
+    
+private:
+    bool isOK(unordered_map<char, int>& window, unordered_map<char, int>& dict) {
+        for (auto& [c, cnt] : dict) {
+            if (!window.count(c)) return false;
+            if (window[c] < cnt) return false;
+        }
+        return true;
+    }
+};
