b13c4426e48cc7da9e4673b0f755b1273852fd76
Ray <3932488+cmeslo@users.noreply.github.com>
2022-01-06 01:18:10 +0800

    Update 303.cpp

--- solution/303. Range Sum Query - Immutable/303.cpp
+++ solution/303. Range Sum Query - Immutable/303.cpp
@@ -2,22 +2,21 @@ class NumArray {
 public:
     NumArray(vector<int>& nums) {
         int n = nums.size();
-        sums = vector<int>(n + 1, 0);
-        
+        presum_.resize(n + 1);
         for (int i = 0; i < n; ++i)
-            sums[i + 1] = sums[i] + nums[i];
+            presum_[i + 1] = presum_[i] + nums[i];
     }
     
-    int sumRange(int i, int j) {
-        return sums[j + 1] - sums[i];
+    int sumRange(int left, int right) {
+        return presum_[right + 1] - presum_[left];
     }
-
+    
 private:
-    vector<int> sums;
+    vector<int> presum_;
 };
 
 /**
  * Your NumArray object will be instantiated and called as such:
  * NumArray* obj = new NumArray(nums);
- * int param_1 = obj->sumRange(i,j);
+ * int param_1 = obj->sumRange(left,right);
  */
