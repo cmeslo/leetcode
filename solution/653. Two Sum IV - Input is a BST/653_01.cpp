67f70291fd34ab351f13b990bcb4de7ac5bafc87
Ray <3932488+cmeslo@users.noreply.github.com>
2022-01-17 02:11:28 +0800

    Create 653_01.cpp

---
+++ solution/653. Two Sum IV - Input is a BST/653_01.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        if (!root) return false;
+        if (pre.count(k - root->val))
+            return true;
+        pre.insert(root->val);
+        return findTarget(root->left, k) || findTarget(root->right, k);
+    }
+    
+private:
+    unordered_set<int> pre;
+};
