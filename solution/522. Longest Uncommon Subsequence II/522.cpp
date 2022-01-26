2436f8d6b03fccaa4149559d00166f73a74c1b2b
Ray <3932488+cmeslo@users.noreply.github.com>
2022-01-27 07:33:27 +0800

    Update 522.cpp

--- solution/522. Longest Uncommon Subsequence II/522.cpp
+++ solution/522. Longest Uncommon Subsequence II/522.cpp
@@ -1,61 +1,31 @@
 class Solution {
 public:
     int findLUSlength(vector<string>& strs) {
-        const int n = strs.size();
-        int ans = 0;
+        int n = strs.size();
+        sort(strs.begin(), strs.end(), [&](const string& a, const string& b) {
+            return a.size() > b.size();
+        });
+        
         for (int i = 0; i < n; ++i) {
-            auto subs = getSubsequence(strs[i]);
-            sort(subs.begin(), subs.end(), [&](string& a, string& b) { return a.size() > b.size(); });
-            
-            for (const string& sub : subs) {
-                int j;
-                for (j = 0; j < n; ++j) {
-                    if (sub.size() > strs[j].size()) continue;
-                    if (i == j) continue;
-                    if (isSubsequence(sub, strs[j])) break;
+            bool is_uncommon = true;
+            for (int j = 0; j < n; ++j) {
+                if (i == j) continue;
+                if (isSubsequence(strs[i], strs[j])) {
+                    is_uncommon = false;
+                    break;
                 }
-                if (j == n) ans = max(ans, (int)sub.size());
             }
-            cout << endl;
+            if (is_uncommon) return strs[i].size();
         }
-        return ans == 0 ? -1 : ans;
+        return -1;
     }
     
 private:
-    vector<string> getSubsequence(const string& str) {
-        unordered_set<string> subs;
-        int n = str.size();
-        int size = (1 << n);
-        for (int s = 0; s < size; ++s) {
-            string cur;
-            for (int i = n - 1; i >= 0; --i) {
-                if (s & (1 << i))
-                    cur.push_back(str[n - i - 1]);
-            }
-            subs.insert(cur);
-        }
-        vector<string> ans;
-        for (string sub : subs)
-            ans.push_back(sub);
-        return ans;
-    }
-    
-    bool isSubsequence(const string& sub, const string& str) {
+    bool isSubsequence(const string& a, const string& b) {
+        if (a.size() > b.size()) return false;
         int i = 0;
-        for (int j = 0; j < str.size(); ++j) {
-            if (sub[i] == str[j]) ++i;
-            if (i == sub.size()) return true;
-        }
-        return false;
+        for (int j = 0; i < a.size() && j < b.size(); ++j)
+            if (a[i] == b[j]) ++i;
+        return i == a.size();
     }
 };
-
-// 100
-// 50*50*100
-// 250000
-    
-// 2^10 = 1024
-
-// 1024 * 50 * 50
-// 1024 * 2500
-// 10^6
