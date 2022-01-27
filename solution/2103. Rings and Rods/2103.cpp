4534bc16e7e8208211db987aa9faa3d62749349b
Ray <3932488+cmeslo@users.noreply.github.com>
2022-01-28 01:09:36 +0800

    Create 2103.cpp

---
+++ solution/2103. Rings and Rods/2103.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countPoints(string rings) {
+        int n = 0;
+        for (int i = 1; i < rings.size(); i += 2)
+            n = max(n, rings[i] - '0');
+        n += 1;
+        
+        vector<vector<bool>> A(n, vector<bool>(3));
+        for (int i = 0; i < rings.size(); i += 2) {
+            int index = rings[i + 1] - '0';
+            int color = 0;
+            if (rings[i] == 'R')
+                color = 0;
+            else if (rings[i] == 'G')
+                color = 1;
+            else
+                color = 2;
+            A[index][color] = true;
+        }
+        int res = 0;
+        for (int i = 0; i < n; ++i) {
+            if (A[i][0] && A[i][1] && A[i][2])
+                ++res;
+        }
+        return res;
+    }
+};
