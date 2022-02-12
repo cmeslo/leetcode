a746d524001521d58fc9cc2cd882c982cb86933b
Ray <3932488+cmeslo@users.noreply.github.com>
2022-02-13 02:40:23 +0800

    Create 2164.cpp

---
+++ solution/2164. Sort Even and Odd Indices Independently/2164.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> sortEvenOdd(vector<int>& nums) {
+        vector<int> A, B;
+        int n = nums.size();
+        for (int i = 0; i < n; i += 2) {
+            A.push_back(nums[i]);
+            if (i + 1 < n)
+                B.push_back(nums[i + 1]);
+        }
+        sort(A.begin(), A.end());
+        sort(B.rbegin(), B.rend());
+        
+        vector<int> res(n);
+        int j = 0;
+        for (int i = 0; i < n; i += 2)
+            res[i] = A[j++];
+        j = 0;
+        for (int i = 1; i < n; i += 2)
+            res[i] = B[j++];
+        return res;
+    }
+};
