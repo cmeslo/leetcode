2a22f8ebbe2758105896565a65f050ede6570731
Ray <3932488+cmeslo@users.noreply.github.com>
2022-02-15 01:03:01 +0800

    Create 1879_01.cpp

---
+++ solution/1879. Minimum XOR Sum of Two Arrays/1879_01.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minimumXORSum(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        vector<int> dp(1 << m, INT_MAX / 2);
+        vector<int> dp2;
+        dp[0] = 0;
+        for (int j = 0; j < m; ++j) {
+            dp2 = dp;
+            for (int state = 0; state < (1 << m); ++state) {
+                for (int i = 0; i < m; ++i) {
+                    if ((state >> i) & 1) {
+                        dp[state] = min(dp[state], dp2[state - (1 << i)] + (nums1[i] ^ nums2[j]));
+                    }
+                }
+            }
+        }
+        return dp[(1 << m) - 1];
+    }
+};
