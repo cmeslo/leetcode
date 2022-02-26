0beccf73d499e396611a4ad36190d7631973cb03
Ray <3932488+cmeslo@users.noreply.github.com>
2022-02-27 01:14:22 +0800

    Update 446.cpp

--- solution/446. Arithmetic Slices II - Subsequence/446.cpp
+++ solution/446. Arithmetic Slices II - Subsequence/446.cpp
@@ -2,15 +2,30 @@ class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
         int n = nums.size();
-        int ans = 0;
-        vector<unordered_map<long, int>> dp(n);
+        unordered_map<int, vector<int>> pos;
+        for (int i = 0; i < n; ++i)
+            pos[nums[i]].push_back(i);
+        
+        vector<vector<int>> dp(n, vector<int>(n));
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < i; ++j) {
-                long diff = (long)nums[i] - nums[j];
-                dp[i][diff] += dp[j][diff] + 1;
-                ans += dp[j][diff];
+                long val = nums[j] * 2L - nums[i];
+                if (val < INT_MIN || val > INT_MAX) continue;
+                for (int k : pos[val]) {
+                    if (k < j)
+                        dp[j][i] += dp[k][j] + 1;
+                }
             }
         }
-        return ans;
+        
+        int res = 0;
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < i; ++j)
+                res += dp[j][i];
+        return res;
     }
 };
+
+// nums[k], nums[j], nums[i]
+// nums[k] - nums[j] = nums[j] - nums[i]
+// nums[k] = nums[j] + nums[j] - nums[i]
