5a2e1d1d2657aaf6efcfea22345c1d8d5b50a54e
Ray <3932488+cmeslo@users.noreply.github.com>
2022-03-02 01:23:48 +0800

    Create 1189.cpp

---
+++ solution/1189. Maximum Number of Balloons/1189.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        vector<int> f(26);
+        for (char& c : text)
+            ++f[c - 'a'];
+        
+        int ans = 0;
+        int b = 1;
+        int a = 0;
+        int l = 'l' - 'a';
+        int o = 'o' - 'a';
+        int n = 'n' - 'a';
+        while (f[b] && f[a] && f[l] >= 2 && f[o] >= 2 && f[n]) {
+            ++ans;
+            f[b]--;
+            f[a]--;
+            f[l] -= 2;
+            f[o] -= 2;
+            f[n]--;
+        }
+        return ans;
+    }
+};
