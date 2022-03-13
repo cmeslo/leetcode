5cbf405c13380ca9b0390f8503a4afd1784d4986
Ray <3932488+cmeslo@users.noreply.github.com>
2022-03-14 00:46:37 +0800

    Update 1239_01.cpp

--- solution/1239. Maximum Length of a Concatenated String with Unique Characters/1239_01.cpp
+++ solution/1239. Maximum Length of a Concatenated String with Unique Characters/1239_01.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     int maxLength(vector<string>& arr) {
-        vector<pair<int, int>> A; // {code, len}
-        for (string& x : arr) {
-            int code = 0;
-            for (char& c : x) {
-                int pos = 1 << (c - 'a');
-                if ((code & pos) != 0) { // 如果自己本身已經有重複，則不考慮
-                    code = 0;
-                    break;
-                }
-                code |= pos;
-            }
-            if (code) A.push_back({code, x.length()});
-        }
-        
         int ans = 0;
-        dfs(A, 0, 0, 0, ans);
+        dfs(arr, 0, 0, 0, ans);
         return ans;
     }
     
 private:
-    void dfs(vector<pair<int, int>> A, int start, int cur, int len, int& ans) {
+    void dfs(vector<string>& arr, int start, int curMask, int len, int& ans) {
         ans = max(ans, len);
-        for (int i = start; i < A.size(); ++i) {
-            if ((cur & A[i].first) == 0)
-                dfs(A, i + 1, cur | A[i].first, len + A[i].second, ans);
+        for (int i = start; i < arr.size(); ++i) {
+            int mask = concat(curMask, arr[i]);
+            if (mask != -1)
+                dfs(arr, i + 1, mask, len + arr[i].length(), ans);
         }
+    }
+    
+    int concat(int mask, const string& str) {
+        for (const char& c : str) {
+            int pos = 1 << (c - 'a');
+            if ((mask & pos) != 0) return -1;
+            mask |= pos;
+        }
+        return mask;
     }
 };
