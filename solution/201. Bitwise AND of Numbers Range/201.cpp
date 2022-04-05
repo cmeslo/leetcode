b3ea633ad952d149b7559d49ed77297c80b8e807
Ray <3932488+cmeslo@users.noreply.github.com>
2022-04-06 01:12:20 +0800

    Update 201.cpp

--- solution/201. Bitwise AND of Numbers Range/201.cpp
+++ solution/201. Bitwise AND of Numbers Range/201.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
-        int res = 0;
-        for (int i = 31; i >= 0; --i) {
-            int bitL = left & (1 << i);
-            int bitR = right & (1 << i);
-            
-            if (bitL && bitR)
-                res |= (1 << i);
-            else if (bitL || bitR)
-                break;
+        int i = 0;
+        while (left != right) {
+            left >>= 1;
+            right >>= 1;
+            ++i;
         }
-        return res;
+        return left << i;
     }
 };
