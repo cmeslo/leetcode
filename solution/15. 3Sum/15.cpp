b40bab650a516df8309be39c50b542eb4bc5877a
Ray <3932488+cmeslo@users.noreply.github.com>
2022-05-01 02:15:57 +0800

    Update 15.cpp

--- solution/15. 3Sum/15.cpp
+++ solution/15. 3Sum/15.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
-        sort(nums.begin(), nums.end());
         int n = nums.size();
-        for (int i = 0; i < n; ++i) {
-            if (i && nums[i - 1] == nums[i]) continue;
+        vector<vector<int>> res;
+        
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n - 2; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
             int j = i + 1, k = n - 1;
             while (j < k) {
-                if (nums[j] + nums[k] < - nums[i])
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < 0)
                     ++j;
-                else if (nums[j] + nums[k] > - nums[i])
+                else if (sum > 0)
                     --k;
                 else {
-                    ans.push_back({nums[i], nums[j], nums[k]});
-                    while (j < k && nums[j] == nums[j + 1]) ++j;
-                    while (j < k && nums[k - 1] == nums[k]) --k;
-                    ++j, --k;
+                    res.push_back({nums[i], nums[j++], nums[k--]});
+                    while (j < k && nums[j] == nums[j - 1]) ++j;
+                    while (j < k && nums[k] == nums[k + 1]) --k;
                 }
             }
         }
-        return ans;
+        return res;
     }
 };
