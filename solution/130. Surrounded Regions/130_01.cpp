516bcc207b16609ac54f3e0a06b0bdb7d0a42607
Ray <3932488+cmeslo@users.noreply.github.com>
2022-05-05 01:19:17 +0800

    Update 130_01.cpp

--- solution/130. Surrounded Regions/130_01.cpp
+++ solution/130. Surrounded Regions/130_01.cpp
@@ -1,49 +1,48 @@
 class Solution {
 public:
     void solve(vector<vector<char>>& board) {
-        if (board.size() == 0) return;
+        const vector<int> dirs = {0, -1, 0, 1, 0};
+        int m = board.size(), n = board[0].size();
         
-        int m = board.size();
-        int n = board[0].size();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!(i==0 || i==m-1 || j==0 || j==n-1)) continue; // not border
-                if (board[i][j] != 'O') continue;
-                
-                // bfs
-                queue<vector<int>> q;
-                q.push(vector<int>{i, j});
-                while (!q.empty()) {
-                    auto front = q.front(); q.pop();
-                    int x = front[0], y = front[1];
-                    board[x][y] = '@';
-                    
-                    // up
-                    if (x-1 >= 0 && board[x-1][y] == 'O') {
-                        board[x-1][y] = '@';
-                        q.push(vector<int>{x-1, y});
-                    }
-                    // down
-                    if (x+1 < m && board[x+1][y] == 'O') {
-                        board[x+1][y] = '@';
-                        q.push(vector<int>{x+1, y});
-                    }
-                    // left
-                    if (y-1 >= 0 && board[x][y-1] == 'O') {
-                        board[x][y-1] = '@';
-                        q.push(vector<int>{x, y-1});
-                    }
-                    // right
-                    if (y+1 < n && board[x][y+1] == 'O') {
-                        board[x][y+1] = '@';
-                        q.push(vector<int>{x, y+1});
-                    }
-                }
+        queue<pair<int, int>> q;
+        for (int i = 0; i < m; ++i) {
+            if (board[i][0] == 'O') {
+                board[i][0] = '#';
+                q.push({i, 0});
+            }
+            if (board[i][n - 1] == 'O') {
+                board[i][n - 1] = '#';
+                q.push({i, n - 1});
             }
         }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                board[i][j] = board[i][j]=='@'? 'O' : 'X';
+        for (int j = 0; j < n; ++j) {
+            if (board[0][j] == 'O') {
+                board[0][j] = '#';
+                q.push({0, j});
+            }
+            if (board[m - 1][j] == 'O') {
+                board[m - 1][j] = '#';
+                q.push({m - 1, j});
+            }
+        }
+        
+        while (!q.empty()) {
+            auto [i, j] = q.front(); q.pop();
+            for (int k = 0; k < 4; ++k) {
+                int y = i + dirs[k];
+                int x = j + dirs[k + 1];
+                if (y <= 0 || y >= m - 1 || x <= 0 || x >= n - 1 || board[y][x] != 'O') continue;
+                board[y][x] = '#';
+                q.push({y, x});
+            }
+        }
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (board[i][j] == 'O')
+                    board[i][j] = 'X';
+                else if (board[i][j] == '#')
+                    board[i][j] = 'O';
             }
         }
     }
