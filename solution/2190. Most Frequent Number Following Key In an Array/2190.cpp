51891bb1a2520de8d1146fa17f60674eab8ebc36
Ray <3932488+cmeslo@users.noreply.github.com>
2022-05-12 01:35:40 +0800

    Create 2190.cpp

---
+++ solution/2190. Most Frequent Number Following Key In an Array/2190.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mostFrequent(vector<int>& nums, int key) {
+        unordered_map<int, int> m;
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i - 1] == key)
+                m[nums[i]]++;
+        }
+        int ans = 0, cnt = 0;
+        for (auto [k, c] : m)
+            if (c > cnt) {
+                ans = k;
+                cnt = c;
+            }
+        return ans;
+    }
+};
