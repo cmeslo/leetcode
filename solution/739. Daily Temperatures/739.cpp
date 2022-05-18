8156007a94be7d179030ecc4be991bf686222f3c
Ray <3932488+cmeslo@users.noreply.github.com>
2022-05-19 02:17:29 +0800

    Create 739.cpp

---
+++ solution/739. Daily Temperatures/739.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& T) {
+        int n = T.size();
+        vector<int> res(n);
+        stack<int> st;
+        for (int i = 0; i < T.size(); ++i) {
+            while (!st.empty() && T[st.top()] < T[i]) {
+                res[st.top()] = i - st.top();
+                st.pop();
+            }
+            st.push(i);
+        }
+        return res;
+    }
+};
