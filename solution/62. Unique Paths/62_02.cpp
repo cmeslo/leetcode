3c37fca8dd48cb9c81cd0806282684da67f836b6
Ray <3932488+cmeslo@users.noreply.github.com>
2022-05-23 01:24:15 +0800

    Update 62_02.cpp

--- solution/62. Unique Paths/62_02.cpp
+++ solution/62. Unique Paths/62_02.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        if (m == 0 || n == 0) return 1;
-        
-        vector<int> dp(m*n, 0);
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int cur = i*m + j;
-                if (i == 0 || j == 0) {
-                    dp[cur] = 1; continue;
-                }
-                dp[cur] = dp[cur-m] + dp[cur-1];
+        vector<int> dp(n, 1);
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                dp[j] += dp[j - 1];
             }
         }
-        return dp[m*n-1];
+        return dp[n - 1];
     }
 };
