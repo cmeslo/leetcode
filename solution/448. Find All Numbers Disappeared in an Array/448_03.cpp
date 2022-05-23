9b5a5068fedbbfc5ee871c8fc4efec1a0b72fefd
Ray <3932488+cmeslo@users.noreply.github.com>
2022-05-24 01:59:10 +0800

    Update 448_03.cpp

--- solution/448. Find All Numbers Disappeared in an Array/448_03.cpp
+++ solution/448. Find All Numbers Disappeared in an Array/448_03.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            int index = abs(nums[i]) - 1;
+            if (nums[index] > 0)
+                nums[index] = -nums[index];
+        }
         vector<int> res;
-        
-        for (int i = 0; i < nums.size(); ++i) {
-            int j = abs(nums[i]) - 1;
-            if (nums[j] > 0) nums[j] = -nums[j];
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > 0)
+                res.push_back(i + 1);
         }
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] > 0) res.push_back(i + 1);
-        }
-        
         return res;
     }
 };
