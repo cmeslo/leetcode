4703f47a7e4412a8c808c3820a8b3b8ef760f62b
Ray <3932488+cmeslo@users.noreply.github.com>
2022-05-28 02:21:58 +0800

    Update 450_01.cpp

--- solution/450. Delete Node in a BST/450_01.cpp
+++ solution/450. Delete Node in a BST/450_01.cpp
@@ -14,23 +14,24 @@ public:
     TreeNode* deleteNode(TreeNode* root, int key) {
         if (!root) return nullptr;
         
-        if (key < root->val)
+        if (key < root->val) {
             root->left = deleteNode(root->left, key);
-        else if (root->val < key)
+        } else if (root->val < key) {
             root->right = deleteNode(root->right, key);
-        else if (root->val == key) {
+        } else if (root->val == key) {
             if (root->left && root->right) {
-                auto* min = root->right;
-                while (min->left) min = min->left;
-                root->val = min->val;
-                root->right = deleteNode(root->right, min->val);
+                auto mn = root->right;
+                while (mn->left)
+                    mn = mn->left;
+                root->val = mn->val;
+                root->right = deleteNode(root->right, mn->val);
             } else {
-                auto* new_root = root->left ? root->left : root->right;
-                root->left = root->right = nullptr;
+                auto new_root = root->left ? root->left : root->right;
                 delete root;
                 return new_root;
             }
         }
+        
         return root;
     }
 };
