e0aedd254c5c55674ae946207cf0d3ffa94fd14a
Ray <3932488+cmeslo@users.noreply.github.com>
2022-06-02 00:31:48 +0800

    Update 53.cpp

--- solution/53. Maximum Subarray/53.cpp
+++ solution/53. Maximum Subarray/53.cpp
@@ -1,27 +1,14 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
-	int maxSubArray(vector<int>& nums)
-	{
-		int sum = nums[0], ans = nums[0];
-
-		for (int i = 1; i < nums.size(); ++i) {
-			sum = max(sum + nums[i], nums[i]);
-			ans = max(sum, ans);
-		}
-		return ans;
-	}
+    int maxSubArray(vector<int>& nums) {
+        int ans, sum;
+        ans = sum = nums[0];
+        
+        for (int i = 1; i < nums.size(); i++) {
+            sum = max(sum + nums[i], nums[i]);
+            ans = max(ans, sum);
+        }
+        
+        return ans;
+    }
 };
-
-void main()
-{
-	auto ans = new Solution;
-
-	vector<int> test = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
-	cout << ans->maxSubArray(test) << endl;
-	
-}
