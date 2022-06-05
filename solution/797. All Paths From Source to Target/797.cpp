fd386968154ba575093f2fcb405316c07be4ac11
Ray <3932488+cmeslo@users.noreply.github.com>
2022-06-06 01:24:11 +0800

    Create 797.cpp

---
+++ solution/797. All Paths From Source to Target/797.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>> res;
+        vector<int> cur{0};
+        dfs(graph, 0, cur, res);
+        return res;
+    }
+    
+private:
+    void dfs(vector<vector<int>>& G, int node, vector<int>& cur, vector<vector<int>>& res) {
+        if (node == G.size() - 1) {
+            res.push_back(cur);
+            return;
+        }
+        
+        for (int next : G[node]) {
+            cur.push_back(next);
+            dfs(G, next, cur, res);
+            cur.pop_back();
+        }
+    }
+};
