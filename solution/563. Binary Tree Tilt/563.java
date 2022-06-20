0d8ea092037f77b70a645f5762e7b659d88525a8
Ray <3932488+cmeslo@users.noreply.github.com>
2022-06-21 01:59:29 +0800

    Create 563.java

---
+++ solution/563. Binary Tree Tilt/563.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findTilt(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+    
+    private int res = 0;
+    
+    private int dfs(TreeNode root) {
+        if (root == null) return 0;
+        int l = dfs(root.left);
+        int r = dfs(root.right);
+        res += Math.abs(l - r);
+        return root.val + l + r;
+    }
+}
