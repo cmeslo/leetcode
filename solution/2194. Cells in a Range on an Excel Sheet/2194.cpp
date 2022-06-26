4339632534a63bcfc278de559623213e093d6a6a
Ray <3932488+cmeslo@users.noreply.github.com>
2022-06-27 01:51:11 +0800

    Create 2194.cpp

---
+++ solution/2194. Cells in a Range on an Excel Sheet/2194.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<string> cellsInRange(string s) {
+        vector<string> ans;
+        for (int i = s[0] - 'A'; i <= s[3] - 'A'; ++i) {
+            for (int j = s[1] - '0'; j <= s[4] - '0'; ++j) {
+                ans.push_back(string(1, (i + 'A')) + string(1, (j + '0')));
+            }
+        }
+        return ans;
+    }
+};
