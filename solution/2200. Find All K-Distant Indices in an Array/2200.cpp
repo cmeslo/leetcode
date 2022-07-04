a05ac3427b59ba6d6eaef5ad9e837f328a7bd0b2
Ray <3932488+cmeslo@users.noreply.github.com>
2022-07-04 17:46:31 +0800

    Create 2200.cpp

---
+++ solution/2200. Find All K-Distant Indices in an Array/2200.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+        int n = nums.size();
+        vector<int> A;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == key)
+                A.push_back(i);
+        }
+        
+        vector<int> ans;
+        for (int i = 0; i < n; ++i) {
+            auto it = lower_bound(A.begin(), A.end(), i);
+            if (it != A.end() && abs(*it - i) <= k)
+                ans.push_back(i);
+            else if (it != A.begin() && abs(i - *prev(it)) <= k)
+                ans.push_back(i);
+        }
+        return ans;
+    }
+};
