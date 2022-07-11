37419cb0fd1016bb178626628eb3336e24e8e065
Ray <3932488+cmeslo@users.noreply.github.com>
2022-07-11 12:49:31 +0800

    Update 210.cpp

--- solution/210. Course Schedule II/210.cpp
+++ solution/210. Course Schedule II/210.cpp
@@ -1,48 +1,32 @@
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites)
-	{
-		vector<int> degrees(numCourses, 0);
-		vector<vector<int>> graph = makeGraph(numCourses, prerequisites, degrees);
-		vector<int> visited(numCourses, false);
-		queue<int> zeros;
-		vector<int> topoSort;
-
-
-		for (int i = 0; i < numCourses; ++i) {
-			if (degrees[i] == 0) {
-				zeros.push(i);
-			}
-		}
-
-		while(!zeros.empty()) {
-			int cur = zeros.front();
-			zeros.pop();
-			topoSort.push_back(cur);
-
-			for (int neigh : graph[cur]) {
-				degrees[neigh]--;
-				if (degrees[neigh] == 0) {
-					zeros.push(neigh);
-				}
-			}
-		}
-
-		for (int i : degrees) {
-			if (i != 0) 
-				return {};
-		}
-		return topoSort;
-    }
-	
-private:
-    vector<vector<int>> makeGraph(int numCourses, vector<pair<int, int>>& prerequisites, vector<int>& degrees)
-    {
-    	vector<vector<int>> graph(numCourses, vector<int>(0));
-    	for (auto pre : prerequisites) {
-    		graph[pre.second].push_back(pre.first);
-    		degrees[pre.first]++;
-    	}
-    	return graph;
+    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(n);
+        vector<int> inDegree(n);
+        for (auto& p : prerequisites) {
+            adj[p[1]].push_back(p[0]);
+            inDegree[p[0]]++;
+        }
+        
+        queue<int> q;
+        for (int i = 0; i < n; ++i)
+            if (inDegree[i] == 0)
+                q.push(i);
+        
+        vector<int> res;
+        while (!q.empty()) {
+            int node = q.front(); q.pop();
+            res.push_back(node);
+            for (int next : adj[node]) {
+                if (--inDegree[next] == 0)
+                    q.push(next);
+            }
+        }
+        
+        for (int i = 0; i < n; ++i)
+            if (inDegree[i] != 0)
+                return {};
+        
+        return res;
     }
 };
