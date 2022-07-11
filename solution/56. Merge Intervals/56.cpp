500d9675a948ea489b6f4e51a6e9e5c3b3427ce3
Ray <3932488+cmeslo@users.noreply.github.com>
2022-07-12 00:06:09 +0800

    Update 56.cpp

--- solution/56. Merge Intervals/56.cpp
+++ solution/56. Merge Intervals/56.cpp
@@ -1,27 +1,14 @@
-/**
- * Definition for an interval.
- * struct Interval {
- *     int start;
- *     int end;
- *     Interval() : start(0), end(0) {}
- *     Interval(int s, int e) : start(s), end(e) {}
- * };
- */
 class Solution {
 public:
-    vector<Interval> merge(vector<Interval>& intervals) {
-        if (intervals.empty()) return vector<Interval>{};
-
-		sort(intervals.begin(), intervals.end(), [](Interval a, Interval b) { return a.start < b.start; });
-		vector<Interval> res{ intervals.at(0) };
-		for (int i = 1; i < intervals.size(); ++i) {
-			if (res.back().end >= intervals[i].start) {
-				res.back().end = max(res.back().end, intervals[i].end);
-			}
-			else {
-				res.push_back(intervals[i]);
-			}
-		}
-		return res;
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> res;
+        for (auto& interval : intervals) {
+            if (!res.empty() && res.back()[1] >= interval[0])
+                res.back()[1] = max(res.back()[1], interval[1]);
+            else
+                res.push_back(interval);
+        }
+        return res;
     }
 };
