63be93df79eb959eed1d4138779c0743e93f8909
Ray <3932488+cmeslo@users.noreply.github.com>
2022-07-19 01:58:14 +0800

    Update 116_01.cpp

--- solution/116. Populating Next Right Pointers in Each Node/116_01.cpp
+++ solution/116. Populating Next Right Pointers in Each Node/116_01.cpp
@@ -1,29 +1,36 @@
-/**
- * Definition for binary tree with next pointer.
- * struct TreeLinkNode {
- *  int val;
- *  TreeLinkNode *left, *right, *next;
- *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
- * };
- */
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
 class Solution {
 public:
-    void connect(TreeLinkNode *root) {
-        int level = 1;
-        queue<TreeLinkNode*> q;
-        if (root != NULL) q.push(root);
-
+    Node* connect(Node* root) {
+        if (!root) return root;
+        
+        queue<Node*> q;
+        q.push(root);
         while (!q.empty()) {
-            for (int i = 0; i < level; ++i) {
-                if (q.empty()) break;
-                TreeLinkNode* cur = q.front();
-                q.pop();
-
-                if (cur->left != NULL) q.push(cur->left);
-                if (cur->right != NULL) q.push(cur->right);
-                cur->next = (!q.empty() && i != level - 1) ? q.front() : NULL;
+            for (int i = q.size(); i > 0; --i) {
+                auto cur = q.front(); q.pop();
+                cur->next = i != 1 ? q.front(): nullptr;
+                if (cur->left) q.push(cur->left);
+                if (cur->right) q.push(cur->right);
             }
-            level <<= 1;
         }
+        return root;
     }
 };
