5a5f34ce2da8cb2316b76ff929587983f4b54931
Ray <3932488+cmeslo@users.noreply.github.com>
2022-07-22 18:21:06 +0800

    Create 2201.cpp

---
+++ solution/2201. Count Artifacts That Can Be Extracted/2201.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int digArtifacts(int n, vector<vector<int>>& artifacts, vector<vector<int>>& dig) {
+        vector<vector<int>> M(1001, vector<int>(1001, -1));
+        unordered_map<int, int> m;
+        for (int i = 0; i < artifacts.size(); ++i) {
+            auto& a = artifacts[i];
+            int y1 = a[0], x1 = a[1], y2 = a[2], x2 = a[3];
+            int len = 0;
+            for (int y = y1; y <= y2; ++y) {
+                for (int x = x1; x <= x2; ++x) {
+                    M[y][x] = i;
+                    len++;
+                }
+            }
+            m[i] = len;
+        }
+        
+        int ans = 0;
+        unordered_map<int, int> count;
+        for (auto& d : dig) {
+            int y = d[0], x = d[1];
+            int color = M[y][x];
+            if (++count[color] == m[color])
+                ans++;
+        }
+        return ans;
+    }
+};
