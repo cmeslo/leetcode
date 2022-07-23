614371b360166388e013d5c29091c0db188b64a2
Ray <3932488+cmeslo@users.noreply.github.com>
2022-07-24 02:36:10 +0800

    Update 1009_02.cpp

--- solution/1009. Complement of Base 10 Integer/1009_02.cpp
+++ solution/1009. Complement of Base 10 Integer/1009_02.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
-    int bitwiseComplement(int N) {
+    int bitwiseComplement(int n) {
         int x = 1;
-        while (x < N)
+        while (x < n)
             x = (x << 1) | 1;
         
-        return x ^ N;
+        return x ^ n;
     }
 };
+
+
+// 0 0 0 1 0 1 <-- n
+
+// 0 0 0 1 1 1 <-- xor this
+
+// 0 0 0 0 1 0
