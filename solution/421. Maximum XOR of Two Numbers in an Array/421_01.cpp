60e6240fb11108222597835093714416b395ba06
Ray <3932488+cmeslo@users.noreply.github.com>
2022-08-09 16:36:47 +0800

    Update 421_01.cpp

--- solution/421. Maximum XOR of Two Numbers in an Array/421_01.cpp
+++ solution/421. Maximum XOR of Two Numbers in an Array/421_01.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int findMaximumXOR(vector<int>& nums) {
-        int max = 0, mask = 0;
+        int res = 0, mask = 0;
         
         for (int i = 31; i >= 0; --i) {
-            mask |= (1 << i);
+            mask |= 1 << i;
+            unordered_set<int> prefix;
             
-            unordered_set<int> s;
-            for (int n : nums) {
-                s.insert(n & mask); // reserve Left bits and ignore Right bits
-            }
-            
-            int tmp = max | (1 << i); // in each iteration, there are pair(s) whoes Left bits can XOR to max
-            for (int prefix : s) {
-                if (s.find(prefix ^ tmp) != s.end()) {
-                    max = tmp;
+            int expect = res | (1 << i);
+            for (int x : nums) {
+                int a = mask & x; // expect = a ^ b, --> b = expect ^ a
+                if (prefix.count(expect ^ a)) { // 當我確實可以得到 expect
+                    res = expect;
                     break;
                 }
+                prefix.insert(a);
             }
         }
-        return max;
+        
+        return res;
     }
 };
