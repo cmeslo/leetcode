3d015fbe1a5868e477c7d90d92c9bba609163523
Ray <3932488+cmeslo@users.noreply.github.com>
2022-08-22 02:36:44 +0800

    Update 532_02.cpp

--- solution/532. K-diff Pairs in an Array/532_02.cpp
+++ solution/532. K-diff Pairs in an Array/532_02.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
         unordered_map<int, int> m;
+        for (int x : nums)
+            m[x]++;
         
-        for (int n : nums)
-            ++m[n];
-        
-        int ans = 0;
-        for (auto it : m)
-            if ((k == 0 && it.second > 1) || (k > 0 && m.count(it.first + k)))
-                ++ans;
-        
-        return ans;
+        int res = 0;
+        for (auto& [x, count] : m) {
+            if ((k == 0 && count > 1) || (k > 0 && m.count(x + k)))
+                res++;
+        }
+        return res;
     }
 };
