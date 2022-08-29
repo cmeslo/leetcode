8aee37b9460bd29b7cf671471601c749cef2d634
Ray <3932488+cmeslo@users.noreply.github.com>
2022-08-30 01:50:17 +0800

    Update 402.cpp

--- solution/402. Remove K Digits/402.cpp
+++ solution/402. Remove K Digits/402.cpp
@@ -5,16 +5,17 @@ public:
         int targetSize = n - k;
         
         string res;
-        for (int i = 0; i < n; ++i) {
-            while (!res.empty() && res.back() > num[i] && n - i + res.size() > targetSize)
+        for (char& c : num) {
+            while (!res.empty() && res.back() > c && k) {
                 res.pop_back();
-            if (res.size() < targetSize)
-                res.push_back(num[i]);
+                k--;
+            }
+            if (!res.empty() || c != '0')
+                res.push_back(c);
         }
 
-        int i = 0;
-        while (i < res.size() && res[i] == '0') i++;
-        return i == res.size() ? "0" : res.substr(i);
+        while (res.size() && k--) res.pop_back();
+        return res.empty() ? "0" : res;
     }
 };
 
