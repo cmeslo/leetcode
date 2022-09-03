fe8ea546c3f6fae4eb0af1ce5dc5c2a2a385e8e8
Ray <3932488+cmeslo@users.noreply.github.com>
2022-09-03 11:43:58 +0800

    Update 171.cpp

--- solution/171. Excel Sheet Column Number/171.cpp
+++ solution/171. Excel Sheet Column Number/171.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
-    int titleToNumber(string s) {
+    int titleToNumber(string columnTitle) {
         int ans = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            ans = ans * 26 + (s[i] - 'A' + 1);
-        }
-        
+        for (char& x : columnTitle)
+            ans = ans * 26 + (x - 'A' + 1);
         return ans;
     }
 };
+
+
+// 1  2  3  ... 26
+// A  B  C  ... Z
+
+// 27 28 29 ... 52
+// AA AB AC ... AZ
+
+// 53 53 55 ... 78
+// BA BB BC ... BZ
+
+// AAA
