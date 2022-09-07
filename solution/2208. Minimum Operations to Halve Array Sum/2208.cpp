6cfc2960760d6d1a9fa1df4563976ddf4a1e23c4
Ray <3932488+cmeslo@users.noreply.github.com>
2022-09-08 01:42:27 +0800

    Create 2208.cpp

---
+++ solution/2208. Minimum Operations to Halve Array Sum/2208.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int halveArray(vector<int>& nums) {
+        using ll = long long;
+        double sum = 0;
+        priority_queue<double> pq;
+        for (int x : nums) {
+            sum += x;
+            pq.push(x);
+        }
+        
+        int ans = 0;
+        double target = sum / 2;
+        while (sum > target) {
+            double x = pq.top(); pq.pop();
+            sum = sum - x + x / 2;
+            pq.push(x / 2);
+            ++ans;
+        }
+        return ans;
+    }
+};
