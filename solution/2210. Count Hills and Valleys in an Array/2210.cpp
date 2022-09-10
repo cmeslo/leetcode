790d6f6b2799049ff397278a20a790e655350ec7
Ray <3932488+cmeslo@users.noreply.github.com>
2022-09-10 22:43:29 +0800

    Update 2210.cpp

--- solution/2210. Count Hills and Valleys in an Array/2210.cpp
+++ solution/2210. Count Hills and Valleys in an Array/2210.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int countHillValley(vector<int>& nums) {
-        vector<int> A;
-        A.push_back(nums[0]);
-        for (int i = 1; i < nums.size(); ++i) {
-            if (nums[i - 1] == nums[i]) continue;
-            A.push_back(nums[i]);
-        }
-        
         int res = 0;
-        for (int i = 1; i < A.size() - 1; ++i) {
-            if ((A[i - 1] < A[i] and A[i] > A[i + 1])
-               or (A[i - 1] > A[i] and A[i] < A[i + 1]))
-                ++res;
+        for (int i = 0, j = 1; j < nums.size() - 1; ++j) {
+            if ((nums[i] < nums[j] && nums[j] > nums[j + 1]) ||
+                (nums[i] > nums[j] && nums[j] < nums[j + 1])) {
+                   ++res;
+                   i = j;
+               }
         }
         return res;
     }
