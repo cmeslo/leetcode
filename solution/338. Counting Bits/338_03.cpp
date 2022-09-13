3b1a1e2fb0e26a7c493d795af63186cd5c42a720
Ray <3932488+cmeslo@users.noreply.github.com>
2022-09-14 01:36:29 +0800

    Update 338_03.cpp

--- solution/338. Counting Bits/338_03.cpp
+++ solution/338. Counting Bits/338_03.cpp
@@ -1,35 +1,10 @@
-// Runtime: 4 ms
-// Your runtime beats 98.88 % of cpp submissions.
-
 class Solution {
 public:
-    vector<int> countBits(int num) {
-        if (num == 0) return vector<int>{0};
-        vector<int> res {0};
-        
-        for (int i = 1; i <= num; i++) {
-            if (i % 2 != 0) {
-                res.push_back(res[i-1]+1);
-            } else {
-                res.push_back(res[i/2]);
-            }
+    vector<int> countBits(int n) {
+        vector<int> ans(n + 1);
+        for (int i = 0; i <= n; ++i) {
+            ans[i] = i % 2 ? ans[i - 1] + 1 : ans[i / 2];
         }
-        return res;
+        return ans;
     }
 };
-/*
-class Solution {
-public:
-    vector<int> countBits(int num) {
-        vector<int> res(num+1, 0);        
-        for (int i = 1; i <= num; i++) {
-            if (i % 2 != 0) {
-                res[i] = res[i-1]+1;
-            } else {
-                res[i] = res[i/2];
-            }
-        }
-        return res;
-    }
-};
-*/
