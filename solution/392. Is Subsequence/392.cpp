8fcfe9a100e7555835765c0566cfebe07728eed4
Ray <3932488+cmeslo@users.noreply.github.com>
2022-09-16 01:49:31 +0800

    Update 392.cpp

--- solution/392. Is Subsequence/392.cpp
+++ solution/392. Is Subsequence/392.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        if (s.size() == 0) return true;
-        
-        int s_idx = 0, t_idx = 0;
-        while (t_idx < t.size()) {
-            if (s[s_idx] == t[t_idx]) s_idx++;
-            if (s_idx >= s.size()) return true;
-            t_idx++;
+        int i = 0, j = 0;
+        while (i < s.size() && j < t.size()) {
+            if (s[i] == t[j]) i++;
+            j++;
         }
-        return false;
+        return i == s.size();
     }
 };
