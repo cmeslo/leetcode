5be7b2e937d38002fd52c40f93c56329c2ebbbcd
Ray <3932488+cmeslo@users.noreply.github.com>
2022-10-02 01:53:16 +0800

    Update 81_02.cpp

--- solution/81. Search in Rotated Sorted Array II/81_02.cpp
+++ solution/81. Search in Rotated Sorted Array II/81_02.cpp
@@ -4,24 +4,24 @@ public:
         int l = 0, r = nums.size() - 1;
         
         while (l <= r) {
-            int mid = l + ((r - l) >> 1);
-            if (nums[mid] == target) return true;
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) return true;
             
             // 前半段有序：
-            if (nums[l] < nums[mid]) {
-                if (nums[l] <= target && target < nums[mid])
-                    r = mid - 1;
+            if (nums[l] < nums[m]) {
+                if (nums[l] <= target && target < nums[m])
+                    r = m - 1;
                 else
-                    l = mid + 1;
+                    l = m + 1;
             // 後半段有序：
-            } else if (nums[mid] < nums[r]) {
-                if (nums[mid] < target && target <= nums[r])
-                    l = mid + 1;
+            } else if (nums[m] < nums[r]) {
+                if (nums[m] < target && target <= nums[r])
+                    l = m + 1;
                 else
-                    r = mid - 1;
+                    r = m - 1;
             // 不知道 前半 還是 後半 有序，一點點縮小範圍：
             } else {
-                if (nums[l] == nums[mid])
+                if (nums[l] == nums[m])
                     ++l;
                 else
                     --r;
