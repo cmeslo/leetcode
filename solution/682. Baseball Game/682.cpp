75841e79d3074aa3760391eb81b4b9ecd0ba45cb
Ray <3932488+cmeslo@users.noreply.github.com>
2022-10-15 18:13:28 +0800

    Create 682.cpp

---
+++ solution/682. Baseball Game/682.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int calPoints(vector<string>& ops) {
+        vector<int> A;
+        for (string& op : ops) {
+            if (op == "+")
+                A.push_back(A[A.size() - 2] + A[A.size() - 1]);
+            else if (op == "D")
+                A.push_back(A.back() * 2);
+            else if (op == "C")
+                A.pop_back();
+            else
+                A.push_back(stoi(op));
+        }
+        return accumulate(A.begin(), A.end(), 0);
+    }
+};
