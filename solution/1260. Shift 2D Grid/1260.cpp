0c7679c969f9111e5fbb462cf1a8340545b02e5b
Ray <3932488+cmeslo@users.noreply.github.com>
2022-10-16 03:49:15 +0800

    Create 1260.cpp

---
+++ solution/1260. Shift 2D Grid/1260.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        auto M = grid;
+        int m = grid.size(), n = grid[0].size();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int pos = i * n + j;
+                int pos_new = (pos + k) % (m * n);
+                int y = pos_new / n;
+                int x = pos_new % n;
+                M[y][x] = grid[i][j];
+            }
+        }
+        return M;
+    }
+};
+
+// i, j -> i * n + j
+// 0, 0 -> 1, 0
+
+// 0 -> 3
+// i*n+j -> i*n+j + k
