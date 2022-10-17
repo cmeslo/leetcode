4affeb196048a08268561e6e6bff96b361f7821f
Ray <3932488+cmeslo@users.noreply.github.com>
2022-10-18 01:17:31 +0800

    Update 59.cpp

--- solution/59. Spiral Matrix II/59.cpp
+++ solution/59. Spiral Matrix II/59.cpp
@@ -2,28 +2,19 @@ class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
         vector<vector<int>> ans(n, vector<int>(n));
-        int rowStart = 0, rowEnd = n - 1;
-        int colStart = 0, colEnd = n - 1;
-        int num = 1;
-        
-        while (rowStart <= rowEnd && colStart <= colEnd) {
-            for (int i = colStart; i <= colEnd; ++i)
-                ans[rowStart][i] = num++;
-            ++rowStart;
-            
-            for (int i = rowStart; i <= rowEnd; ++i)
-                ans[i][colEnd] = num++;
-            --colEnd;
-            
-            for (int i = colEnd; i >= colStart; --i)
-                ans[rowEnd][i] = num++;
-            --rowEnd;
-            
-            for (int i = rowEnd; i >= rowStart; --i)
-                ans[i][colStart] = num++;
-            ++colStart;
+        vector<vector<int>> dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int d = 0;
+        int y = 0, x = -1;
+        for (int i = 1; i <= n * n; ++i) {
+            int next_y = y + dirs[d][0];
+            int next_x = x + dirs[d][1];
+            // 因為下一步會走出邊界、或者已經走過，所以要轉方向
+            if (next_y < 0 || next_y >= n || next_x < 0 || next_x >= n|| ans[next_y][next_x] != 0)
+                d = (d + 1) % 4;
+            y = y + dirs[d][0];
+            x = x + dirs[d][1];
+            ans[y][x] = i;
         }
-        
         return ans;
     }
 };
