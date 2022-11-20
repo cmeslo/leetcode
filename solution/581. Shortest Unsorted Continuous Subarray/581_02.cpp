8e622b9b06abe1e278b6e6ee350ea7062fd71243
Ray <3932488+cmeslo@users.noreply.github.com>
2022-11-21 03:00:20 +0800

    Update 581_02.cpp

--- solution/581. Shortest Unsorted Continuous Subarray/581_02.cpp
+++ solution/581. Shortest Unsorted Continuous Subarray/581_02.cpp
@@ -2,26 +2,31 @@ class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
         int n = nums.size();
-        int l = n, r = -1;
-        stack<int> st;
-        for (int i = 0; i < n; ++i) {
-            while (!st.empty() && nums[st.top()] > nums[i]) {
-                l = min(l, st.top());
-                st.pop();
-            }
-            st.push(i);
-        }
-        if (st.size() == n) return 0;
+        int l = -1, r = -2;
+        int mn = INT_MAX, mx = INT_MIN;
         
-        while (!st.empty()) st.pop();
+        for (int i = 0; i < n; ++i) {
+            mx = max(mx, nums[i]);
+            if (mx > nums[i])
+                r = i;
+        }
         
         for (int i = n - 1; i >= 0; --i) {
-            while (!st.empty() && nums[i] > nums[st.top()]) {
-                r = max(r, st.top());
-                st.pop();
-            }
-            st.push(i);
+            mn = min(mn, nums[i]);
+            if (mn < nums[i])
+                l = i;
         }
+        
         return r - l + 1;
     }
 };
+
+// 這種做法與 stack 的原理其實是一樣的:
+
+// 我用 正向遞增 monotnoic stack 是找到第一個不合理的位置 i
+// 這個不合理的 i 其實就是指: 在這個 i 後面存在一個比 nums[i] 更小的數字
+// 尋找時要由後向前找
+
+// 我用 反向遞減 monotnoic stack 是找到最後一個不合理的位置 i
+// 這個不合理的 i 其實就是指: 在這個 i 前面存在一個比 nums[i] 更大的數字
+// 尋找時要由前向後找
