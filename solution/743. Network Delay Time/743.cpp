852e71140fff4a5187d73049fb08cd3656c7c868
Ray <3932488+cmeslo@users.noreply.github.com>
2022-11-29 01:01:45 +0800

    Update 743.cpp

--- solution/743. Network Delay Time/743.cpp
+++ solution/743. Network Delay Time/743.cpp
@@ -4,29 +4,25 @@ public:
         using pii = pair<int, int>;
         vector<vector<pii>> adj(n + 1);
         for (auto& t : times) {
-            adj[t[0]].push_back({t[1], t[2]});
+            adj[t[0]].emplace_back(t[1], t[2]);
         }
         
         priority_queue<pii, vector<pii>, greater<>> pq;
         vector<int> dist(n + 1, INT_MAX);
         dist[k] = 0;
-        pq.push({0, k});
+        pq.emplace(0, k);
+        
+        int ans = 0;
         while (!pq.empty()) {
             auto [d, node] = pq.top(); pq.pop();
             if (d > dist[node]) continue;
+            ans = max(ans, dist[node]);
+            if (--n == 0) return ans;
             for (auto& [next, w] : adj[node]) {
                 if (w + d >= dist[next]) continue;
-                dist[next] = w + d;
-                pq.push({w + d, next});
+                pq.emplace(dist[next] = w + d, next);
             }
         }
-        
-        int ans = 0;
-        for (int i = 1; i <= n; ++i) {
-            if (dist[i] == INT_MAX)
-                return -1;
-            ans = max(ans, dist[i]);
-        }
-        return ans;
+        return -1;
     }
 };
