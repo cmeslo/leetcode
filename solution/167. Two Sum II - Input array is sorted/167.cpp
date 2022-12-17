1f7db0777589dca7fbcbc2aaeb6c0ca25467c1b1
Ray <3932488+cmeslo@users.noreply.github.com>
2022-12-18 02:22:14 +0800

    Update 167.cpp

--- solution/167. Two Sum II - Input array is sorted/167.cpp
+++ solution/167. Two Sum II - Input array is sorted/167.cpp
@@ -1,39 +1,14 @@
-#include <iostream>
-#include <vector>
-#include <stack>
-using namespace std;
-
 class Solution {
 public:
-	vector<int> twoSum(vector<int>& numbers, int target)
-	{
-		vector<int> ans;
-		vector<int>::iterator left, right;
-		left = numbers.begin();
-		right = numbers.end() - 1;
-
-		while (*left + *right != target) {
-			if (*left + *right < target) {
-				left++;
-			}
-			else {
-				right--;
-			}
-		}
-		ans.push_back(left - numbers.begin() + 1);
-		ans.push_back(right - numbers.begin() + 1);
-		return ans;
-	}
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0, r = numbers.size() - 1;
+        while (l < r) {
+            int sum = numbers[l] + numbers[r];
+            if (sum == target) break;
+            else if (sum < target) l++;
+            else if (target < sum) r--;
+        }
+        
+        return {l + 1, r + 1};
+    }
 };
-
-void main()
-{
-	auto ans = new Solution;
-	//vector<int> numbers = { 2, 7, 11, 15 };
-	vector<int> numbers = { 2, 3, 4 };
-
-	vector<int> indexs = ans->twoSum(numbers, 6);
-
-	cout << indexs.at(0) << ", " << indexs.at(1) << endl;
-	
-}
