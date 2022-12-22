1c9727c11eae0a28c5b2399fc01fcc00d17dd267
Ray <3932488+cmeslo@users.noreply.github.com>
2022-12-22 21:33:07 +0800

    Create 2196.cpp

---
+++ solution/2196. Create Binary Tree From Descriptions/2196.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_set<int> leaf;
+        unordered_map<int, pair<int, int>> m;
+        for (auto& d : descriptions) {
+            int parent = d[0], child = d[1], isLeft = d[2];
+            if (isLeft)
+                m[parent].first = child;
+            else
+                m[parent].second = child;
+            leaf.insert(child);
+        }
+        
+        // find root;
+        int rootVal = 0;
+        for (auto& d : descriptions) {
+            if (!leaf.count(d[0])) {
+                rootVal = d[0];
+                break;
+            }
+        }
+        
+        TreeNode* root = new TreeNode(rootVal);
+        dfs(root, m);
+        return root;
+    }
+    
+    void dfs(TreeNode* node, unordered_map<int, pair<int, int>>& m) {
+        if (!m.count(node->val)) return;
+        
+        if (m[node->val].first) {
+            node->left = new TreeNode(m[node->val].first);
+            dfs(node->left, m);
+        }
+        if (m[node->val].second) {
+            node->right = new TreeNode(m[node->val].second);
+            dfs(node->right, m);
+        }
+    }
+};
