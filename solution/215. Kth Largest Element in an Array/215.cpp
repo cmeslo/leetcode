34fc6b31904a159775e754774cdc7d1a603ffee8
Ray <3932488+cmeslo@users.noreply.github.com>
2022-12-27 03:04:39 +0800

    Update 215.cpp

--- solution/215. Kth Largest Element in an Array/215.cpp
+++ solution/215. Kth Largest Element in an Array/215.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        int l = 0, r = nums.size() - 1;
         --k;
+        int l = 0, r = nums.size() - 1;
         while (l < r) {
-            int pivotPos = partition(nums, l, r);
-            if (pivotPos == k) return nums[k];
-            l = pivotPos < k ? pivotPos + 1 : l;
-            r = pivotPos < k ? r : pivotPos - 1;
+            int pos = partition(nums, l, r);
+            if (pos == k) return nums[k];
+            if (pos < k)
+                l = pos + 1;
+            else
+                r = pos - 1;
         }
         return nums[k];
     }
     
 private:
     int partition(vector<int>& nums, int l, int r) {
-        int pos = rand() % (r - l + 1) + l;
-        int pivot = nums[pos];
+        int rnd = rand() % (r - l + 1) + l;
+        int pivot = nums[rnd];
         
-        swap(nums[pos], nums[r]);
-        int i = l - 1;
+        swap(nums[rnd], nums[r]);
+        int i = l;
         for (int j = l; j < r; ++j) {
-            if (nums[j] >= pivot)
-                swap(nums[++i], nums[j]);
+            if (nums[j] > pivot)
+                swap(nums[i++], nums[j]);
         }
-        swap(nums[i + 1], nums[r]);
-        return i + 1;
+        swap(nums[i], nums[r]);
+        return i;
     }
 };
