f152ad83a91a45f6736ce1cc36142bbaeacb58be
Ray <3932488+cmeslo@users.noreply.github.com>
2023-01-03 01:51:08 +0800

    Update 462_01.cpp

--- solution/462. Minimum Moves to Equal Array Elements II/462_01.cpp
+++ solution/462. Minimum Moves to Equal Array Elements II/462_01.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     int minMoves2(vector<int>& nums) {
-        sort(begin(nums), end(nums));
-        
         int n = nums.size();
-        vector<long> prefix(n);
-        prefix[0] = nums[0];
-        for (int i = 1; i < n; ++i)
-            prefix[i] = prefix[i - 1] + nums[i];
+        sort(nums.begin(), nums.end());
+        
+        vector<int> presum(n + 1);
+        for (int i = 1; i <= n; ++i)
+            presum[i] = presum[i - 1] + nums[i - 1];
         
         long ans = INT_MAX;
-        for (int i = 0; i < n; ++i) {
-            long moves = 0;
-            long left = (i == 0 ? 0 : prefix[i - 1]);
-            long right = (i == n - 1 ? 0 : prefix[n - 1] - prefix[i]);
-            moves += (i == 0 ? 0 : (long) i * nums[i]) - left;
-            moves += right - (i == n - 1 ? 0 : (long) (n - i - 1) * nums[i]);
-            ans = min(ans, moves);
+        for (int i = 1; i <= n; ++i) {
+            long left = 1L * nums[i - 1] * (i - 1) - presum[i - 1];
+            long right = presum[n] - presum[i] - 1L * (n - i) * nums[i - 1];
+            ans = min(ans, left + right);
         }
         return ans;
     }
