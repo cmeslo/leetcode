4d76ac3a1ded06b470c94f055ca4815b817e47d5
Ray <3932488+cmeslo@users.noreply.github.com>
2023-01-06 00:39:46 +0800

    Update 135_02.cpp

--- solution/135. Candy/135_02.cpp
+++ solution/135. Candy/135_02.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int sum = 0;
-        int size = ratings.size();
-        vector<int> candies(size, 1);
-        
-        for (int i = 0; i < size - 1; ++i) {
-            if (ratings[i] < ratings[i + 1]) candies[i + 1] = candies[i] + 1;
+        int n = ratings.size();
+        vector<int> A(n, 1);
+        for (int i = 1; i < n; ++i) {
+            if (ratings[i - 1] < ratings[i])
+                A[i] = A[i - 1] + 1;
         }
-        for (int i = size - 1; i > 0; --i) {
-            if (ratings[i - 1] > ratings[i]) candies[i - 1] = max(candies[i - 1], candies[i] + 1);
+        for (int i = n - 2; i >= 0; --i) {
+            if (ratings[i] > ratings[i + 1])
+                A[i] = max(A[i], A[i + 1] + 1);
         }
-        
-        for (int c : candies) sum += c;
-        return sum;
+        return accumulate(A.begin(), A.end(), 0);
     }
 };
