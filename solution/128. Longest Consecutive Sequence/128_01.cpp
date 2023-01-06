e730d935f16f960f9eafd728379bd96d138a3d58
Ray <3932488+cmeslo@users.noreply.github.com>
2023-01-07 00:11:20 +0800

    Update 128_01.cpp

--- solution/128. Longest Consecutive Sequence/128_01.cpp
+++ solution/128. Longest Consecutive Sequence/128_01.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if (nums.size() <= 1) return nums.size();
-        
         set<int> s;
-        for (int x : nums) s.insert(x);
+        for (int x : nums)
+            s.insert(x);
         
-        int ans = 1, cnt = 1;
-        auto it = s.begin(); ++it;
-        for (; it != s.end(); ++it) {
-            if (*prev(it) + 1 == *it) ++cnt;
-            else cnt = 1;
-            ans = max(ans, cnt);
+        int res = 0, len = 0;
+        int pre = INT_MIN;
+        for (int x : s) {
+            len = pre + 1 == x ? len + 1 : 1;
+            pre = x;
+            res = max(res, len);
         }
-        return ans;
+        return res;
     }
 };
