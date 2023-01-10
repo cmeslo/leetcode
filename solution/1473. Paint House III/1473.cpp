61aaa8f06d2e43dcb5374d0812718d5d50d54d16
Ray <3932488+cmeslo@users.noreply.github.com>
2023-01-11 00:33:17 +0800

    Update 1473.cpp

--- solution/1473. Paint House III/1473.cpp
+++ solution/1473. Paint House III/1473.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int minCost(vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target) {
         int dp[101][101][21];
-        // vector<vector<vector<int>>> dp(m + 1, vector<vector<int>>(target + 1, vector<int>(n + 1, INT_MAX / 2)));
         for (int i = 0; i <= m; ++i)
             for (int j = 0; j <= target; ++j)
                 for (int k = 0; k <= n; ++k)
@@ -17,19 +16,35 @@ public:
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= target; ++j) {
                 if (houses[i] == 0) {
-                    for (int k = 1; k <= n; ++k) {
-                        for (int kk = 1; kk <= n; ++kk) {
-                            if (k == kk)
-                                dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j][k] + cost[i][k - 1]);
-                            else
-                                dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j - 1][kk] + cost[i][k - 1]);
-                        }
+                    vector<pair<int, int>> pre; // {cost, color}
+                    for (int kk = 1; kk <= n; ++kk) {
+                        pre.push_back({dp[i - 1][j - 1][kk], kk});
                     }
+                    sort(pre.begin(), pre.end());
+                    
+                    for (int k = 1; k <= n; ++k) {
+                        // consider i-1 and i houses painted same color
+                        dp[i][j][k] = dp[i - 1][j][k] + cost[i][k - 1];
+                        
+                        // consider i-1 and i houses painted different color
+                        if (k != pre[0].second)
+                            dp[i][j][k] = min(dp[i][j][k], pre[0].first + cost[i][k - 1]);
+                        else
+                            dp[i][j][k] = min(dp[i][j][k], pre[1].first + cost[i][k - 1]);
+                    }
+                    // for (int k = 1; k <= n; ++k) {
+                    //     for (int kk = 1; kk <= n; ++kk) {
+                    //         if (k == kk)
+                    //             dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j][k] + cost[i][k - 1]);
+                    //         else
+                    //             dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j - 1][kk] + cost[i][k - 1]);
+                    //     }
+                    // }
                 } else {
                     int k = houses[i];
                     for (int kk = 1; kk <= n; ++kk) {
                         if (k == kk)
-                            dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j][k]);
+                            dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j][kk]);
                         else
                             dp[i][j][k] = min(dp[i][j][k], dp[i - 1][j - 1][kk]);
                     }
