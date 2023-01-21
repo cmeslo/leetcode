177800c2d8a8a8f149990e7f242c79190567cdf7
Ray <3932488+cmeslo@users.noreply.github.com>
2023-01-21 18:07:06 +0800

    Update 118.cpp

--- solution/118. Pascal's Triangle/118.cpp
+++ solution/118. Pascal's Triangle/118.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> ans{{1}};
-
-        for (int n = 2; n <= numRows; ++n) {
-            vector<int> nums(n);
-            for (int i = 0; i < n; ++i) {
-                int left = i == 0 ? 0 : ans.back()[i - 1];
-                int right = i == n - 1 ? 0 : ans.back()[i];
-                nums[i] = left + right;
+        vector<vector<int>> res(numRows);
+        
+        for (int i = 0; i < numRows; ++i) {
+            vector<int> cur(i + 1);
+            cur[0] = cur[i] = 1;
+            for (int j = 1; j < i; ++j) {
+                cur[j] += res[i - 1][j - 1] + res[i - 1][j];
             }
-            ans.push_back(nums);
+            res[i] = cur;
         }
-        return ans;
+        
+        return res;
     }
 };
+
+// 0 1 2 3
+// 1
+// 1 1
+// 1 2 1
+// 1 3 3 1
+
+// res[i][j] = res[i - 1][j - 1] + res[i - 1][j]
