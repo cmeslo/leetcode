8a1b52db122c751d30b694e91f9b769b4e439867
Ray <3932488+cmeslo@users.noreply.github.com>
2023-01-26 00:04:40 +0800

    Update 890_02.cpp

--- solution/890. Find and Replace Pattern/890_02.cpp
+++ solution/890. Find and Replace Pattern/890_02.cpp
@@ -3,15 +3,20 @@ public:
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
         vector<string> ans;
         string p = normalize(pattern);
-        for (string& w : words) if (normalize(w) == p) ans.push_back(w);
+        for (string& w : words)
+            if (normalize(w) == p)
+                ans.push_back(w);
         return ans;
     }
     
 private:
     string normalize(string s) {
         unordered_map<char, int> m;
-        for (char& c : s) if (m.find(c) == m.end()) m[c] = m.size();
-        for (int i = 0; i < s.size(); ++i) s[i] = 'a' + m[s[i]];
+        for (char& c : s)
+            if (m.find(c) == m.end())
+                m[c] = m.size();
+        for (int i = 0; i < s.size(); ++i)
+            s[i] = 'a' + m[s[i]];
         return s;
     }
 };
