aa92e40ba8eafa81b6dfa5c928a3884386b4bed8
Ray <3932488+cmeslo@users.noreply.github.com>
2023-02-13 02:25:15 +0800

    Update 417_01.cpp

--- solution/417. Pacific Atlantic Water Flow/417_01.cpp
+++ solution/417. Pacific Atlantic Water Flow/417_01.cpp
@@ -1,62 +1,50 @@
 class Solution {
 public:
-    vector<vector<int>> pacificAtlantic(vector<vector<int>>& matrix) {
-        if (matrix.empty()) return {};
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        m = heights.size(), n = heights[0].size();
+        mark.resize(m, vector<int>(n));
         
-        int m = matrix.size(), n = matrix[0].size();
-        const vector<int> dirs = {0, -1, 0, 1, 0};
-        vector<vector<int>> visited(m, vector<int>(n)); // 1: Pacific visited, 2: Atlantic visited
+        for (int j = 0; j < n; ++j) {
+            bfs(heights, 0, j, 0); // Pacific Ocean
+            bfs(heights, m - 1, j, 1); // Atlantic Ocean
+        }
+        for (int i = 0; i < m; ++i) {
+            bfs(heights, i, 0, 0); // Pacific Ocean
+            bfs(heights, i, n - 1, 1); // Atlantic Ocean
+        }
         
-        // Pacific BFS
+        vector<vector<int>> res;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (mark[i][j] == 3)
+                    res.push_back({i, j});
+            }
+        }
+        return res;
+    }
+    
+private:
+    int m, n;
+    vector<vector<int>> mark;
+    const vector<int> dirs = {0, -1, 0, 1, 0};
+    
+    void bfs(vector<vector<int>>& H, int i, int j, int flag) {
+        if (mark[i][j] & (1 << flag)) return;
+        
         queue<pair<int, int>> q;
-        for (int i = 0; i < m; ++i) {
-            visited[i][0] = 1;
-            q.emplace(i, 0);
-        }
-        for (int j = 1; j < n; ++j) {
-            visited[0][j] = 1;
-            q.emplace(0, j);
-        }
+        mark[i][j] |= (1 << flag);
+        q.emplace(i, j);
         while (!q.empty()) {
             auto [y, x] = q.front(); q.pop();
-            for (int i = 0; i < 4; ++i) {
-                int nx = x + dirs[i];
-                int ny = y + dirs[i + 1];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                if (visited[ny][nx] == 1) continue;
-                if (matrix[y][x] > matrix[ny][nx]) continue;
-                visited[ny][nx] = 1;
+            for (int k = 0; k < 4; ++k) {
+                int ny = y + dirs[k];
+                int nx = x + dirs[k + 1];
+                if (ny < 0 || ny >= m || nx < 0 || nx >= n) continue;
+                if (H[y][x] > H[ny][nx]) continue;
+                if (mark[ny][nx] & (1 << flag)) continue;
+                mark[ny][nx] |= (1 << flag);
                 q.emplace(ny, nx);
             }
         }
-        
-        // Atlantic BFS
-        vector<vector<int>> ans;
-        for (int i = 0; i < m; ++i) {
-            if (visited[i][n - 1] == 1)
-                ans.push_back({i, n - 1});
-            visited[i][n - 1] = 2;
-            q.emplace(i, n - 1);
-        }
-        for (int j = 0; j < n - 1; ++j) {
-            if (visited[m - 1][j] == 1)
-                ans.push_back({m - 1, j});
-            visited[m - 1][j] = 2;
-            q.emplace(m - 1, j);
-        }
-        while (!q.empty()) {
-            auto [y, x] = q.front(); q.pop();
-            for (int i = 0; i < 4; ++i) {
-                int nx = x + dirs[i];
-                int ny = y + dirs[i + 1];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                if (visited[ny][nx] == 2) continue;
-                if (matrix[y][x] > matrix[ny][nx]) continue;
-                if (visited[ny][nx] == 1) ans.push_back({ny, nx});
-                visited[ny][nx] = 2;
-                q.emplace(ny, nx);
-            }
-        }
-        return ans;
     }
 };
