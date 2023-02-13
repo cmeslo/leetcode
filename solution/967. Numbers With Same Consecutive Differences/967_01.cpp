cd01e27293e15dfda8af31f1daf57144bbd58c07
Ray <3932488+cmeslo@users.noreply.github.com>
2023-02-14 01:34:36 +0800

    Update and rename 967.cpp to 967_01.cpp

---
+++ solution/967. Numbers With Same Consecutive Differences/967_01.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> numsSameConsecDiff(int n, int k) {
+        vector<int> res;
+        
+        for (int x = 1; x <= 9; ++x)
+            dfs(n - 1, k, x, res);
+        
+        return res;
+    }
+    
+    void dfs(int n, int k, int x, vector<int>& res) {
+        if (n == 0) {
+            res.push_back(x);
+            return;
+        }
+        
+        int last = x % 10;
+        
+        if (last - k >= 0)
+            dfs(n - 1, k, x * 10 + (last - k), res);
+        
+        if (k != 0 && last + k <= 9)
+            dfs(n - 1, k, x * 10 + (last + k), res);
+    }
+};
