37ac141f1fa2b5d2727515c301ee5c7756ef2a4e
Ray <3932488+cmeslo@users.noreply.github.com>
2023-03-19 00:36:29 +0800

    Update 557.cpp

--- solution/557. Reverse Words in a String III/557.cpp
+++ solution/557. Reverse Words in a String III/557.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string res;
-        istringstream iss(s);
-        string word;
-        while (iss >> word) {
-            int i = 0, j = word.size() - 1;
-            while (i < j)
-                swap(word[i++], word[j--]);
-            res += ' ' + word;
+        int n = s.size();
+        for (int i = 0; i < n; ++i) {
+            int l = i, r = i;
+            while (r < n && s[r] != ' ') ++r;
+            i = r;
+            --r;
+            while (l < r)
+                swap(s[l++], s[r--]);
         }
-        return res.substr(1);
+        return s;
     }
 };
