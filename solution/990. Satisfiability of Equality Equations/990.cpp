f5a188d532e31119a3a6963f65553373557bc10a
Ray <3932488+cmeslo@users.noreply.github.com>
2023-03-23 01:36:34 +0800

    Update 990.cpp

--- solution/990. Satisfiability of Equality Equations/990.cpp
+++ solution/990. Satisfiability of Equality Equations/990.cpp
@@ -1,20 +1,22 @@
 class UFS {
 private:
-    vector<int> father;
+    vector<int> father, rank;
 public:
-    UFS(int n): father(n) {
-        for (int i = 0; i < n; ++i) {
+    UFS(int n): father(n), rank(n) {
+        for (int i = 0; i < n; ++i)
             father[i] = i;
-        }
     }
     
     void merge(int a, int b) {
         a = find(a), b = find(b);
         if (a == b) return;
-        if (a < b) {
+        if (rank[a] < rank[b]) {
+            father[a] = b;
+        } else if (rank[b] < rank[a]) {
             father[b] = a;
         } else {
             father[a] = b;
+            rank[b]++;
         }
     }
     
@@ -29,11 +31,12 @@ class Solution {
 public:
     bool equationsPossible(vector<string>& equations) {
         UFS ufs(26);
-        for (auto& e : equations)
+        
+        for (string& e : equations)
             if (e[1] == '=')
                 ufs.merge(e[0] - 'a', e[3] - 'a');
         
-        for (auto& e : equations)
+        for (string& e : equations)
             if (e[1] == '!' && ufs.find(e[0] - 'a') == ufs.find(e[3] - 'a'))
                 return false;
         
