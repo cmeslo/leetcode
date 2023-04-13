5e38cd690c12715395cc4af38475d6b6ac34ee4c
Ray <3932488+cmeslo@users.noreply.github.com>
2023-04-14 01:13:24 +0800

    Update 12.cpp

--- solution/12. Integer to Roman/12.cpp
+++ solution/12. Integer to Roman/12.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<int> values{1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        vector<string> symbols{"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        static const vector<int> A = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        static const vector<string> S = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         
-        string ans = "";
-        int i = 0;
-        while (num) {
-            if (num >= values[i]) {
-                ans += symbols[i];
-                num -= values[i];
-            } else {
-                ++i;
+        string res = "";
+        for (int i = 0; i < A.size(); ++i) {
+            while (num >= A[i]) {
+                res += S[i];
+                num -= A[i];
             }
         }
-        return ans;
+        return res;
     }
 };
