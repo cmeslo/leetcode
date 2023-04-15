474e538d21c6dba3ea6dd9ac2be9e17a39a5337f
Ray <3932488+cmeslo@users.noreply.github.com>
2023-04-16 02:56:09 +0800

    Update 219_01.cpp

--- solution/219. Contains Duplicate II/219_01.cpp
+++ solution/219. Contains Duplicate II/219_01.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        int n = nums.size();
-        if (n < 2) return false;
-        
-        unordered_map<int, int> m;
-        
-        for (int i = 0; i < n; ++i) {
-            if (m.find(nums[i]) != m.end() && i - m[nums[i]] <= k) return true; 
-            m[nums[i]] = i;
+        unordered_map<int, int> last;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (last.count(nums[i]) && i - last[nums[i]] <= k)
+                return true;
+            last[nums[i]] = i;
         }
         return false;
     }
