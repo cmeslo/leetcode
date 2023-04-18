9496ccc019d63f871e9923682a466b126fa44242
Ray <3932488+cmeslo@users.noreply.github.com>
2023-04-19 01:50:40 +0800

    Update 523_01.cpp

--- solution/523. Continuous Subarray Sum/523_01.cpp
+++ solution/523. Continuous Subarray Sum/523_01.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        if (nums.size() == 0) return false;
-
-		for (int i = 0; i < nums.size(); ++i) {
-			int sum = nums[i];
-			for (int j = i + 1; j < nums.size(); ++j) {
-				sum += nums[j];
-				if (sum == k) return true;
-				if (k != 0 && sum % k == 0) return true;
-			}
-		}
-		return false;
+        int sum = 0;
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); ++i) {
+            sum += nums[i];
+            int x = sum % k;
+            if (x == 0 && i > 0) return true;
+            if (m.count(x) && i - m[x] >= 2) return true;
+            if (!m.count(x)) m[x] = i;
+        }
+        return false;
     }
 };
+
+// x x x [x x x] x x
+//        i   j
+
+// sum[i:j] % k = 0
+
+// (sum[j] - sum[i-1]) % k = 0;
+
+// sum[j] - x = nk;
+
+// sum[j] - nk = x
+
+// sum[j] % k = x % k
