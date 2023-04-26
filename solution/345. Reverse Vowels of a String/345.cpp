f1f9ad30b6ed9b2c084998ce784b5b01ee9ebc66
Ray <3932488+cmeslo@users.noreply.github.com>
2023-04-27 01:42:55 +0800

    Create 345.cpp

---
+++ solution/345. Reverse Vowels of a String/345.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        unordered_set<char> vowels{'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            while (i < j && !vowels.count(s[i])) {
+                ++i;
+            }
+            while (i < j && !vowels.count(s[j])) {
+                --j;
+            }
+            swap(s[i++], s[j--]);
+        }
+        
+        return s;
+    }
+};
