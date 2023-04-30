1bfd2ae475f6d122ca79dd9869a5fa2fe0267510
Ray <3932488+cmeslo@users.noreply.github.com>
2023-05-01 02:07:42 +0800

    Update 1706.cpp

--- solution/1706. Where Will the Ball Fall/1706.cpp
+++ solution/1706. Where Will the Ball Fall/1706.cpp
@@ -2,22 +2,18 @@ class Solution {
 public:
     vector<int> findBall(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<int> res;
+        vector<int> res(n);
         for (int i = 0; i < n; ++i) {
-            int y = 0, x = i;
-            while (true) {
-                if (grid[y][x] == 1) {
-                    if (x + 1 == n || grid[y][x + 1] == -1)
-                        break;
-                    y++, x++;
-                } else {
-                    if (x - 1 < 0 || grid[y][x - 1] == 1)
-                        break;
-                    y++, x--;
+            int x1 = i;
+            for (int y = 0; y < m; ++y) {
+                int x2 = x1 + grid[y][x1];
+                if (x2 < 0 || x2 >= n || grid[y][x1] != grid[y][x2]) {
+                    x1 = -1;
+                    break;
                 }
-                if (y == m) break;
+                x1 = x2;
             }
-            res.push_back(y == m ? x : -1);
+            res[i] = x1;
         }
         return res;
     }
