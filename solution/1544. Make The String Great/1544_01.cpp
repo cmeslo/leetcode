e09e80952eb639eaf88a249425e91b4fa5306d64
Ray <3932488+cmeslo@users.noreply.github.com>
2023-05-04 00:33:24 +0800

    Update 1544_01.cpp

--- solution/1544. Make The String Great/1544_01.cpp
+++ solution/1544. Make The String Great/1544_01.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     string makeGood(string s) {
-        int i = 0;
-        
-        while (s.length() && i < s.length() - 1) {
-            if (s[i] != s[i+1] && tolower(s[i]) == tolower(s[i+1])) {
-                s.erase(i, 2);
-                if (--i < 0) i = 0;
-            } else {
-                i++;
-            }
+        string res;
+        for (char& c : s) {
+            if (!res.empty() && ((res.back() ^ 32) == c || res.back() == (c ^ 32)))
+                res.pop_back();
+            else
+                res.push_back(c);
         }
-        
-        return s;
+        return res;
     }
 };
