3e165186db35df9e24860baa941b8cb8772bc652
Ray <3932488+cmeslo@users.noreply.github.com>
2023-05-10 01:21:37 +0800

    Create 223_01.cpp

---
+++ solution/223. Rectangle Area/223_01.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        // h * w -> covered
+        int w = 0, h = 0;
+        int area = 0;
+        multiset<pair<int, int>> events;
+        events.insert({ax1, 1}), events.insert({ax2, -1});
+        events.insert({bx1, 1}), events.insert({bx2, -1});
+        int pre_x = (*events.begin()).first;
+        for (auto& [x, e] : events) {
+            if (e == -1 && area == 2)
+                w = x - pre_x;
+            area += e;
+            pre_x = x;
+        }
+        
+        events.clear();
+        area = 0;
+        events.insert({ay1, 1}), events.insert({ay2, -1});
+        events.insert({by1, 1}), events.insert({by2, -1});
+        int pre_y = (*events.begin()).first;
+        for (auto& [y, e] : events) {
+            if (e == -1 && area == 2)
+                h = y - pre_y;
+            area += e;
+            pre_y = y;
+        }
+        
+        return (ay2 - ay1) * (ax2 - ax1) + (by2 - by1) * (bx2 - bx1) - h * w;
+    }
+};
