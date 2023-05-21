263f1917f94836abe0226c03db5137783b8ce379
Ray <3932488+cmeslo@users.noreply.github.com>
2023-05-22 00:58:14 +0800

    Create 944.cpp

---
+++ solution/944. Delete Columns to Make Sorted/944.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int m = strs.size(), n = strs[0].size();
+        int res = 0;
+        for (int j = 0; j < n; ++j) {
+            for (int i = 1; i < m; ++i) {
+                if (strs[i - 1][j] > strs[i][j]) {
+                    ++res;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
