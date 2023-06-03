b8b1f1436b9810dac4cbd6d9b683738056dc8750
Ray <3932488+cmeslo@users.noreply.github.com>
2023-06-04 00:35:35 +0800

    Update 100.cpp

--- solution/100. Same Tree/100.cpp
+++ solution/100. Same Tree/100.cpp
@@ -12,13 +12,10 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        // both null
         if (!p && !q) return true;
-        // one of them is null
         if (!p || !q) return false;
-        // different value
-        if (p->val != q->val) return false;
-        return isSameTree(p->left, q->left)
+        return p->val == q->val
+            && isSameTree(p->left, q->left)
             && isSameTree(p->right, q->right);
     }
 };
