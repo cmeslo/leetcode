97da1827709020d56579b7ebd1d3c3d36bb5c9be
Ray <3932488+cmeslo@users.noreply.github.com>
2023-06-28 01:15:43 +0800

    Create 1470.cpp

---
+++ solution/1470. Shuffle the Array/1470.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> res(n + n);
+        for (int i = 0; i < n; ++i) {
+            res[i * 2] = nums[i];
+            res[i * 2 + 1] = nums[i + n];
+        }
+        return res;
+    }
+};
