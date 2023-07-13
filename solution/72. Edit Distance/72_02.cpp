ad14720695bf6f007f6696ab6128391e750e4c97
Ray <3932488+cmeslo@users.noreply.github.com>
2023-07-14 00:32:55 +0800

    Update 72_02.cpp

--- solution/72. Edit Distance/72_02.cpp
+++ solution/72. Edit Distance/72_02.cpp
@@ -2,21 +2,16 @@ class Solution {
 public:
     int minDistance(string word1, string word2) {
         int m = word1.size(), n = word2.size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
         
-        // 初始化邊界
-        for (int i = 0; i <= m; i++) {
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        for (int i = 1; i <= m; ++i)
             dp[i][0] = i;
-        }
-        for (int j = 0; j <= n; j++) {
+        for (int j = 1; j <= n; ++j)
             dp[0][j] = j;
-        }
         
-        // 遞推
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                int r = word1[i-1] == word2[j-1] ? 0 : 1;
-                
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                int r = word1[i-1] != word2[j-1];
                 dp[i][j] = min(dp[i-1][j-1] + r,
                                 min(dp[i-1][j], dp[i][j-1]) + 1); 
             }
@@ -24,3 +19,12 @@ public:
         return dp[m][n];
     }
 };
+
+
+// dp[i][j] := minimum number of operations to make word1[:i] = word2[:j]
+
+// dp[i][j] = dp[i-1][j-1] if word1[i] == word2[j]
+//            min(dp[i-1][j], dp[i][j-1]) + 1 if word1[i] != word2[j]
+
+// x x s
+// x x x x r
