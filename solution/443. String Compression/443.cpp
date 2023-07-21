c59cdf4d1610ecf230ff6193added72e4e6889e4
Ray <3932488+cmeslo@users.noreply.github.com>
2023-07-22 02:28:35 +0800

    Update 443.cpp

--- solution/443. String Compression/443.cpp
+++ solution/443. String Compression/443.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        vector<pair<char, int>> A;
-        A.push_back({chars[0], 0});
-        for (char& c : chars) {
-            if (A.back().first == c)
-                A.back().second++;
-            else
-                A.push_back({c, 1});
-        }
+        int n = chars.size();
         int i = 0;
-        for (int j = 0; j < A.size(); ++j) {
-            chars[i++] = A[j].first;
-            if (A[j].second == 1) continue;
-            string num = to_string(A[j].second);
-            for (char& c : num)
-                chars[i++] = c;
+        for (int j = 1, len = 1; j <= n; ++j, ++len) {
+            if (j == n || chars[j] != chars[j - 1]) {
+                chars[i++] = chars[j - 1];
+                if (len > 1) {
+                    string num = to_string(len);
+                    for (char& x : num)
+                        chars[i++] = x;
+                }
+                len = 0;
+            }
         }
         return i;
     }
