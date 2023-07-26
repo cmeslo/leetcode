e181acf773a699c726ce0da59435fed66a78c0fc
Ray <3932488+cmeslo@users.noreply.github.com>
2023-07-27 00:41:29 +0800

    Update 129_01.cpp

--- solution/129. Sum Root to Leaf Numbers/129_01.cpp
+++ solution/129. Sum Root to Leaf Numbers/129_01.cpp
@@ -14,12 +14,14 @@ public:
     int sumNumbers(TreeNode* root) {
         return dfs(root, 0);
     }
-    
-    int dfs(TreeNode* root, int cur) {
-        if (!root) return 0;
-        cur = cur * 10 + root->val;
-        if (!root->left && !root->right)
-            return cur;
-        return dfs(root->left, cur) + dfs(root->right, cur);
-    }
+private:
+	int dfs(TreeNode* node, int num) {
+		if (!node) return 0;
+        
+		num = num * 10 + node->val;
+		if (!node->left && !node->right)
+			return num;
+        
+		return dfs(node->left, num) + dfs(node->right, num);
+	}
 };
