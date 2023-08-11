fd506441520add65629a786e55af1e6e4db44410
Ray <3932488+cmeslo@users.noreply.github.com>
2023-08-11 11:36:36 +0800

    Update 1466.cpp

--- solution/1466. Reorder Routes to Make All Paths Lead to the City Zero/1466.cpp
+++ solution/1466. Reorder Routes to Make All Paths Lead to the City Zero/1466.cpp
@@ -6,8 +6,8 @@ public:
         
         for (auto& c : connections) {
             int a = c[0], b = c[1];
-            adj[a].push_back({b, 1});
-            adj[b].push_back({a, 0});
+            adj[a].push_back(b);
+            adj[b].push_back(-a);
         }
         
         res = 0;
@@ -16,16 +16,17 @@ public:
     }
 
 private:
-    vector<vector<pair<int, int>>> adj; // {node, direction}
+    vector<vector<int>> adj;
     vector<int> seen;
     int res;
     
     void dfs(int node) {
         seen[node]++;
-        for (auto [next, dir] : adj[node]) {
-            if (seen[next]) continue;
-            if (dir) ++res;
-            dfs(next);
+        for (int next : adj[node]) {
+            int nxt = abs(next);
+            if (seen[nxt]) continue;
+            if (next > 0) ++res;
+            dfs(nxt);
         }
     }
 };
