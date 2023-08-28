2e671ff5aaafd70119c5380a8c0b9ff196119bda
Ray <3932488+cmeslo@users.noreply.github.com>
2023-08-29 01:26:43 +0800

    Create 2390.cpp

---
+++ solution/2390. Removing Stars From a String/2390.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string res = "";
+        for (char& c : s) {
+            if (c == '*')
+                res.pop_back();
+            else
+                res.push_back(c);
+        }
+        return res;
+    }
+};
