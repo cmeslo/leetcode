3b69c0f686c5e2959a8bde1376696f9cac89380c
Ray <3932488+cmeslo@users.noreply.github.com>
2023-09-07 01:23:25 +0800

    Update 1372.cpp

--- solution/1372. Longest ZigZag Path in a Binary Tree/1372.cpp
+++ solution/1372. Longest ZigZag Path in a Binary Tree/1372.cpp
@@ -12,20 +12,14 @@
 class Solution {
 public:
     int longestZigZag(TreeNode* root) {
-        dfs(root, 0, 0);
-        return res;
+        return dfs(root, 0, 0);
     }
     
 private:
-    int res = 0;
-    
-    void dfs(TreeNode* root, int left, int right) {
-        if (!root) {
-            res = max({res, left - 1, right - 1});
-            return;
-        }
+    int dfs(TreeNode* root, int left, int right) {
+        if (!root) return max(left - 1, right - 1);
         
-        dfs(root->left, 0, left + 1);
-        dfs(root->right, right + 1, 0);
+        return max(dfs(root->left, 0, left + 1),
+                   dfs(root->right, right + 1, 0));
     }
 };
