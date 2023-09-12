1b58ef0918ad3c83db425911a4ed219bb7b4e61f
Ray <3932488+cmeslo@users.noreply.github.com>
2023-09-13 07:57:53 +0800

    Create 2336_01.cpp

---
+++ solution/2336. Smallest Number in Infinite Set/2336_01.cpp
@@ -0,0 +1,28 @@
+class SmallestInfiniteSet {
+private:
+    set<int> s;
+
+public:
+    SmallestInfiniteSet() {
+        for (int i = 1; i <= 1000; ++i)
+            s.insert(i);
+    }
+    
+    int popSmallest() {
+        int x = *s.begin();
+        s.erase(x);
+        return x;
+    }
+    
+    void addBack(int num) {
+        if (!s.count(num))
+            s.insert(num);
+    }
+};
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet* obj = new SmallestInfiniteSet();
+ * int param_1 = obj->popSmallest();
+ * obj->addBack(num);
+ */
