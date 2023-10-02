bb1c0ecafc23f6e5756387de0d5074cee3c4f67d
Ray <3932488+cmeslo@users.noreply.github.com>
2023-10-03 01:22:10 +0800

    Update 1035.cpp

--- solution/1035. Uncrossed Lines/1035.cpp
+++ solution/1035. Uncrossed Lines/1035.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
         int m = nums1.size(), n = nums2.size();
-        nums1.insert(nums1.begin(), 0);
-        nums2.insert(nums2.begin(), 0);
         vector<vector<int>> dp(m + 1, vector<int>(n + 1));
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
-                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
-                if (nums1[i] == nums2[j])
+                if (nums1[i-1] == nums2[j-1])
                     dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1);
+                else
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
             }
         }
         return dp[m][n];
@@ -17,8 +16,9 @@ public:
 };
 
 
-//  2,5,1,2,5
-//    |  \ \
-// 10,5,2,1,5,2
+// x x x x i x
+
+// x x x j x x
      
-// dp[i][j] = dp[i-1][j-1] + (A[i] == B[j])
+// dp[i][j] = dp[i-1][j-1] + (A[i] == B[j]) if A[i] == B[j]
+//          = max(dp[i-1][j], dp[i][j-1]]) if A[i] != B[j]
