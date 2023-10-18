d6ea60dfd245f02be1115ee59240844606a36e9b
Ray <3932488+cmeslo@users.noreply.github.com>
2023-10-18 22:11:50 +0800

    Create 1140.cpp

---
+++ solution/1140. Stone Game II/1140.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+        suf.resize(n + 1);
+        for (int i = n - 1; i >= 0; --i) {
+            suf[i] = suf[i + 1] + piles[i];
+        }
+        return dp(piles, 0, 1);
+    }
+    
+private:
+    vector<int> suf;
+    int cache[101][101];
+    
+    int dp(vector<int>& piles, int i, int M) {
+        if (i >= piles.size()) return 0;
+        if (cache[i][M] != 0)
+            return cache[i][M];
+        
+        int res = 0, sum = 0;
+        for (int x = 1; x <= 2 * M && i + x - 1 < piles.size(); ++x) {
+            sum += piles[i + x - 1];
+            res = max(res, sum + suf[i + x] - dp(piles, i + x, max(M, x)));
+        }
+        return cache[i][M] = res;
+    }
+};
