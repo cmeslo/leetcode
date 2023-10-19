77f0e2e830b3c25e97b22fda64c61ec28e637adb
Ray <3932488+cmeslo@users.noreply.github.com>
2023-10-20 01:49:50 +0800

    Update 1406_01.cpp

--- solution/1406. Stone Game III/1406_01.cpp
+++ solution/1406. Stone Game III/1406_01.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     string stoneGameIII(vector<int>& stoneValue) {
-        this->n = stoneValue.size();
-        fill(begin(memo), begin(memo) + n, INT_MIN);
-        
-        for (int i = n - 1; i >= 0; --i)
-            suf[i] = suf[i + 1] + stoneValue[i];
-        
-        int res = dp(0, stoneValue) - (suf[0] - dp(0, stoneValue));
+        int res = dfs(0, stoneValue);
         if (res > 0)
             return "Alice";
         if (res < 0)
@@ -17,20 +11,18 @@ public:
     
     
 private:
-    int n;
-    int suf[50001];
-    int memo[50001];
+    int dp[50001] = {[0 ... 50000] = INT_MIN};
     
-    int dp(int i, vector<int>& stone) {
-        if (i >= n) return 0;
-        if (memo[i] != INT_MIN) return memo[i];
+    int dfs(int i, vector<int>& stone) {
+        if (i >= stone.size()) return 0;
+        if (dp[i] != INT_MIN) return dp[i];
         
         int sum = 0;
-        for (int k = 1; k <= 3 && i + k - 1 < n; ++k) {
+        for (int k = 1; k <= 3 && i + k - 1 < stone.size(); ++k) {
             sum += stone[i + k - 1];
-            memo[i] = max(memo[i], sum + suf[i + k] - dp(i + k, stone));
+            dp[i] = max(dp[i], sum - dfs(i + k, stone));
         }
-        return memo[i];
+        return dp[i];
     }
 };
 
