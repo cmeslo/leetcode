1296d9c0f26a9788cd721655204da2576e21c2e9
Ray <3932488+cmeslo@users.noreply.github.com>
2024-01-03 00:55:49 +0800

    Update 207_01.cpp

--- solution/207. Course Schedule/207_01.cpp
+++ solution/207. Course Schedule/207_01.cpp
@@ -17,15 +17,12 @@ public:
         
         while (!q.empty()) {
             int node = q.front(); q.pop();
+            n--;
             for (int next : adj[node]) {
                 if (--inDegree[next] == 0)
                     q.push(next);
             }
         }
-        for (int i = 0; i < n; ++i) {
-            if (inDegree[i] != 0)
-                return false;
-        }
-        return true;
+        return n == 0;
     }
 };
