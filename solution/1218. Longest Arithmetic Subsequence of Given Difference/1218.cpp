c7ecf6dc9c8ec477ebbf7d3dc5f39d9691518ecb
Ray <3932488+cmeslo@users.noreply.github.com>
2024-01-05 01:27:26 +0800

    Update 1218.cpp

--- solution/1218. Longest Arithmetic Subsequence of Given Difference/1218.cpp
+++ solution/1218. Longest Arithmetic Subsequence of Given Difference/1218.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int longestSubsequence(vector<int>& arr, int difference) {
-        int ans = 0;
-        unordered_map<int, int> dp;
-        
-        for (int n : arr) {
-            if (dp.count(n - difference))
-                dp[n] = dp[n - difference] + 1;
+        int res = 0;
+        unordered_map<int, int> m;
+        for (int x : arr) {
+            if (m.count(x - difference))
+                m[x] = m[x - difference] + 1;
             else
-                dp[n] = 1;
-            
-            ans = max(ans, dp[n]);
+                m[x] = 1;
+            res = max(res, m[x]);
         }
-        
-        return ans;
+        return res;
     }
 };
