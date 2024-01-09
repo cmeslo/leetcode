b29683fbd18668e9d552996439831f891d5f2200
Ray <3932488+cmeslo@users.noreply.github.com>
2024-01-10 01:25:12 +0800

    Update 435_01.cpp

--- solution/435. Non-overlapping Intervals/435_01.cpp
+++ solution/435. Non-overlapping Intervals/435_01.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        int n = intervals.size();
-        if (n <= 1) return 0;
-        
+        const int n = intervals.size();
         sort(intervals.begin(), intervals.end(), [](auto& a, auto& b) {
             return a[1] < b[1];
         });
-        int ans = 0;
         
-        auto& prev = intervals[0];
-        for (int i = 1; i < n; i++) {
-            auto& next = intervals[i];
-            if (prev[1] <= next[0]) prev = next;
-            else ans++;
+        int ans = 0, i = 0;
+        for (int j = 1; j < n; ++j) {
+            if (intervals[i][1] <= intervals[j][0])
+                i = j;
+            else
+                ans++;
         }
-        
         return ans;
     }
 };
+
+
+// ____
+//     _____
+//          ____
+// _________
+    
