49c57addb276f684198dfcd6cd49b099a49bae1f
Ray <3932488+cmeslo@users.noreply.github.com>
2024-01-14 00:53:51 +0800

    Update 50.cpp

--- solution/50. Pow(x, n)/50.cpp
+++ solution/50. Pow(x, n)/50.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        return myPowHelper(x, n);
-    }
-
-private:
-    double myPowHelper(double x, long n) { // long n, for case: (2.00000, -2147483648)
-        if (n == 0) return 1;
+    double myPow(double x, long n) {
+        if (n < 0) return 1.0 / myPow(x, -n);
+        if (n == 0) return 1.0;
         if (n == 1) return x;
-        if (x == 1) return 1;
         
-        if (n < 0) return myPowHelper(1/x, -n);
-        
-        double result = myPowHelper(x*x, n/2);
-        if (n % 2) result *= x;
-        return result;
+        double res = myPow(x, n / 2);
+        res *= res;
+        if (n % 2) res *= x;
+        return res;
     }
 };
