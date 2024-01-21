0337935efbccdc30e8c5f1f9a937a55f85836090
Ray <3932488+cmeslo@users.noreply.github.com>
2024-01-22 04:02:09 +0800

    Create 486_03.cpp

---
+++ solution/486. Predict the Winner/486_03.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool PredictTheWinner(vector<int>& nums) {
+		int n = nums.size();
+		vector<vector<int>> dp(n, vector<int>(n));
+
+		for (int s = n - 1; s >= 0; --s) {
+			for (int e = s + 1; e < n; ++e) {
+				int a = nums[s] - dp[s + 1][e];
+				int b = nums[e] - dp[s][e - 1];
+				dp[s][e] = max(a, b);
+			}
+		}
+		return dp[0][n - 1] >= 0;
+	}
+};
