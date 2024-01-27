634fd9912c43a7b57a9b63d76c44ffeb94490bf5
Ray <3932488+cmeslo@users.noreply.github.com>
2024-01-27 11:24:48 +0800

    Create 77.cpp

---
+++ solution/77. Combinations/77.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> res;
+        vector<int> cur;
+        dfs(1, n, k, cur, res);
+        return res;
+    }
+    
+    void dfs(int start, int n, int k, vector<int>& cur, vector<vector<int>>& res) {
+        if (cur.size() == k) {
+            res.push_back(cur);
+            return;
+        }
+        
+        for (int i = start; i <= n; ++i) {
+            cur.push_back(i);
+            dfs(i + 1, n, k, cur, res);
+            cur.pop_back();
+        }
+    }
+};
