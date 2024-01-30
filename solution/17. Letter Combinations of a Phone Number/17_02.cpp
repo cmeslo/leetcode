e4684cf75f5ae6668a62cab4df40172de6976164
Ray <3932488+cmeslo@users.noreply.github.com>
2024-01-30 08:57:52 +0800

    Update 17_02.cpp

--- solution/17. Letter Combinations of a Phone Number/17_02.cpp
+++ solution/17. Letter Combinations of a Phone Number/17_02.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        if (digits.length() == 0) return {};
+        if (digits.empty()) return {};
         
-        vector<vector<char>> d(10);
-        d[0] = {' '};
-        d[1] = {};
-        d[2] = {'a', 'b', 'c'};
-        d[3] = {'d', 'e', 'f'};
-        d[4] = {'g', 'h', 'i'};
-        d[5] = {'j', 'k', 'l'};
-        d[6] = {'m', 'n', 'o'};
-        d[7] = {'p', 'q', 'r', 's'};
-        d[8] = {'t', 'u', 'v'};
-        d[9] = {'w', 'x', 'y', 'z'};
+        const vector<string> m = {
+            "0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+        };
         
-        vector<string> ans = {""};
-        for (const char& digit : digits) {
+        vector<string> ans;
+        ans.push_back("");
+        for (char& d : digits) {
             vector<string> tmp;
-            for (const string& pre : ans) {
-                for (const char& c : d[digit - '0']) {
+            for (char c : m[d - '0']) {
+                for (string& pre : ans) {
                     tmp.push_back(pre + c);
                 }
             }
-            ans.swap(tmp);
+            swap(ans, tmp);
         }
+        
         return ans;
     }
 };
