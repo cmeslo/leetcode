cc8d7c79d6bd23f27ec1bb6358072886a01ed549
Ray <3932488+cmeslo@users.noreply.github.com>
2024-01-30 08:55:49 +0800

    Update 17_01.cpp

--- solution/17. Letter Combinations of a Phone Number/17_01.cpp
+++ solution/17. Letter Combinations of a Phone Number/17_01.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        if (digits.length() == 0) return {};
-        
-        vector<vector<char>> d(10);
-        d[0] = {' '};
-        d[1] = {};
-        d[2] = {'a', 'b', 'c'};
-        d[3] = {'d', 'e', 'f'};
-        d[4] = {'g', 'h', 'i'};
-        d[5] = {'j', 'k', 'l'};
-        d[6] = {'m', 'n', 'o'};
-        d[7] = {'p', 'q', 'r', 's'};
-        d[8] = {'t', 'u', 'v'};
-        d[9] = {'w', 'x', 'y', 'z'};
-        
-        vector<string> ans;
+        if (digits.empty()) return {};
+        vector<string> res;
         string cur;
-        dfs(digits, d, 0, cur, ans);
-        return ans;
+        dfs(digits, 0, cur, res);
+        return res;
     }
-    
+
 private:
-    void dfs(const string& digits, const vector<vector<char>>& d, int l, string& cur, vector<string>& ans) {
-        if (l == digits.length()) {
-            ans.push_back(cur);
+    const vector<string> m = {
+        "0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+    };
+    
+    void dfs(const string& digits, int i, string& cur, vector<string>& res) {
+        if (i == digits.size()) {
+            res.push_back(cur);
             return;
         }
         
-        for (const char& c : d[digits[l] - '0']) {
+        for (const char& c : m[digits[i] - '0']) {
             cur.push_back(c);
-            dfs(digits, d, l+1, cur, ans);
+            dfs(digits, i + 1, cur, res);
             cur.pop_back();
         }
     }
