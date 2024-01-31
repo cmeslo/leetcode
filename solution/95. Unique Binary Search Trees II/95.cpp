a80c2dee235850899f5c48f5a356efea51274e2c
Ray <3932488+cmeslo@users.noreply.github.com>
2024-02-01 01:52:06 +0800

    Update 95.cpp

--- solution/95. Unique Binary Search Trees II/95.cpp
+++ solution/95. Unique Binary Search Trees II/95.cpp
@@ -12,26 +12,26 @@
 class Solution {
 public:
     vector<TreeNode*> generateTrees(int n) {
-        return generateTrees(1, n);
+        return dfs(1, n);
     }
-    
-private:
-    vector<TreeNode*> generateTrees(int start, int end) {
-        if (start > end) return {nullptr};
-        
-        vector<TreeNode*> ans;
+
+    vector<TreeNode*> dfs(int start, int end) {
+        if (start > end) return { nullptr };
+
+        vector<TreeNode*> res;
         for (int i = start; i <= end; ++i) {
-            auto left_subtrees = generateTrees(start, i - 1);
-            auto right_subtrees = generateTrees(i + 1, end);
-            for (auto l : left_subtrees) {
-                for (auto r : right_subtrees) {
-                    TreeNode* root = new TreeNode(i);
+            vector<TreeNode*> L = dfs(start, i - 1);
+            vector<TreeNode*> R = dfs(i + 1, end);
+            for (auto& l : L) {
+                for (auto& r : R) {
+                    auto root = new TreeNode(i);
                     root->left = l;
                     root->right = r;
-                    ans.push_back(root);
+                    res.push_back(root);
                 }
             }
         }
-        return ans;
+        
+        return res;
     }
 };
