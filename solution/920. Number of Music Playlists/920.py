3f40e736ada4e35098d56d4f13ebe70480a9a7f9
Ray <3932488+cmeslo@users.noreply.github.com>
2024-02-05 00:34:36 +0800

    Update 920.py

--- solution/920. Number of Music Playlists/920.py
+++ solution/920. Number of Music Playlists/920.py
@@ -1,15 +1,13 @@
 class Solution:
     def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
         mod = 10**9 + 7
-        dp = {}
 
+        @lru_cache(None)
         def count(cur_goal, old_songs):
             if cur_goal == 0 and old_songs == n:
                 return 1
             if cur_goal == 0 or old_songs > n:
                 return 0
-            if (cur_goal, old_songs) in dp:
-                return dp[(cur_goal, old_songs)]
 
             # choose new song
             res = (n - old_songs) * count(cur_goal - 1, old_songs + 1)
@@ -17,9 +15,7 @@ class Solution:
             # choose old song
             if old_songs > k:
                 res += (old_songs - k) * count(cur_goal - 1, old_songs)
-
-            dp[(cur_goal, old_songs)] = res % mod
-            return dp[(cur_goal, old_songs)]
+            
+            return res % mod
 
         return count(goal, 0)
-      
