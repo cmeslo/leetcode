a12ab5ef90b5829b19bb2afef926e15bba107347
Ray <3932488+cmeslo@users.noreply.github.com>
2024-03-08 00:42:38 +0800

    Create 1269.cpp

---
+++ solution/1269. Number of Ways to Stay in the Same Place After Some Steps/1269.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numWays(int steps, int arrLen) {
+        int n = min(steps/2+2, arrLen);
+        vector<long>dp(n);
+        long M = 1e9+7;
+        dp[0] = 1;
+        for (int k=0; k<steps; k++)
+        {
+            auto dp2 = dp;
+            for (int i=0; i<n; i++)
+            {
+                dp[i] = dp2[i] + (i>=1 ? dp2[i-1]:0) + (i+1<n ? dp2[i+1] : 0);
+                dp[i] %= M;
+            }
+        }
+        return dp[0];
+    }
+};
+
+// dp[i][0] = dp[i-1][0] + dp[i-1][1]
+
+// dp[i][1] = dp[i-1][0] + dp[i-1][1] + dp[i-1][2]
+
+// dp[0][0] = 1;
+// dp[0][1...arrlen-1] = 0
