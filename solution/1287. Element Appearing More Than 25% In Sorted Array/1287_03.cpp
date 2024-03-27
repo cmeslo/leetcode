a452d6da0e0efa222979b370fd26ca435a2713f9
Ray <3932488+cmeslo@users.noreply.github.com>
2024-03-28 00:21:57 +0800

    Update 1287_03.cpp

--- solution/1287. Element Appearing More Than 25% In Sorted Array/1287_03.cpp
+++ solution/1287. Element Appearing More Than 25% In Sorted Array/1287_03.cpp
@@ -4,35 +4,11 @@ public:
         const int n = arr.size();
         vector<int> A { arr[n/4], arr[n/2], arr[n*3/4] }; // 斬開4份, 只有這3個數有可能是答案
         for (int x : A) {
-            int l = firstOccurence(arr, x);
-            int r = lastOccurence(arr, x);
-            if ((r - l + 1) > n / 4)
+            auto l = lower_bound(arr.begin(), arr.end(), x);
+            auto r = upper_bound(arr.begin(), arr.end(), x);
+            if (distance(l, r) > n/4)
                 return x;
         }
         return -1;
-    }
-    
-    int firstOccurence(vector<int>& arr, int val) {
-        int l = 0, r = arr.size(), m;
-        while (l < r) {
-            m = l + (r - l) / 2;
-            if (arr[m] < val)
-                l = m + 1;
-            else
-                r = m;
-        }
-        return l;
-    }
-    
-    int lastOccurence(vector<int>& arr, int val) {
-        int l = 0, r = arr.size(), m;
-        while (l < r) {
-            m = l + (r - l) / 2;
-            if (arr[m] <= val)
-                l = m + 1;
-            else
-                r = m;
-        }
-        return l - 1;
     }
 };
