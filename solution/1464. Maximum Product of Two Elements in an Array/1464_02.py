3ac8fd925694e28213e7c55cafe217a3164c2c06
Ray <3932488+cmeslo@users.noreply.github.com>
2024-03-31 01:37:22 +0800

    Create 1464_02.py

---
+++ solution/1464. Maximum Product of Two Elements in an Array/1464_02.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        mx1, mx2 = 0, 0
+        for x in nums:
+            if x > mx1:
+                mx1, mx2 = x, mx1
+            elif x > mx2:
+                mx2 = x
+        return (mx1 - 1) * (mx2 - 1)
