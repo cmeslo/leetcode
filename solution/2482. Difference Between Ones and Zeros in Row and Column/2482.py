81e87bafcbeb82cd62387026c93e8ad53320bf0b
Ray <3932488+cmeslo@users.noreply.github.com>
2024-04-03 00:30:09 +0800

    Create 2482.py

---
+++ solution/2482. Difference Between Ones and Zeros in Row and Column/2482.py
@@ -0,0 +1,18 @@
+class Solution:
+    def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
+        m, n = len(grid), len(grid[0])
+        onesRow = [0] * m
+        onesCol = [0] * n
+        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    onesRow[i] += 1
+                    onesCol[j] += 1
+        
+        res = [[0] * n for i in range(m)]
+        for i in range(m):
+            for j in range(n):
+                res[i][j] = 2*onesRow[i] + 2*onesCol[j] - m - n
+        return res
+    
