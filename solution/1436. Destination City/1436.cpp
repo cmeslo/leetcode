835fb50b1da745daa52cfe00746e0440b38b379c
Ray <3932488+cmeslo@users.noreply.github.com>
2024-04-05 00:18:48 +0800

    Update 1436.cpp

--- solution/1436. Destination City/1436.cpp
+++ solution/1436. Destination City/1436.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
-        unordered_map<string, int> outDegree;
-        unordered_set<string> cities;
-        for (auto& path : paths) {
-            outDegree[path[0]]++;
-            cities.insert(path[0]);
-            cities.insert(path[1]);
-        }
-        for (string city : cities) {
-            if (outDegree[city] == 0)
-                return city;
-        }
-        return "";
+        unordered_set<string> s;
+        for (auto& path : paths)
+            s.insert(path[1]);
+        for (auto& path : paths)
+            s.erase(path[0]);
+        return *s.begin();
     }
 };
