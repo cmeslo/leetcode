70ee50797ad76543e203cc5118bfd2ddb3806c35
Ray <3932488+cmeslo@users.noreply.github.com>
2024-04-24 01:51:27 +0800

    Update 1758.cpp

--- solution/1758. Minimum Changes To Make Alternating Binary String/1758.cpp
+++ solution/1758. Minimum Changes To Make Alternating Binary String/1758.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int minOperations(string s) {
-        int res1 = 0, res2 = 0;
-        for (int i = 0; i < s.size(); ++i) {
-            if (i % 2 == 0) {
-                res1 += s[i] != '0';
-                res2 += s[i] != '1';
-            } else {
-                res1 += s[i] != '1';
-                res2 += s[i] != '0';
-            }
+        int res = 0, n = s.size();
+        for (int i = 0; i < n; ++i) {
+            if ((s[i] - '0') != i % 2)
+                ++res;
         }
-        return min(res1, res2);
+        return min(res, n - res);
     }
 };
 
