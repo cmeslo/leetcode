177abf1ee568790c2277d225784b9daeb1300bbd
Ray <3932488+cmeslo@users.noreply.github.com>
2024-06-24 02:13:18 +0800

    Update 1043.cpp

--- solution/1043. Partition Array for Maximum Sum/1043.cpp
+++ solution/1043. Partition Array for Maximum Sum/1043.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
-    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+    int maxSumAfterPartitioning(vector<int>& arr, int K) {
         const int n = arr.size();
-        arr.insert(arr.begin(), 0);
-        
-        vector<vector<int>> mx(n + 1, vector<int>(n + 1));
-        for (int i = 1; i <= n; ++i) {
-            mx[i][i] = arr[i];
-            for (int j = i-1; j >= 1; --j) {
-                mx[j][i] = max(mx[j + 1][i], arr[j]);
-            }
-        }
-        
         vector<int> dp(n + 1);
         for (int i = 1; i <= n; ++i) {
-            for (int j = max(1, i-k+1); j <= i; ++j) {
-                dp[i] = max(dp[i], mx[j][i] * (i-j+1) + dp[j-1]);
+            int mx = 0;
+            for (int k = 1; k <= K && i - k >= 0; ++k) {
+                mx = max(mx, arr[i - k]);
+                dp[i] = max(dp[i], mx * k + dp[i - k]);
             }
         }
         return dp[n];
