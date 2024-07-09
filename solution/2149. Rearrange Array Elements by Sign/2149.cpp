d6435594171b6d53e4aaa724fc76245196ff39aa
Ray <3932488+cmeslo@users.noreply.github.com>
2024-07-10 01:22:39 +0800

    Update 2149.cpp

--- solution/2149. Rearrange Array Elements by Sign/2149.cpp
+++ solution/2149. Rearrange Array Elements by Sign/2149.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        queue<int> P, N;
+        vector<int> res(nums.size());
+        int i = 0, j = 1;
         for (int x : nums) {
-            if (x > 0)
-                P.push(x);
-            else
-                N.push(x);
-        }
-        vector<int> res;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (i % 2) {
-                res.push_back(N.front());
-                N.pop();
+            if (x > 0) {
+                res[i] = x;
+                i += 2;
             } else {
-                res.push_back(P.front());
-                P.pop();
+                res[j] = x;
+                j += 2;
             }
         }
         return res;
