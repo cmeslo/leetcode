35da82a1ebdc43f544b23f2b77db2a536c6c1991
Ray <3932488+cmeslo@users.noreply.github.com>
2024-08-22 01:16:46 +0800

    Create 2540_01.cpp

---
+++ solution/2540. Minimum Common Value/2540_01.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() < nums2.size())
+            swap(nums1, nums2);
+        
+        for (int x : nums1) {
+            auto it = lower_bound(nums2.begin(), nums2.end(), x);
+            if (it != nums2.end() && *it == x)
+                return x;
+        }
+        return -1;
+    }
+};
