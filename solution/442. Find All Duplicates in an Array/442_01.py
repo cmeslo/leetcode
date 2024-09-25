86af3b4f98af17fcaae08f024ac65dd88b7377ae
Ray <3932488+cmeslo@users.noreply.github.com>
2024-09-26 02:15:44 +0800

    Create 442_01.py

---
+++ solution/442. Find All Duplicates in an Array/442_01.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        res = []
+        for x in nums:
+            idx = abs(x) - 1
+            if nums[idx] < 0:
+                res.append(idx + 1)
+            else:
+                nums[idx] = -nums[idx]
+        return res
+    
