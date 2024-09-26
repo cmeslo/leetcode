2ee534a78f67727fa33cc89aedc68e631f0e64e8
Ray <3932488+cmeslo@users.noreply.github.com>
2024-09-27 01:33:01 +0800

    Update 41.cpp

--- solution/41. First Missing Positive/41.cpp
+++ solution/41. First Missing Positive/41.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int i = 0;
-        int n = nums.size();
-        
-        for (int i = 0; i < n; ++i) {
-            while (nums[i] >= 1 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
-                std::swap(nums[i], nums[nums[i] - 1]);
-            }
+        const int n = nums.size();
+        for (int x : nums) {
+            while (x > 0 && x <= n && nums[x - 1] != x)
+                swap(nums[x - 1], x);
         }
-        
         for (int i = 0; i < n; ++i) {
-            if (nums[i] != i + 1) return i + 1;
+            if (nums[i] != i + 1)
+                return i + 1;
         }
         return n + 1;
     }
