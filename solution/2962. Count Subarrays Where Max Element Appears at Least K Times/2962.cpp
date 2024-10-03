1cfd7b8ef40f494472dc7126a8f2341a8e0d2597
Ray <3932488+cmeslo@users.noreply.github.com>
2024-10-04 00:52:07 +0800

    Update 2962.cpp

--- solution/2962. Count Subarrays Where Max Element Appears at Least K Times/2962.cpp
+++ solution/2962. Count Subarrays Where Max Element Appears at Least K Times/2962.cpp
@@ -3,25 +3,16 @@ public:
     long long countSubarrays(vector<int>& nums, int k) {
         const int n = nums.size();
         int mx = *max_element(nums.begin(), nums.end());
-        
-        vector<int> nextMax(n, n);
-        int mx_idx = n;
-        for (int i = n - 1; i >= 0; --i) {
-            nextMax[i] = mx_idx;
-            if (nums[i] == mx)
-                mx_idx = i;
-        }
-        
-        long long res = 0;
         int mx_cnt = 0;
+        long long res = 0;
+        
         for (int i = 0, j = 0; j < n; ++j) {
-            if (nums[j] == mx && ++mx_cnt == k) {
-                while (mx_cnt == k) {
-                    if (nums[i] == mx) {
-                        res += 1L * (i + 1) * (nextMax[j] - j);
-                        mx_cnt--;
-                    }
-                    i++;
+            if (nums[j] == mx) {
+                ++mx_cnt;
+                while (mx_cnt >= k) {
+                    res += n - j;
+                    if (nums[i++] == mx)
+                        --mx_cnt;
                 }
             }
         }
@@ -30,8 +21,11 @@ public:
 };
 
 
-// . . x . . [x . . x] . . .
-//            i     j        n
+// [. . x . . x . . x] . . .
+//  i               j        n
+ 
+// . [. x . . x . . x] . . .
+//    i             j        n
 
            
 // 1 [3 2 3] 3
