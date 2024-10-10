2d989b6e07367cb79e956cd7fa074be6ff74308a
Ray <3932488+cmeslo@users.noreply.github.com>
2024-10-11 01:04:24 +0800

    Update and rename 205.cpp to 205_01.cpp

--- solution/205. Isomorphic Strings/205_01.cpp
+++ solution/205. Isomorphic Strings/205_01.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        int n = s.size();
         vector<int> m1(128, -1), m2(128, -1);
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < s.size(); ++i) {
             if (m1[s[i]] != m2[t[i]]) return false;
             m1[s[i]] = i;
             m2[t[i]] = i;
