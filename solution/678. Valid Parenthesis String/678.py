46a5aa6011511bed7f372ce7515a3b6a7325aa84
Ray <3932488+cmeslo@users.noreply.github.com>
2024-10-21 00:37:00 +0800

    Create 678.py

---
+++ solution/678. Valid Parenthesis String/678.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        cmin = cmax = 0
+        for c in s:
+            if c == '(':
+                cmin += 1
+                cmax += 1
+            elif c == ')':
+                cmin -= 1
+                cmax -= 1
+            else:
+                cmax += 1
+                cmin -= 1
+            
+            if cmax < 0:
+                return False
+            if cmin < 0:
+                cmin = 0
+                
+        return cmin == 0
+    
