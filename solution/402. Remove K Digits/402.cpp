74ebf90657c0bfd37d4bdeb3409669233b2cd6f4
Ray <3932488+cmeslo@users.noreply.github.com>
2024-10-31 01:02:09 +0800

    Update 402.cpp

--- solution/402. Remove K Digits/402.cpp
+++ solution/402. Remove K Digits/402.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        int n = num.size();
-        int targetSize = n - k;
-        
         string res;
         for (char& c : num) {
             while (!res.empty() && res.back() > c && k) {
@@ -13,21 +10,9 @@ public:
             if (!res.empty() || c != '0')
                 res.push_back(c);
         }
-
-        while (res.size() && k--) res.pop_back();
+        
+        while (!res.empty() && k--)
+            res.pop_back();
         return res.empty() ? "0" : res;
     }
 };
-
-
-// 1432219 -> xxxx
-
-// 143
-// 14
-// 122
-// 121
-// 1219
-
-// x..xx.x
-
-// 1 1 2
