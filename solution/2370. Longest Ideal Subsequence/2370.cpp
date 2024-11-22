832fae4133e9d85eae61718ed13d434eaf94f802
Ray <3932488+cmeslo@users.noreply.github.com>
2024-11-23 02:00:05 +0800

    Update 2370.cpp

--- solution/2370. Longest Ideal Subsequence/2370.cpp
+++ solution/2370. Longest Ideal Subsequence/2370.cpp
@@ -2,21 +2,16 @@ class Solution {
 public:
     int longestIdealString(string s, int k) {
         const int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(26));
-        int res = dp[0][s[0] - 'a'] = 1;
-        for (int i = 1; i < n; ++i) {
-            int c = s[i] - 'a';
-            for (int j = 0; j < 26; ++j)
-                dp[i][j] = dp[i - 1][j];
-            for (int j = max(0, c - k); j <= min(25, c + k); ++j)
-                dp[i][c] = max(dp[i][c], dp[i - 1][j] + 1);
-            res = max(res, dp[i][c]);
+        int res = 0;
+        vector<int> A(26);
+        for (int i = 0; i < n; ++i) {
+            const int a = s[i] - 'a';
+            int cnt = 0;
+            for (int j = max(0, a - k); j <= min(25, a + k); ++j)
+                cnt = max(cnt, A[j] + 1);
+            A[a] = max(A[a], cnt);
+            res = max(res, A[a]);
         }
         return res;
     }
 };
-
-
-// x x x x c
-
-// dp[i][c] = dp[i - 1][c-k : c+k] + 1
