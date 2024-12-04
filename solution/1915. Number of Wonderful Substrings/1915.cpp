fb5279cb7b53babba8e33e95725d3f65692e8c90
Ray <3932488+cmeslo@users.noreply.github.com>
2024-12-05 00:31:03 +0800

    Update 1915.cpp

--- solution/1915. Number of Wonderful Substrings/1915.cpp
+++ solution/1915. Number of Wonderful Substrings/1915.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     long long wonderfulSubstrings(string word) {
         long long res = 0;
-        unordered_map<int, int> prefix;
-        int mask = 0;
-        prefix[0] = 1;
+        vector<int> cnt(1 << 10);
+        cnt[0] = 1;
+        int state = 0;
         for (char& c : word) {
-            mask ^= (1 << (c - 'a'));
-            res += prefix[mask];
+            state ^= (1 << (c - 'a'));
+            res += cnt[state];
             for (int i = 0; i < 10; ++i) {
-                if (prefix.count(mask ^ (1 << i)))
-                    res += prefix[mask ^ (1 << i)];
+                res += cnt[state ^ (1 << i)];
             }
-            ++prefix[mask];
+            ++cnt[state];
         }
         return res;
     }
