5754e2673a6c8ab95ddf6006069b78cd32db3cec
Ray <3932488+cmeslo@users.noreply.github.com>
2024-12-29 01:25:36 +0800

    Update 3075.cpp

--- solution/3075. Maximize Happiness of Selected Children/3075.cpp
+++ solution/3075. Maximize Happiness of Selected Children/3075.cpp
@@ -3,8 +3,8 @@ public:
     long long maximumHappinessSum(vector<int>& H, int k) {
         sort(H.rbegin(), H.rend());
         long res = 0;
-        for (int i = 0, dec = 0; i < k && H[i] > dec; ++i, ++dec) {
-            res += H[i] - dec;
+        for (int i = 0; i < k && H[i] > i; ++i) {
+            res += H[i] - i;
         }
         return res;
     }
