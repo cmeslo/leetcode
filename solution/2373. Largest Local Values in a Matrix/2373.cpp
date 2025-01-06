61e2a07209831c0be16e66be40dc2a7716543c44
Ray <3932488+cmeslo@users.noreply.github.com>
2025-01-06 22:56:30 +0800

    Update 2373.cpp

--- solution/2373. Largest Local Values in a Matrix/2373.cpp
+++ solution/2373. Largest Local Values in a Matrix/2373.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
-        int n = grid.size();
+        const int n = grid.size();
         vector<vector<int>> res(n - 2, vector<int>(n - 2));
         for (int i = 0; i < n - 2; ++i) {
             for (int j = 0; j < n - 2; ++j) {
-                int mx = 0;
-                for (int y = i; y < i + 3; ++y) {
-                    for (int x = j; x < j + 3; ++x) {
-                        mx = max(mx, grid[y][x]);
-                    }
-                }
-                res[i][j] = mx;
+                for (int y = i; y < i + 3; ++y)
+                    for (int x = j; x < j + 3; ++x)
+                        res[i][j] = max(res[i][j], grid[y][x]);
             }
         }
         return res;
