07fc289ae7fc76175a8e66871de7f9b6c0e41068
Ray <3932488+cmeslo@users.noreply.github.com>
2025-02-12 01:09:43 +0800

    Update 1404.cpp

--- solution/1404. Number of Steps to Reduce a Number in Binary Representation to One/1404.cpp
+++ solution/1404. Number of Steps to Reduce a Number in Binary Representation to One/1404.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
     int numSteps(string s) {
-        const int n = s.size();
-        int one = count(s.begin(), s.end(), '1');
         int res = 0, carry = 0;
-        for (int i = n - 1; i >= 0; --i) {
-            if (one == 1 && s[i] == '1')
-                break;
-            
-            int x = s[i] - '0' + carry;
-            if (x == 0) {
-                res += 1; // divide it by 2
-            } else if (x == 1) {
+        for (int i = s.size() - 1; i > 0; --i) {
+            if (s[i] - '0' + carry == 1) {
                 carry = 1;
-                res += 2; // add 1 + divide by 2
-            } else if (x == 2) {
-                carry = 1;
-                res += 1;  // divide it by 2
+                res += 1; // add 1
             }
+            res += 1; // divide it by 2
         }
-        return res;
+        return res + carry; // carry 出現過一次, 就只會不斷向左移而不會消失
     }
 };
 
