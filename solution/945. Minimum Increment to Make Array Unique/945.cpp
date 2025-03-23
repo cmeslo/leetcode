6ad090c4d3f7a0147ff2a5e9aa5919765dec9085
Ray <3932488+cmeslo@users.noreply.github.com>
2025-03-23 08:27:34 +0800

    Update 945.cpp

--- solution/945. Minimum Increment to Make Array Unique/945.cpp
+++ solution/945. Minimum Increment to Make Array Unique/945.cpp
@@ -3,18 +3,16 @@ public:
     int minIncrementForUnique(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         
-        int res = 0;
-        for (int i = 1; i < nums.size(); ++i) {
-            if (nums[i - 1] >= nums[i]) {
-                res += nums[i - 1] + 1 - nums[i];
-                nums[i] = nums[i - 1] + 1;
-            }
+        int res = 0, need = 0;
+        for (int x : nums) {
+            res += max(need - x, 0);
+            need = max(need, x) + 1;
         }
         return res;
     }
 };
 
 // 1 1 2 2 3 7
-// 1 2 3 
+// 1 2 3 4
 
 // 1 2 3 4 5 7
