4b72e6ce378d3898e50cf91dd172420c7b311d1f
Ray <3932488+cmeslo@users.noreply.github.com>
2025-04-15 08:57:05 +0800

    Update 995.cpp

--- solution/995. Minimum Number of K Consecutive Bit Flips/995.cpp
+++ solution/995. Minimum Number of K Consecutive Bit Flips/995.cpp
@@ -2,23 +2,17 @@ class Solution {
 public:
     int minKBitFlips(vector<int>& nums, int k) {
         const int n = nums.size();
-        vector<int> F(n + 1);
-        int res = 0, f = 0;
-        if (nums[0] == 0) {
-            F[0] += 1;
-            F[k] -= 1;
-            f = 1;
-            ++res;
-        }
-        for (int i = 1; i < n; ++i) {
-            f += F[i];
-            if ((nums[i] == 0 && (f % 2 == 0)) || (nums[i] == 1 && (f % 2))) {
+        
+        vector<int> diff(n + 1);
+        int res = 0, sum = 0;
+        for (int i = 0; i < n; ++i) {
+            if ((sum + diff[i] + nums[i]) % 2 == 0) {
                 if (i + k > n) return -1;
-                F[i] += 1;
-                F[i + k] -= 1;
-                f += 1;
+                diff[i] += 1;
+                diff[i + k] -= 1;
                 ++res;
             }
+            sum += diff[i];
         }
         return res;
     }
