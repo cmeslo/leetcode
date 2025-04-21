7721607fc46b9f7b9b472f7e27014f7676be08e0
Ray <3932488+cmeslo@users.noreply.github.com>
2025-04-21 21:14:21 +0800

    Update 1382.cpp

--- solution/1382. Balance a Binary Search Tree/1382.cpp
+++ solution/1382. Balance a Binary Search Tree/1382.cpp
@@ -12,25 +12,24 @@
 class Solution {
 public:
     TreeNode* balanceBST(TreeNode* root) {
-        vector<int> A;
+        vector<TreeNode*> A;
         dfs(root, A);
         return build(A, 0, A.size() - 1);
     }
 
 private:
-    void dfs(TreeNode* node, vector<int>& A) {
+    void dfs(TreeNode* node, vector<TreeNode*>& A) {
         if (!node) return;
         dfs(node->left, A);
-        A.push_back(node->val);
+        A.push_back(node);
         dfs(node->right, A);
     }
     
-    TreeNode* build(vector<int>& A, int l, int r) {
+    TreeNode* build(vector<TreeNode*>& A, int l, int r) {
         if (l > r) return nullptr;
-        if (l == r) return new TreeNode(A[l]);
         
         int mid = l + (r - l) / 2;
-        auto node = new TreeNode(A[mid]);
+        auto node = A[mid];
         node->left = build(A, l, mid - 1);
         node->right = build(A, mid + 1, r);
         return node;
