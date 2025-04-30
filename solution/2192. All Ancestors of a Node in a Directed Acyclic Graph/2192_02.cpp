1351d52c6d53058344c2b0b81ed96ccc0e899ad5
Ray <3932488+cmeslo@users.noreply.github.com>
2025-04-30 10:10:55 +0800

    Update 2192_02.cpp

--- solution/2192. All Ancestors of a Node in a Directed Acyclic Graph/2192_02.cpp
+++ solution/2192. All Ancestors of a Node in a Directed Acyclic Graph/2192_02.cpp
@@ -2,28 +2,25 @@ class Solution {
 public:
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
         vector<vector<int>> adj(n);
-        for (auto& e : edges) {
-            int from = e[0], to = e[1];
-            adj[to].push_back(from);
-        }
+        for (auto& e : edges)
+            adj[e[0]].push_back(e[1]);
         
-        vector<vector<int>> ans(n);
+        vector<vector<int>> res(n);
         for (int i = 0; i < n; ++i) {
-            vector<bool> visited(n, false);
-            dfs(i, adj, visited, ans[i]);
-            ans[i].pop_back();
-            sort(ans[i].begin(), ans[i].end());
+            vector<bool> seen(n);
+            seen[i] = true;
+            dfs(i, i, adj, seen, res);
         }
-        return ans;
+        return res;
     }
     
-    void dfs(int i, vector<vector<int>>& adj, vector<bool>& visited, vector<int>& ans) {
-        if (visited[i]) return;
-        
-        visited[i] = true;
-        for (int a : adj[i]) {
-            dfs(a, adj, visited, ans);
+private:
+    void dfs(int root, int node, vector<vector<int>>& adj, vector<bool>& seen, vector<vector<int>>& res) {
+        for (int next : adj[node]) {
+            if (seen[next]) continue;
+            seen[next] = true;
+            res[next].push_back(root);
+            dfs(root, next, adj, seen, res);
         }
-        ans.push_back(i);
     }
 };
