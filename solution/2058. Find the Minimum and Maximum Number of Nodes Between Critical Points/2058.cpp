3f6b91cacbb58fefdb452b5011840432a4e6647b
Ray <3932488+cmeslo@users.noreply.github.com>
2025-05-16 09:39:00 +0800

    Update 2058.cpp

--- solution/2058. Find the Minimum and Maximum Number of Nodes Between Critical Points/2058.cpp
+++ solution/2058. Find the Minimum and Maximum Number of Nodes Between Critical Points/2058.cpp
@@ -13,27 +13,22 @@ public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
         vector<int> res = {INT_MAX, -1};
         
-        int mn_c = -1, mx_c = -1, pre_c = -1;
-        int pre = head->val;
-        head = head->next;
-        int j = 1, i = 1;
-        
-        while (head && head->next) {
+        int min_c = 0, last = 0;
+        int pre = 0;
+        for (int i = 0; head && head->next; ++i, head = head->next) {
             int next = head->next->val;
             if ((pre < head->val && head->val > next) || (pre > head->val && head->val < next)) {
-                if (pre_c != -1)
-                    res[0] = min(res[0], i - pre_c);
-                mx_c = pre_c = i;
-                if (mn_c == -1)
-                    mn_c = i;
+                if (last)
+                    res[0] = min(res[0], i - last);
+                if (!min_c)
+                    min_c = i;
+                last = i;
             }
             pre = head->val;
-            head = head->next;
-            ++i;
         }
         
-        if (mn_c == mx_c) return {-1, -1};
-        res[1] = mx_c - mn_c;
+        if (res[0] == INT_MAX) return {-1, -1};
+        res[1] = last - min_c;
         return res;
     }
 };
