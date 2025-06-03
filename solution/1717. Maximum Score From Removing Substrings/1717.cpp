9ca43e0a225191cd6e909248bd22f9b6aa496e62
Ray <3932488+cmeslo@users.noreply.github.com>
2025-06-03 08:58:42 +0800

    Update 1717.cpp

--- solution/1717. Maximum Score From Removing Substrings/1717.cpp
+++ solution/1717. Maximum Score From Removing Substrings/1717.cpp
@@ -1,52 +1,27 @@
 class Solution {
 public:
     int maximumGain(string s, int x, int y) {
-        int res = 0;
+        char a = 'a', b = 'b';
+        if (x < y) { // 保證 'ab', 即 x 是最大的
+            swap(a, b);
+            swap(x, y);
+        }
+        string s1 = remove(s, a, b); // 先移除 'ab'
+        string s2 = remove(s1, b, a); // 後移除 'ba'
 
+        return (s.size() - s1.size()) / 2 * x + (s1.size() - s2.size()) / 2 * y;
+    }
+    
+private:
+    string remove(string& s, char& a, char& b) {
         string st;
-        bool ab = x > y;
         for (char& c : s) {
-            bool push = true;
-            if (!st.empty() && (c == 'a' || c == 'b')) {
-                if (ab) {
-                    if (st.back() == 'a' && c == 'b') {
-                        st.pop_back();
-                        res += x;
-                        push = false;
-                    }
-                } else {
-                    if (st.back() == 'b' && c == 'a') {
-                        st.pop_back();
-                        res += y;
-                        push = false;
-                    }
-                }
-            }
-            if (push) st.push_back(c);
+            if (!st.empty() && st.back() == a && c == b)
+                st.pop_back();
+            else
+                st.push_back(c);
         }
-
-        string st2;
-        for (char& c : st) {
-            bool push = true;
-            if (!st2.empty() && (c == 'a' || c == 'b')) {
-                if (ab) {
-                    if (st2.back() == 'b' && c == 'a') {
-                        st2.pop_back();
-                        res += y;
-                        push = false;
-                    }
-                } else {
-                    if (st2.back() == 'a' && c == 'b') {
-                        st2.pop_back();
-                        res += x;
-                        push = false;
-                    }
-                }
-            }
-            if (push) st2.push_back(c);
-        }
-
-        return res;
+        return st;
     }
 };
 
