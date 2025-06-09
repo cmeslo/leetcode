507d7eea7fa3af87b1cbe6e0911030db61cb67fa
Ray <3932488+cmeslo@users.noreply.github.com>
2025-06-09 09:00:27 +0800

    Update 2196_02.cpp

--- solution/2196. Create Binary Tree From Descriptions/2196_02.cpp
+++ solution/2196. Create Binary Tree From Descriptions/2196_02.cpp
@@ -12,24 +12,25 @@
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        unordered_map<int, int> inDegree;
+        unordered_set<int> exclude;
         unordered_map<int, TreeNode*> m;
         for (auto& d : descriptions) {
             int root = d[0], child = d[1], isLeft = d[2];
-            if (!inDegree.count(root)) inDegree[root] = 0;
+            
             if (!m.count(root)) m[root] = new TreeNode(root);
             if (!m.count(child)) m[child] = new TreeNode(child);
-            if (isLeft) {
+            
+            if (isLeft)
                 m[root]->left = m[child];
-            } else {
+            else
                 m[root]->right = m[child];
-            }
-            inDegree[child]++;
+            
+            exclude.insert(child);
         }
         
-        for (auto& [node, d] : inDegree)
-            if (d == 0)
-                return m[node];
+        for (auto& d : descriptions)
+            if (!exclude.count(d[0]))
+                return m[d[0]];
         return nullptr;
     }
 };
