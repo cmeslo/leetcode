c4a4992480129e6fc2f2fcb3b10e3b0ffa6897bf
Ray <3932488+cmeslo@users.noreply.github.com>
2025-06-20 08:56:37 +0800

    Create 1380.cpp

---
+++ solution/1380. Lucky Numbers in a Matrix/1380.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> luckyNumbers (vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        
+        vector<int> row_min(m, 1e5 + 1), col_max(n, 0);
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                col_max[j] = max(col_max[j], matrix[i][j]);
+                row_min[i] = min(row_min[i], matrix[i][j]);
+            }
+        }
+        
+        vector<int> res;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == col_max[j] && matrix[i][j] == row_min[i])
+                    res.push_back(matrix[i][j]);
+            }
+        }
+        return res;
+    }
+};
