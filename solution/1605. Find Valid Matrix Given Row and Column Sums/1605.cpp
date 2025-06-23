5ca00b75cc720766778eeb5696a15517aea3856a
Ray <3932488+cmeslo@users.noreply.github.com>
2025-06-23 08:54:33 +0800

    Create 1605.cpp

---
+++ solution/1605. Find Valid Matrix Given Row and Column Sums/1605.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+        const int m = rowSum.size(), n = colSum.size();
+        vector<vector<int>> res(m, vector<int>(n));
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                res[i][j] = min(rowSum[i], colSum[j]);
+                rowSum[i] -= res[i][j];
+                colSum[j] -= res[i][j];
+            }
+        }
+        return res;
+    }
+};
