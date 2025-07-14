2ec49f4b826acf23822bf530ecedda78ca70f667
Ray <3932488+cmeslo@users.noreply.github.com>
2025-07-14 08:55:53 +0800

    Update 1653_02.cpp

--- solution/1653. Minimum Deletions to Make String Balanced/1653_02.cpp
+++ solution/1653. Minimum Deletions to Make String Balanced/1653_02.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     int minimumDeletions(string s) {
         int n = s.size();
-        
-        vector<int> dp(n);
-        dp[0] = 0;
-        int bCount = s[0] == 'b';
-        for (int i = 1; i < n; ++i) {
+        s = '#' + s;
+        int dpi = 0, bCount = 0;
+        for (int i = 1; i <= n; ++i) {
             if (s[i] == 'a') {
                 // keep 'a' => bCount
                 // delete 'a' => dp[i - 1] + 1
-                dp[i] = min(bCount, dp[i - 1] + 1);
+                // dp[i] = min(bCount, dp[i - 1] + 1);
+                dpi = min(bCount, dpi + 1);
             } else {
-                dp[i] = dp[i - 1];
+                // dp[i] = dp[i - 1];
                 bCount++;
             }
         }
-        return dp.back();
+        return dpi;
     }
 };
 
