f1ef30e05965bddcc5af914e4bc606ead4f52ca7
Ray <3932488+cmeslo@users.noreply.github.com>
2025-07-19 10:17:45 +0800

    Create 2678.cpp

---
+++ solution/2678. Number of Senior Citizens/2678.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int res = 0;
+        for (string& d : details) {
+            int age = stoi(d.substr(11, 2));
+            if (age > 60)
+                ++res;
+        }
+        return res;
+    }
+};
