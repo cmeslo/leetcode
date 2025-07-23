5111cce27032f1a02f3a754342e72c2bcda2733f
Ray <3932488+cmeslo@users.noreply.github.com>
2025-07-23 09:07:11 +0800

    Update 2134.cpp

--- solution/2134. Minimum Swaps to Group All 1's Together II/2134.cpp
+++ solution/2134. Minimum Swaps to Group All 1's Together II/2134.cpp
@@ -1,36 +1,17 @@
 class Solution {
 public:
     int minSwaps(vector<int>& nums) {
-        return min(minSwaps(nums, 0), minSwaps(nums, 1));
-    }
-    
-    int minSwaps(vector<int>& nums, int x) {
-        int n = nums.size(), cnt = 0;
-        vector<int> presum(n);
-        if (nums[0] == x) {
-            presum[0] = 1;
-            cnt = 1;
-        }
-        
-        for (int i = 1; i < n; ++i) {
-            presum[i] = presum[i - 1];
-            if (nums[i] == x) {
-                cnt++;
-                presum[i]++;
-            }
-        }
-        if (cnt == 0) return 0;
-        
-        int res = n;
-        for (int i = cnt - 1; i < n; ++i) {
-            int left = i - cnt < 0 ? 0 : presum[i - cnt];
-            int mid = presum[i] - left;
-            res = min(res, cnt - mid);
+        int ones = count(nums.begin(), nums.end(), 1), n = nums.size(), res = nums.size();
+        for (int i = 0, j = 0, cnt = 0; i < n; ++i) {
+            while (j - i < ones)
+                cnt += nums[j++ % n];
+            res = min(res, ones - cnt);
+            cnt -= nums[i];
         }
         return res;
     }
 };
 
 
-// x x [x x x] x x
+// x x [1 0 1] x x
 // [x x x] x x x x
