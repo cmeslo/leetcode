28cf4fd4950e59c107ac073a8909d2068c44e68c
Ray <3932488+cmeslo@users.noreply.github.com>
2025-08-15 08:59:51 +0800

    Update and rename 959_01.cpp to 959.cpp

--- solution/959. Regions Cut By Slashes/959.cpp
+++ solution/959. Regions Cut By Slashes/959.cpp
@@ -3,18 +3,14 @@ public:
     int regionsBySlashes(vector<string>& grid) {
         const int m = grid.size(), n = grid[0].size();
         
-        int len = ((m - 1) * n + (n - 1) + 1) * 2;
+        int len = m * n * 2;
         father.resize(len);
         
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 int a = (i * n + j) * 2;
                 int b = a + 1;
-                if (grid[i][j] == '/') {
-                    father[a] = a;
-                    father[b] = b;
-                    mergeNeighbor(grid, i, j);
-                } else if (grid[i][j] == '\\') {
+                if (grid[i][j] == '/' || grid[i][j] == '\\') {
                     father[a] = a;
                     father[b] = b;
                     mergeNeighbor(grid, i, j);
@@ -26,9 +22,8 @@ public:
         }
         
         unordered_set<int> s;
-        for (int i = 0; i < len; ++i) {
+        for (int i = 0; i < len; ++i)
             s.insert(findFather(i));
-        }
         
         return s.size();
     }
@@ -82,44 +77,11 @@ private:
             }
         }
         
-//         // merge bottom
-//         if (i + 1 < m) {
-//             int down_a = ((i + 1) * n + j) * 2;
-//             int down_b = down_a + 1;
-            
-//             if (grid[i][j] == '/' && grid[i + 1][j] == '/') {
-//                 merge(b, down_a);
-//             } else if (grid[i][j] == '/' && grid[i + 1][j] == '\\') {
-//                 merge(b, down_b);
-//             } else if (grid[i][j] == '/' && grid[i + 1][j] == ' ') {
-//                 merge(b, down_a);
-//             } else if (grid[i][j] == '\\' && grid[i + 1][j] == '\\') {
-//                 merge(a, down_b);
-//             } else if (grid[i][j] == '\\' && grid[i + 1][j] == '/') {
-//                 merge(a, down_a);
-//             } else if (grid[i][j] == '\\' && grid[i + 1][j] == ' ') {
-//                 merge(a, down_a);
-//             } else if (grid[i][j] == ' ' && grid[i + 1][j] == '/') {
-//                 merge(a, down_a);
-//             } else if (grid[i][j] == ' ' && grid[i + 1][j] == '\\') {
-//                 merge(a, down_b);
-//             } else if (grid[i][j] == ' ' && grid[i + 1][j] == ' ') {
-//                 merge(a, down_a);
-//             }
-//         }
-        
         // merge left
         if (j - 1 >= 0) {
             int left_a = (i * n + (j - 1)) * 2;
             int left_b = left_a + 1;
             merge(a, left_b);
         }
-        
-        // // merge right
-        // if (j + 1 < n) {
-        //     int right_a = (i * n + (j + 1)) * 2;
-        //     int right_b = right_a + 1;
-        //     merge(b, right_a);
-        // }
     }
 };
