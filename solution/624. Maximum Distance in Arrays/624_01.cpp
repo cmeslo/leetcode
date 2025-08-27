b51695acdc3ab76ee36b46fde1b86d6b73a9b369
Ray <3932488+cmeslo@users.noreply.github.com>
2025-08-27 08:56:20 +0800

    Update 624_01.cpp

--- solution/624. Maximum Distance in Arrays/624_01.cpp
+++ solution/624. Maximum Distance in Arrays/624_01.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& arrays) {
+        const int n = arrays.size();
         int res = 0;
-        multiset<int> s;
-        for (auto& arr : arrays)
-            s.insert(arr.back());
         
-        for (auto& arr : arrays) {
-            auto it = s.find(arr.back());
-            s.erase(it);
-            res = max(res, *s.rbegin() - arr[0]);
-            s.insert(arr.back());
+        vector<pair<int, int>> mx(2);
+        mx[0] = mx[1] = {INT_MIN, -1};
+        for (int i = 0; i < n; ++i) {
+            if (arrays[i].back() > mx[0].first) {
+                swap(mx[0], mx[1]);
+                mx[0] = {arrays[i].back(), i};
+            } else if (arrays[i].back() > mx[1].first) {
+                mx[1] = {arrays[i].back(), i};
+            }
+        }
+        
+        for (int i = 0; i < n; ++i) {
+            if (mx[0].second != i)
+                res = max(res, mx[0].first - arrays[i][0]);
+            else
+                res = max(res, mx[1].first - arrays[i][0]);
         }
         
         return res;
