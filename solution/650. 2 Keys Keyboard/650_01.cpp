3fff1591fbb932bbcbac2fa3da693c6ddb408a43
Ray <3932488+cmeslo@users.noreply.github.com>
2025-09-03 00:38:08 +0800

    Update 650_01.cpp

--- solution/650. 2 Keys Keyboard/650_01.cpp
+++ solution/650. 2 Keys Keyboard/650_01.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int minSteps(int n) {
         if (n <= 1) return 0;
-        for (int x = n / 2; x >= 2; --x) {
+        for (int x = 2; x < n; ++x) {
             if (n % x == 0)
-                return minSteps(x) + n/x;
+                return minSteps(n / x) + x;
         }
         return n;
     }
