e69e81b6a0623676b277f68802e5f6fbeddf7d5c
Ray <3932488+cmeslo@users.noreply.github.com>
2025-09-16 08:59:04 +0800

    Update 145.cpp

--- solution/145. Binary Tree Postorder Traversal/145.cpp
+++ solution/145. Binary Tree Postorder Traversal/145.cpp
@@ -1,59 +1,28 @@
-#include <iostream>
-#include <vector>
-#include <stack>
-using namespace std;
-
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-	vector<int> postorderTraversal(TreeNode* root)
-	{
-		vector<int> ans;
-		if (nullptr == root) return ans;
-
-		stack<TreeNode*> st;
-
-		st.push(root);
-
-		while (!st.empty())
-		{
-			TreeNode* current = st.top();
-			st.pop();
-
-			ans.push_back(current->val);
-
-			if (nullptr != current->left) st.push(current->left);
-			if (nullptr != current->right) st.push(current->right);
-		}
-		std::reverse(ans.begin(), ans.end());
-
-		return ans;
-	}
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        dfs(root, res);
+        return res;
+    }
+    
+private:
+    void dfs(TreeNode* node, vector<int>& res) {
+        if (!node) return;
+        
+        dfs(node->left, res);
+        dfs(node->right, res);
+        res.push_back(node->val);
+    }
 };
-
-void main()
-{
-//	TreeNode* t = nullptr;
-
-	TreeNode* t = new TreeNode(1);
-
-	t->left = new TreeNode(2);
-	t->right = new TreeNode(3);
-	t->left->left = new TreeNode(4);
-	t->left->right = new TreeNode(5);
-	t->right->left = new TreeNode(6);
-	t->right->right = new TreeNode(7);
-
-	auto ans = new Solution;
-	vector<int> v = ans->postorderTraversal(t);
-	for (vector<int>::iterator i = v.begin(); i != v.end(); ++i){
-		cout << *i << ", ";
-	}
-	cout << endl;
-}
