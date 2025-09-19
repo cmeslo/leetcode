741afaee48b50d09341afa518bc45ed6294638d3
Ray <3932488+cmeslo@users.noreply.github.com>
2025-09-19 08:56:26 +0800

    Create 590.py

---
+++ solution/590. N-ary Tree Postorder Traversal/590.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        res = []
+        
+        def dfs(node):
+            if not node:
+                return
+            
+            for nxt in node.children:
+                dfs(nxt)
+            
+            res.append(node.val)
+            
+        dfs(root)
+        return res
+    
