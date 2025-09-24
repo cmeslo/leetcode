593a71db0d0cb9e81e4a75032ad4271772dedde8
Ray <3932488+cmeslo@users.noreply.github.com>
2025-09-24 13:19:28 +0800

    Create 2022.py

---
+++ solution/2022. Convert 1D Array Into 2D Array/2022.py
@@ -0,0 +1,13 @@
+class Solution:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        if len(original) != m * n:
+            return []
+        
+        res = [[0] * n for _ in range(m)]
+        k = 0
+        for i in range(m):
+            for j in range(n):
+                res[i][j] = original[k]
+                k += 1
+        return res
+    
