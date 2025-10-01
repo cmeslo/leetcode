a1eee3562de1a1889b4980c707b93aaf5569fac7
Ray <3932488+cmeslo@users.noreply.github.com>
2025-10-01 08:26:44 +0800

    Update 874.cpp

--- solution/874. Walking Robot Simulation/874.cpp
+++ solution/874. Walking Robot Simulation/874.cpp
@@ -4,41 +4,24 @@ public:
         set<pair<int, int>> O;
         for (auto& o : obstacles)
             O.insert({o[0], o[1]});
+        const vector<pair<int, int>> dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
         
         int res = 0;
-        int dy = 1, dx = 0;
-        int y = 0, x = 0;
+        int y = 0, x = 0, d = 0;
         for (auto& k : commands) {
             if (k > 0) {
                 while (k--) {
-                    if (O.count({x + dx, y + dy})) continue;
-                    x += dx;
-                    y += dy;
+                    if (O.count({x + dirs[d].first, y + dirs[d].second})) break;
+                    x += dirs[d].first;
+                    y += dirs[d].second;
                 }
             } else if (k == -2) {
-                if (dx == 0 && dy == 1)
-                    dx = -1, dy = 0;
-                else if (dx == -1 && dy == 0)
-                    dx = 0, dy = -1;
-                else if (dx == 0 && dy == -1)
-                    dx = 1, dy = 0;
-                else
-                    dx = 0, dy = 1;
+                d = (d - 1 + 4) % 4;
             } else if (k == -1) {
-                if (dx == 0 && dy == 1)
-                    dx = 1, dy = 0;
-                else if (dx == 1 && dy == 0)
-                    dx = 0, dy = -1;
-                else if (dx == 0 && dy == -1)
-                    dx = -1, dy = 0;
-                else
-                    dx = 0, dy = 1;
+                d = (d + 1) % 4;
             }
             res = max(res, x * x + y * y);
         }
         return res;
     }
 };
-
-// -90 deg:
-//  0, 1 -> -1, 0 -> 0, -1 ->  1, 0 ->  0, 1
