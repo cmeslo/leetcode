918f134f65d9cbfae7c03e70cf5d23f68abbb779
Ray <3932488+cmeslo@users.noreply.github.com>
2025-10-13 00:09:41 +0800

    Update 2326.cpp

--- solution/2326. Spiral Matrix IV/2326.cpp
+++ solution/2326. Spiral Matrix IV/2326.cpp
@@ -11,20 +11,29 @@
 class Solution {
 public:
     vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
-        vector<vector<int>> ans(m, vector<int>(n, -1));
-        vector<vector<int>> dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        int d = 0;
+        vector<vector<int>> res(m, vector<int>(n, -1));
         int y = 0, x = -1;
-        for (int i = 1; i <= m * n && head; ++i) {
-            int next_y = y + dirs[d][0];
-            int next_x = x + dirs[d][1];
-            if (next_y < 0 || next_y >= m || next_x < 0 || next_x >= n || ans[next_y][next_x] != -1)
-                d = (d + 1) % 4;
-            y = y + dirs[d][0];
-            x = x + dirs[d][1];
-            ans[y][x] = head->val;
-            head = head->next;
+        int dy = 0, dx = 1;
+        for (; head; head = head->next) {
+            int ny = y + dy;
+            int nx = x + dx;
+            if (ny < 0 || ny >= m || nx < 0 || nx >= n || res[ny][nx] != -1) {
+                swap(dy, dx);
+                dx = -dx;
+            }
+            y += dy;
+            x += dx;
+            res[y][x] = head->val;
         }
-        return ans;
+        return res;
     }
 };
+
+// dy, dx
+//  0,  1
+//  1,  0
+//  0, -1
+// -1,  0
+
+// swap(dy, dx)
+// dx = -dx
