3564adab2b1aeb94b39f52c8f0904fc3c717e59b
Ray <3932488+cmeslo@users.noreply.github.com>
2025-10-19 10:16:42 +0800

    Update 2220.cpp

--- solution/2220. Minimum Bit Flips to Convert Number/2220.cpp
+++ solution/2220. Minimum Bit Flips to Convert Number/2220.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int ans = 0;
+        int res = 0;
         for (int i = 0; i < 32; ++i) {
-            if (((start >> i) & 1) ^ ((goal >> i) & 1))
-                ans++;
+            res += ((start >> i) ^ (goal >> i)) & 1;
         }
-        return ans;
+        return res;
     }
 };
