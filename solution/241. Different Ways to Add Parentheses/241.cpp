445f6f959323be1e16bbe29ad03d6eb40fa75aad
Ray <3932488+cmeslo@users.noreply.github.com>
2025-11-06 00:39:12 +0800

    Update 241.cpp

--- solution/241. Different Ways to Add Parentheses/241.cpp
+++ solution/241. Different Ways to Add Parentheses/241.cpp
@@ -10,25 +10,23 @@ private:
         int i = start, num = 0;
         vector<int> res;
         for (int i = start; i <= end; ++i) {
-            if ('0' <= E[i] && E[i] <= '9') continue;
+            if (isdigit(E[i])) continue;
+            
             auto L = dfs(E, start, i - 1);
             auto R = dfs(E, i + 1, end);
-            if (E[i] == '+') {
-                for (auto& l : L)
-                    for (auto& r : R)
-                        res.push_back(l + r);
-            } else if (E[i] == '-') {
-                for (auto& l : L)
-                    for (auto& r : R)
-                        res.push_back(l - r);
-            } else if (E[i] == '*') {
-                for (auto& l : L)
-                    for (auto& r : R)
-                        res.push_back(l * r);
-            }
+            for (auto& l : L)
+                for (auto& r : R)
+                    res.push_back(calc(l, r, E[i]));
         }
         if (res.empty())
             res.push_back(stoi(E.substr(start, end - start + 1)));
         return res;
+    }
+    
+    int calc(int a, int b, char op) {
+        if (op == '+') return a + b;
+        if (op == '-') return a - b;
+        if (op == '*') return a * b;
+        return 0;
     }
 };
